3f576ce78ec7831cfb397efbcda1302a82e018de
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-17 22:38:35 +0300

    Add basic patter matcher for future chat

--- src/main/java/dev/bibikvlad/Main.java
+++ src/main/java/dev/bibikvlad/Main.java
@@ -1,17 +1,28 @@
 package dev.bibikvlad;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            Pattern pattern = Pattern.compile("[rgybmw]{4}");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            boolean close = false;
+
+            while (!close) {
+                String line = bufferedReader.readLine();
+
+                if (line.equals("close")) {
+                    close = true;
+                }
+
+                System.out.println(line.matches(String.valueOf(pattern)));
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
         }
     }
 }
