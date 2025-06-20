c6e6b28ea867a02337f8372c9c560b199ba2e963
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-20 19:37:23 +0300

    Add GameCluesConstants

---
+++ src/main/java/dev/bibikvlad/utils/GameCluesConstants.java
@@ -0,0 +1,8 @@
+package dev.bibikvlad.utils;
+
+public class GameCluesConstants {
+    private static final String CIRCLE_FILLED = "⬤";
+    private static final String CIRCLE_SHADED= "◍";
+    private static final String CIRCLE_EMPTY = "○";
+    private static final String UNDERSCORE = "_";
+}
