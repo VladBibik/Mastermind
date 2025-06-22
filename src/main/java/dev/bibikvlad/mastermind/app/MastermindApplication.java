969d8f2a73497602ea737764ef547a1d9fe7af94
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-22 19:08:21 +0300

    Move MastermindApplication to the custom package

--- src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
+++ src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
@@ -1,4 +1,6 @@
-package dev.bibikvlad;
+package dev.bibikvlad.mastermind.app;
+
+import dev.bibikvlad.MastermindGameChat;
 
 public class MastermindApplication {
     public static void main(String[] args) {
