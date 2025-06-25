89b40026c9a36e81afd465f22184aa672ae76e72
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-25 16:49:55 +0300

    Refactor access modifier from private to public

--- src/main/java/dev/bibikvlad/utils/EmojiCodes.java
+++ src/main/java/dev/bibikvlad/utils/EmojiCodes.java
@@ -1,5 +1,5 @@
 package dev.bibikvlad.utils;
 
 public class EmojiCodes {
-    private static final String CELEBRATION_TADA = "\uD83C\uDF89";
+    public static final String CELEBRATION_TADA = "\uD83C\uDF89";
 }
