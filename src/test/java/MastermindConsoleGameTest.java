aadbf7a48d569398fceb7de3d24ceeddfa6a5143
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-25 23:08:18 +0300

    Add MastermindConsoleGameTest test class Add invalidInputTest test method

---
+++ src/test/java/MastermindConsoleGameTest.java
@@ -0,0 +1,31 @@
+import dev.bibikvlad.mastermind.game.MastermindConsoleGame;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MastermindConsoleGameTest {
+    @Test
+    @DisplayName("Invalid input test")
+    public void invalidInputTest() {
+        String answer = "rrrr";
+        String input = "abcd\nclose\n";
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+
+        MastermindConsoleGame game = new MastermindConsoleGame(answer, bufferedReader, printStream);
+
+        game.play();
+
+        String output = outputStream.toString();
+
+        assertTrue(output.contains("Please provide a valid input"));
+    }
+}
