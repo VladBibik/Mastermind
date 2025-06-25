7fd865dad312ba27ccc0d3029a9fe6595329ca0f
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-25 16:59:24 +0300

    Rename CIRCLE_FILLED -> CIRCLE_SOLID

--- src/main/java/dev/bibikvlad/utils/GameCluesConstants.java
+++ src/main/java/dev/bibikvlad/utils/GameCluesConstants.java
@@ -1,7 +1,7 @@
 package dev.bibikvlad.utils;
 
 public class GameCluesConstants {
-    public static final char CIRCLE_FILLED = '⬤';
+    public static final char CIRCLE_SOLID = '⬤';
     public static final char CIRCLE_SHADED= '◍';
     public static final char CIRCLE_EMPTY = '○';
     public static final char UNDERSCORE = '_';
