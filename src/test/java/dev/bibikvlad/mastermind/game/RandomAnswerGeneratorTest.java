05c5bb12a758b9d8f6bfe68ecbc47a7ceddff26b
BibikVlad <bibikvladislav.work@gmail.com>
2025-06-28 00:34:28 +0300

    Add correctAnswerPatternTest test method

--- src/test/java/dev/bibikvlad/mastermind/game/RandomAnswerGeneratorTest.java
+++ src/test/java/dev/bibikvlad/mastermind/game/RandomAnswerGeneratorTest.java
@@ -3,7 +3,10 @@ package dev.bibikvlad.mastermind.game;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RandomAnswerGeneratorTest {
     @Test
@@ -13,5 +16,14 @@ public class RandomAnswerGeneratorTest {
         String answer = RandomAnswerGenerator.generate();
 
         assertEquals(correctAnswerLength, answer.length());
+    }
+
+    @Test
+    @DisplayName("The class generates string that matches correct pattern")
+    void correctAnswerPatternTest() {
+        String answer = RandomAnswerGenerator.generate();
+        Pattern correctPattern = Pattern.compile("[rgybpw]{4}");
+
+        assertTrue(correctPattern.matcher(answer).matches());
     }
 }
