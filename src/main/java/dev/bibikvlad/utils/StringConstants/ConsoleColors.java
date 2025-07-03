da1ef67fb5bc067cd3a55eff6803b8965731624b
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-04 01:58:17 +0300

    Add BRIGHT_YELLOW, and BRIGHT_WHITE console color code constants

--- src/main/java/dev/bibikvlad/utils/StringConstants/ConsoleColors.java
+++ src/main/java/dev/bibikvlad/utils/StringConstants/ConsoleColors.java
@@ -6,10 +6,12 @@ public class ConsoleColors {
         public static final String RED = "\u001B[31m";
         public static final String GREEN = "\u001B[32m";
         public static final String YELLOW = "\u001B[33m";
+        public static final String BRIGHT_YELLOW = "\u001B[93m";
         public static final String BLUE = "\u001B[34m";
         public static final String PURPLE = "\u001B[35m";
         public static final String CYAN = "\u001B[36m";
         public static final String WHITE = "\u001B[37m";
+        public static final String BRIGHT_WHITE = "\u001B[97m";
     }
 
     public static final class Background {
