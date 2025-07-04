513ad5e98be8af7f6a20443acdf1468aca07d76c
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-04 16:48:36 +0300

    Add ConsoleColorFormatter class Util class that maps console color code to the specific char symbol

---
+++ src/main/java/dev/bibikvlad/utils/Formatters/ConsoleColorFormatter.java
@@ -0,0 +1,20 @@
+package dev.bibikvlad.utils.Formatters;
+
+import dev.bibikvlad.utils.StringConstants.ConsoleColors;
+
+import java.util.Map;
+
+public class ConsoleColorFormatter {
+    private static final Map<Character, String> COLOR_CODE_MAP = Map.of(
+            'r', ConsoleColors.BrightForeground.RED,
+            'g', ConsoleColors.BrightForeground.GREEN,
+            'y', ConsoleColors.BrightForeground.YELLOW,
+            'b', ConsoleColors.BrightForeground.BLUE,
+            'p', ConsoleColors.BrightForeground.PURPLE,
+            'w', ConsoleColors.BrightForeground.WHITE
+    );
+
+    public static String getColorCode(char input) {
+        return COLOR_CODE_MAP.get(input);
+    }
+}
