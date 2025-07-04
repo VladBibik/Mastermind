9022ac3d5b5e470b96653e4b075588d2aca0dbf9
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-04 16:22:53 +0300

    Add BrightForeground inner class Class holds constants for brighter console colors

--- src/main/java/dev/bibikvlad/utils/StringConstants/ConsoleColors.java
+++ src/main/java/dev/bibikvlad/utils/StringConstants/ConsoleColors.java
@@ -6,12 +6,21 @@ public class ConsoleColors {
         public static final String RED = "\u001B[31m";
         public static final String GREEN = "\u001B[32m";
         public static final String YELLOW = "\u001B[33m";
-        public static final String BRIGHT_YELLOW = "\u001B[93m";
         public static final String BLUE = "\u001B[34m";
         public static final String PURPLE = "\u001B[35m";
         public static final String CYAN = "\u001B[36m";
         public static final String WHITE = "\u001B[37m";
-        public static final String BRIGHT_WHITE = "\u001B[97m";
+    }
+
+    public static final class BrightForeground {
+        public static final String BLACK = "\u001B[90m";
+        public static final String RED = "\u001B[91m";
+        public static final String GREEN = "\u001B[92m";
+        public static final String YELLOW = "\u001B[93m";
+        public static final String BLUE = "\u001B[94m";
+        public static final String PURPLE = "\u001B[95m"; // or BRIGHT_MAGENTA
+        public static final String CYAN = "\u001B[96m";
+        public static final String WHITE = "\u001B[97m";
     }
 
     public static final class Background {
