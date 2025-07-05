786526ec0074b629a4103e2f926d822e211b0e5d
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-06 02:08:39 +0300

    Refactor getIncorrectGuessMessage, and getGuessVisualRepresentation Make getGuessVisualRepresentation protected, because from now it will be used in getIncorrectGuessMessage Make getIncorrectGuessMessage take another param 'answer' to be able to call getGuessVisualRepresentation

--- src/main/java/dev/bibikvlad/mastermind/chat/language/Language.java
+++ src/main/java/dev/bibikvlad/mastermind/chat/language/Language.java
@@ -6,13 +6,13 @@ import dev.bibikvlad.mastermind.clues.InputVisualRepresentation;
 public abstract class Language {
     public abstract String getInvalidInputMessage();
 
-    public abstract String getTurnCounterAndUserGuess(int maxTurns, int currentTurn, String userInput);
+    public abstract String getIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput);
 
     public abstract String getGameOverMessage(String answer);
 
     public abstract String getWinMessage(String answer);
 
-    public String getIncorrectGuessMessage(String answer, String userInput) {
+    protected String getGuessVisualRepresentation(String answer, String userInput) {
         return InputVisualRepresentation.getVisualRepresentation(userInput)
                 + "        "
                 + ClueGenerator.generate(answer, userInput);
