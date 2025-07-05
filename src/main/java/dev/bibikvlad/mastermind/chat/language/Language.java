3df5b1d58c39bba2effb95e274a331bddde8c3e4
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-05 19:11:07 +0300

    Add getTurnCounterAndUserGuess abstract method

--- src/main/java/dev/bibikvlad/mastermind/chat/language/Language.java
+++ src/main/java/dev/bibikvlad/mastermind/chat/language/Language.java
@@ -2,4 +2,6 @@ package dev.bibikvlad.mastermind.chat.language;
 
 public abstract class Language {
     public abstract String getInvalidInputMessage();
+
+    public abstract String getTurnCounterAndUserGuess(int maxTurns, int currentTurn, String userInput);
 }
