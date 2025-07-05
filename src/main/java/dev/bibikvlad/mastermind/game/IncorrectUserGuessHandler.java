1b4a86b3fc71af50da5a420f7b8ab2318c2936de
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-06 01:11:23 +0300

    Refactor handle method Remove correct answer handling part

--- src/main/java/dev/bibikvlad/mastermind/game/IncorrectUserGuessHandler.java
+++ src/main/java/dev/bibikvlad/mastermind/game/IncorrectUserGuessHandler.java
@@ -4,17 +4,9 @@ import dev.bibikvlad.mastermind.clues.ClueGenerator;
 import dev.bibikvlad.mastermind.clues.InputVisualRepresentation;
 
 public class IncorrectUserGuessHandler {
-    public static boolean handle(String answer, String userInput) {
-        if (userInput.equals(answer)) {
-            System.out.println("You Won!");
-
-            return true;
-        } else {
-            System.out.println(InputVisualRepresentation.getVisualRepresentation(userInput)
-                    + "        "
-                    + ClueGenerator.generate(answer, userInput));
-
-            return false;
-        }
+    public static void handle(String answer, String userInput) {
+        System.out.println(InputVisualRepresentation.getVisualRepresentation(userInput)
+                + "        "
+                + ClueGenerator.generate(answer, userInput));
     }
 }
