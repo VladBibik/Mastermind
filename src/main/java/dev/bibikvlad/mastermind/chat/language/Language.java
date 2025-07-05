0b4227c35a4679be6bed3f4e2e91d2aaede43702
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-06 00:57:36 +0300

    Add getWinMessage abstract method

--- src/main/java/dev/bibikvlad/mastermind/chat/language/Language.java
+++ src/main/java/dev/bibikvlad/mastermind/chat/language/Language.java
@@ -6,4 +6,6 @@ public abstract class Language {
     public abstract String getTurnCounterAndUserGuess(int maxTurns, int currentTurn, String userInput);
 
     public abstract String getGameOverMessage(String answer);
+
+    public abstract String getWinMessage(String answer);
 }
