a21694370f823b9f7fbf2941b41b2a04ea726b8c
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-05 17:43:21 +0300

    Add language object creation

--- src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
+++ src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
@@ -1,11 +1,15 @@
 package dev.bibikvlad.mastermind.app;
 
+import dev.bibikvlad.mastermind.chat.language.English;
+import dev.bibikvlad.mastermind.chat.language.Language;
 import dev.bibikvlad.mastermind.game.MastermindConsoleGame;
 import dev.bibikvlad.mastermind.game.RandomAnswerGenerator;
 
 public class MastermindApplication {
     public static void main(String[] args) {
-        MastermindConsoleGame game = new MastermindConsoleGame(RandomAnswerGenerator.generate());
+        Language language = new English();
+
+        MastermindConsoleGame game = new MastermindConsoleGame(language, RandomAnswerGenerator.generate());
 
         game.play();
     }
