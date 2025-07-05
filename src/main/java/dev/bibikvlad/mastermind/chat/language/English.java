7d4cd1501f7925441ce9c5d489904d62f0da1d7d
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-05 18:09:49 +0300

    Add getInvalidInputMessage method

--- src/main/java/dev/bibikvlad/mastermind/chat/language/English.java
+++ src/main/java/dev/bibikvlad/mastermind/chat/language/English.java
@@ -1,4 +1,17 @@
 package dev.bibikvlad.mastermind.chat.language;
 
-public class English {
+import dev.bibikvlad.utils.StringConstants.ConsoleColors;
+
+public class English extends Language {
+    @Override
+    public String getInvalidInputMessage() {
+        return "Invalid guess. Must include only letters: "
+                + ConsoleColors.BrightForeground.RED + "r "
+                + ConsoleColors.BrightForeground.GREEN + "g "
+                + ConsoleColors.BrightForeground.YELLOW + "y "
+                + ConsoleColors.BrightForeground.BLUE + "b "
+                + ConsoleColors.BrightForeground.PURPLE + "p "
+                + ConsoleColors.BrightForeground.WHITE + "w"
+                + ConsoleColors.RESET;
+    }
 }
