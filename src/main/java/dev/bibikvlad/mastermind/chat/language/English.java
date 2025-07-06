1cee237b812343d4a9e4d6bc5bec9fd5c6698741
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-07 01:11:48 +0300

    Add getRulesMessage method

--- src/main/java/dev/bibikvlad/mastermind/chat/language/English.java
+++ src/main/java/dev/bibikvlad/mastermind/chat/language/English.java
@@ -2,6 +2,7 @@ package dev.bibikvlad.mastermind.chat.language;
 
 import dev.bibikvlad.mastermind.clues.InputVisualRepresentation;
 import dev.bibikvlad.utils.StringConstants.Emojis;
+import dev.bibikvlad.utils.StringConstants.GameCluesConstants;
 
 public class English extends Language {
     @Override
@@ -31,6 +32,24 @@ public class English extends Language {
 
     @Override
     public String getRulesMessage() {
-        return "";
+        return """
+                Puzzle contains 4 boxes. Each turn you choose from 6 colors.
+                Color choices: %s
+                Example turn: ybgr
+                Response:
+                %s : correct color in correct position
+                %s : correct color in incorrect position
+                %s : incorrect color
+                
+                The order of the response tiles does not necessarily match the colored characters.
+                Type 'help', or 'rules' to read these instructions again
+                Type 'close', or 'exit' to quit and show solution.
+                """
+                .formatted(
+                        getColorChoices(),
+                        GameCluesConstants.CIRCLE_SHADED,
+                        GameCluesConstants.CIRCLE_EMPTY,
+                        GameCluesConstants.UNDERSCORE
+                );
     }
 }
