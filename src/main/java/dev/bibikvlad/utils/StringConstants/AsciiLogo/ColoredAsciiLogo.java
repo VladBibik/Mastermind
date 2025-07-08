99e932400052f2bf83b4262d4bd4cdfb2d8edcec
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-08 18:39:15 +0300

    Refactor getLogo method Add 3 color formatting

--- src/main/java/dev/bibikvlad/utils/StringConstants/AsciiLogo/ColoredAsciiLogo.java
+++ src/main/java/dev/bibikvlad/utils/StringConstants/AsciiLogo/ColoredAsciiLogo.java
@@ -6,18 +6,19 @@ public class ColoredAsciiLogo {
     public static String getLogo() {
         return """
                 <violet>████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-                █▌<orange> ██████   ██████  █████████    █████████  ███████████ ██████████ ███████████  ██████   ██████ ███  █████  ░████  █████████  <violet>▐█
-                █▌<orange>░░██████ ██████  ███░░░░░███  ███░░░░░███░█░░░███░░░█░░███░░░░░█░░███░░░░░███ ░██████ ██████  ░░░ ░██████ ░░███ ░███░░░░███ <violet>▐█
-                █▌<orange> ░███░█████░███ ░███    ░███ ░███    ░░░ ░   ░███  ░  ░███  █ ░  ░███    ░███ ░███░█████░███ ░███ ░███░███ ░███ ░███   ░░███<violet>▐█
-                █▌<orange> ░███░░███ ░███ ░███████████ ░░█████████     ░███     ░██████    ░██████████  ░███░░███ ░███ ░███ ░███░░███░███ ░███    ░███<violet>▐█
-                █▌<orange> ░███ ░░░  ░███ ░███░░░░░███  ░░░░░░░░███    ░███     ░███░░█    ░███░░░░░███ ░███ ░░░  ░███ ░███ ░███ ░░██████ ░███    ░███<violet>▐█
-                █▌<orange> ░███      ░███ ░███    ░███  ███    ░███    ░███     ░███ ░   █ ░███    ░███ ░███      ░███ ░███ ░███  ░░█████ ░███    ███ <violet>▐█
-                █▌<orange> █████     ██████████   █████░░█████████     █████    ██████████ █████   ██████████     █████░███ ░████  ░░████ ░█████████  <violet>▐█
-                █▌<orange>░░░░░     ░░░░░░░░░░   ░░░░░  ░░░░░░░░░     ░░░░░    ░░░░░░░░░░ ░░░░░   ░░░░░░░░░░     ░░░░░░░░░░░░░░░    ░░░░░░░░░░░░░░░   <violet>▐█
+                █▌<orange> ██████   ██████  █████████    █████████  ███████████ ██████████ ███████████  ██████   ██████ ███  █████  <red>░<orange>████  █████████  <violet>▐█
+                █▌<red>░░<orange>██████ ██████  ███<red>░░░░░<orange>███  ███<red>░░░░░<orange>███<red>░<orange>█<red>░░░<orange>███<red>░░░<orange>█<red>░░<orange>███<red>░░░░░<orange>█<red>░░<orange>███<red>░░░░░<orange>███ <red>░<orange>██████ ██████  <red>░░░ ░<orange>██████ <red>░░<orange>███ <red>░<orange>███<red>░░░░<orange>███ <violet>▐█
+                █▌<red> ░<orange>███<red>░<orange>█████<red>░<orange>███ <red>░<orange>███    <red>░<orange>███ <red>░<orange>███    <red>░░░ ░   ░<orange>███  <red>░  ░<orange>███  █ <red>░  ░<orange>███    <red>░<orange>███ <red>░<orange>███<red>░<orange>█████<red>░<orange>███ <red>░<orange>███ <red>░<orange>███<red>░<orange>███ <red>░<orange>███ <red>░<orange>███   <red>░░<orange>███<violet>▐█
+                █▌<red> ░<orange>███<red>░░<orange>███ <red>░<orange>███ <red>░<orange>███████████ <red>░░<orange>█████████     <red>░<orange>███     <red>░<orange>██████    <red>░<orange>██████████  <red>░<orange>███<red>░░<orange>███ <red>░<orange>███ <red>░<orange>███ <red>░<orange>███<red>░░<orange>███<red>░<orange>███ <red>░<orange>███    <red>░<orange>███<violet>▐█
+                █▌<red> ░<orange>███ <red>░░░  ░<orange>███ <red>░<orange>███<red>░░░░░<orange>███  <red>░░░░░░░░<orange>███    <red>░<orange>███     <red>░<orange>███<red>░░<orange>█    <red>░<orange>███<red>░░░░░<orange>███ <red>░<orange>███ <red>░░░  <red>░<orange>███ <red>░<orange>███ <red>░<orange>███ <red>░░<orange>██████ <red>░<orange>███    <red>░<orange>███<violet>▐█
+                █▌<red> ░<orange>███      <red>░<orange>███ <red>░<orange>███    <red>░<orange>███  ███    <red>░<orange>███    <red>░<orange>███     <red>░<orange>███ <red>░   <orange>█ <red>░<orange>███    <red>░<orange>███ <red>░<orange>███      <red>░<orange>███ <red>░<orange>███ <red>░<orange>███  <red>░░<orange>█████ <red>░<orange>███    ███ <violet>▐█
+                █▌<orange> █████     ██████████   █████<red>░░<orange>█████████     █████    ██████████ █████   ██████████     █████<red>░<orange>███ <red>░<orange>████  <red>░░<orange>████ <red>░<orange>█████████  <violet>▐█
+                █▌<red>░░░░░     ░░░░░░░░░░   ░░░░░  ░░░░░░░░░     ░░░░░    ░░░░░░░░░░ ░░░░░   ░░░░░░░░░░     ░░░░░░░░░░░░░░░    ░░░░░░░░░░░░░░░   <violet>▐█
                 ████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████<reset>
                 """
                 .replace("<violet>", ConsoleColors.ExtendedAnsiForeground.VIOLET)
                 .replace("<orange>",  ConsoleColors.ExtendedAnsiForeground.ORANGE)
+                .replace("<red>", ConsoleColors.BrightForeground.RED)
                 .replace("<reset>", ConsoleColors.RESET);
     }
 }
