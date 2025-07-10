bb1a5855497d937c10d794e379be960fd6a57c5c
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-11 01:38:58 +0300

    Add logic to the LocaleManager class This class will provide a suited locale based on the passed LocaleType enum

--- src/main/java/dev/bibikvlad/mastermind/localization/manager/LocaleManager.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/manager/LocaleManager.java
@@ -1,7 +1,22 @@
 package dev.bibikvlad.mastermind.localization.manager;
 
+import dev.bibikvlad.mastermind.localization.configurations.LocaleType;
+import dev.bibikvlad.mastermind.localization.factory.EnglishLocaleFactory;
 import dev.bibikvlad.mastermind.localization.factory.LocaleAbstractFactory;
+import dev.bibikvlad.mastermind.localization.factory.RussianLocaleFactory;
+import dev.bibikvlad.mastermind.localization.messages.GameMessagesLocale;
 
 public class LocaleManager {
-    private LocaleAbstractFactory localeFactory;
+    private final LocaleAbstractFactory localeFactory;
+
+    public LocaleManager(LocaleType type) {
+        this.localeFactory = switch (type) {
+            case ENGLISH -> new EnglishLocaleFactory();
+            case RUSSIAN -> new RussianLocaleFactory();
+        };
+    }
+
+    public GameMessagesLocale getGameMessagesLocale() {
+        return localeFactory.getGameMessagesLocale();
+    }
 }
