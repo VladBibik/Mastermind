65182aa652de16e4305b378a1cb10df5ab486ae8
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-11 00:55:47 +0300

    Implement changes in LocaleSwitchAbstractFactory Add correct object creation, and get method logic

--- src/main/java/dev/bibikvlad/mastermind/chat/LocaleFabrics/EnglishLocaleFactory.java
+++ src/main/java/dev/bibikvlad/mastermind/chat/LocaleFabrics/EnglishLocaleFactory.java
@@ -1,4 +1,16 @@
 package dev.bibikvlad.mastermind.chat.LocaleFabrics;
 
+import dev.bibikvlad.mastermind.chat.GameMessages.EnglishGameLocale;
+import dev.bibikvlad.mastermind.chat.GameMessages.GameMessagesLocale;
+
 public class EnglishLocaleFactory extends LocaleSwitchAbstractFactory {
+
+    public EnglishLocaleFactory() {
+        this.gameMessagesLocale = new EnglishGameLocale();
+    }
+
+    @Override
+    public GameMessagesLocale getGameMessagesLocale() {
+        return gameMessagesLocale;
+    }
 }
