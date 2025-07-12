bf7e00270043046ed9626a939a5c80823ad6a5b9
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-12 15:08:19 +0300

    Refactor isInputValid method Add additional check for the string case

--- src/main/java/dev/bibikvlad/mastermind/validators/GameInputValidator.java
+++ src/main/java/dev/bibikvlad/mastermind/validators/GameInputValidator.java
@@ -6,6 +6,6 @@ public class GameInputValidator {
     private static final Pattern VALID_INPUT_PATTERN = Pattern.compile("[rgybpw]{4}");
 
     public static boolean isInputValid(String userInput) {
-        return VALID_INPUT_PATTERN.matcher(userInput).matches();
+        return VALID_INPUT_PATTERN.matcher(userInput.toLowerCase()).matches();
     }
 }
