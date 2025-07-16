62556e5325581214223ac37875f151358a843bb2
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-17 01:25:34 +0300

    Fix dependencies from the old language API to the new one in LocalManagerTest class

--- src/test/java/dev/bibikvlad/mastermind/localization/manager/LocaleManagerTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/manager/LocaleManagerTest.java
@@ -2,7 +2,7 @@ package dev.bibikvlad.mastermind.localization.manager;
 
 import dev.bibikvlad.mastermind.clues.InputVisualRepresentation;
 import dev.bibikvlad.mastermind.localization.configurations.LocaleType;
-import dev.bibikvlad.mastermind.localization.messages.GameMessagesLocale;
+import dev.bibikvlad.mastermind.localization.messages.GameMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +16,7 @@ public class LocaleManagerTest {
     void testEnglishLocaleType() {
         assertEquals("Game Over! The solution was: "
                         + InputVisualRepresentation.getVisualRepresentation(ANSWER),
-                getGameMessagesLocale(LocaleType.ENGLISH).getGameOverMessage(ANSWER));
+                getGameMessages(LocaleType.ENGLISH).getGameOverMessage(ANSWER));
     }
 
     @Test
@@ -24,12 +24,12 @@ public class LocaleManagerTest {
     void testRussianLocaleType() {
         assertEquals("Game Over! Ответом была комбинация: "
                         + InputVisualRepresentation.getVisualRepresentation(ANSWER),
-                getGameMessagesLocale(LocaleType.RUSSIAN).getGameOverMessage(ANSWER));
+                getGameMessages(LocaleType.RUSSIAN).getGameOverMessage(ANSWER));
     }
 
-    private GameMessagesLocale getGameMessagesLocale(LocaleType localeType) {
+    private GameMessages getGameMessages(LocaleType localeType) {
         LocaleManager localeManager = new LocaleManager(localeType);
 
-        return localeManager.getGameMessagesLocale();
+        return localeManager.getGameMessages();
     }
 }
