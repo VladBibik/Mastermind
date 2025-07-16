b757c9b64412bdd8b635efdd9893074114200902
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-17 01:16:00 +0300

    Refactor LocaleManager class Now instead of using old LocaleAbstractFactory it uses new GameMessages API

--- src/main/java/dev/bibikvlad/mastermind/localization/manager/LocaleManager.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/manager/LocaleManager.java
@@ -1,22 +1,26 @@
 package dev.bibikvlad.mastermind.localization.manager;
 
 import dev.bibikvlad.mastermind.localization.configurations.LocaleType;
-import dev.bibikvlad.mastermind.localization.factory.EnglishLocaleFactory;
-import dev.bibikvlad.mastermind.localization.factory.LocaleAbstractFactory;
-import dev.bibikvlad.mastermind.localization.factory.RussianLocaleFactory;
-import dev.bibikvlad.mastermind.localization.messages.GameMessagesLocale;
+import dev.bibikvlad.mastermind.localization.messages.ConsoleGameMessages;
+import dev.bibikvlad.mastermind.localization.messages.GameMessages;
+
+import java.util.ResourceBundle;
 
 public class LocaleManager {
-    private final LocaleAbstractFactory localeFactory;
+    private GameMessages gameMessages;
 
-    public LocaleManager(LocaleType type) {
-        this.localeFactory = switch (type) {
-            case ENGLISH -> new EnglishLocaleFactory();
-            case RUSSIAN -> new RussianLocaleFactory();
-        };
+    public LocaleManager(LocaleType localeType) {
+        getLocalisedGameMessages(localeType);
     }
 
-    public GameMessagesLocale getGameMessagesLocale() {
-        return localeFactory.getGameMessagesLocale();
+    public GameMessages getGameMessages() {
+        return gameMessages;
+    }
+
+    private void getLocalisedGameMessages(LocaleType localeType) {
+        this.gameMessages = switch (localeType) {
+            case ENGLISH -> new ConsoleGameMessages(ResourceBundle.getBundle("i18n.game_messages_en"));
+            case RUSSIAN -> new ConsoleGameMessages(ResourceBundle.getBundle("i18n.game_messages_ru"));
+        };
     }
 }
