6375fcb83ac9227f2e175bddd6dad13e076ec0c1
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-17 01:21:19 +0300

    Delete all old language API

--- src/main/java/dev/bibikvlad/mastermind/localization/factory/LocaleAbstractFactory.java
+++
@@ -1,9 +0,0 @@
-package dev.bibikvlad.mastermind.localization.factory;
-
-import dev.bibikvlad.mastermind.localization.messages.GameMessagesLocale;
-
-public abstract class LocaleAbstractFactory {
-    protected GameMessagesLocale gameMessagesLocale;
-
-    public abstract GameMessagesLocale getGameMessagesLocale();
-}
