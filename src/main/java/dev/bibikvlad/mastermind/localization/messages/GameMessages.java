b419615a0a0fbe966e8c74e1baf3a44ceca54d23
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-17 00:38:23 +0300

    Add GameMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/GameMessages.java
@@ -0,0 +1,19 @@
+package dev.bibikvlad.mastermind.localization.messages;
+
+import dev.bibikvlad.utils.strings.logos.ColoredAsciiLogo;
+
+public interface GameMessages {
+    String getInvalidInputMessage();
+
+    String getIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput);
+
+    String getGameOverMessage(String answer);
+
+    String getWinMessage(String answer);
+
+    String getRulesMessage();
+
+    default public String getAsciiLogo() {
+        return ColoredAsciiLogo.getLogo();
+    }
+}
