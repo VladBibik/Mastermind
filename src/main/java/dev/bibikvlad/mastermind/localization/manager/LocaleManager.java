0bf4f83d7d4970ed1c1e9e689ba2bfa01c6b25e3
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-17 01:20:07 +0300

    Refactor LocaleManager Now logic assignment much more clear

--- src/main/java/dev/bibikvlad/mastermind/localization/manager/LocaleManager.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/manager/LocaleManager.java
@@ -7,18 +7,18 @@ import dev.bibikvlad.mastermind.localization.messages.GameMessages;
 import java.util.ResourceBundle;
 
 public class LocaleManager {
-    private GameMessages gameMessages;
+    private final GameMessages gameMessages;
 
     public LocaleManager(LocaleType localeType) {
-        getLocalisedGameMessages(localeType);
+        this.gameMessages = getLocalisedGameMessages(localeType);
     }
 
     public GameMessages getGameMessages() {
         return gameMessages;
     }
 
-    private void getLocalisedGameMessages(LocaleType localeType) {
-        this.gameMessages = switch (localeType) {
+    private GameMessages getLocalisedGameMessages(LocaleType localeType) {
+        return switch (localeType) {
             case ENGLISH -> new ConsoleGameMessages(ResourceBundle.getBundle("i18n.game_messages_en"));
             case RUSSIAN -> new ConsoleGameMessages(ResourceBundle.getBundle("i18n.game_messages_ru"));
         };
