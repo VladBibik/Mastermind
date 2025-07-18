ae0351a9f71d5930fab7f5cdce22e84dbce69f21
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-18 22:11:18 +0300

    Add MessageBundle interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/manager/MessageBundle.java
@@ -0,0 +1,7 @@
+package dev.bibikvlad.mastermind.localization.manager;
+
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+
+public interface MessageBundle {
+    GameMessages getGameMessages();
+}
