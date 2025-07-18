691f417c87e37e63d51c97ddf3e2dc14ab211e46
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-19 01:28:27 +0300

    Refactor LocaleManager Class was completely refactored for the localization API 3.0

--- src/main/java/dev/bibikvlad/mastermind/localization/factory/LocaleManager.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/factory/LocaleManager.java
@@ -1,26 +1,20 @@
 package dev.bibikvlad.mastermind.localization.factory;
 
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
-import dev.bibikvlad.mastermind.localization.manager.ConsoleMessageBundle;
-import dev.bibikvlad.mastermind.localization.manager.MessageBundle;
-
-import java.util.ResourceBundle;
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 
 public class LocaleManager {
-    private final MessageBundle messageBundle;
+    private final MessageFactoryRegistry messageFactoryRegistry;
+    private final MessageProvider messageProvider;
 
     public LocaleManager(LocaleType localeType) {
-        this.messageBundle = getLocalizedMessageBundle(localeType);
+        this.messageFactoryRegistry = new MessageFactoryRegistry();
+        this.messageProvider = new MessageProvider(localeType, messageFactoryRegistry);
+
+        this.messageFactoryRegistry.register(GameMessages.class, new ConsoleGameMessageFactory());
     }
 
-    public MessageBundle getMessageBundle() {
-        return messageBundle;
-    }
-
-    private MessageBundle getLocalizedMessageBundle(LocaleType localeType) {
-        return switch (localeType) {
-            case ENGLISH -> new ConsoleMessageBundle(ResourceBundle.getBundle("i18n.game_messages_en"));
-            case RUSSIAN -> new ConsoleMessageBundle(ResourceBundle.getBundle("i18n.game_messages_ru"));
-        };
+    public GameMessages getGameMessages() {
+        return messageProvider.getMessages(GameMessages.class, "i18n.game_messages");
     }
 }
