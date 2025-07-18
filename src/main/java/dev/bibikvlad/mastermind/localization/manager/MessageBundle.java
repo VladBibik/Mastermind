56d1daba9203d29917104280ac3a36985e20fc6c
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-19 01:21:18 +0300

    Delete old localization API classes

--- src/main/java/dev/bibikvlad/mastermind/localization/manager/MessageBundle.java
+++
@@ -1,7 +0,0 @@
-package dev.bibikvlad.mastermind.localization.manager;
-
-import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
-
-public interface MessageBundle {
-    GameMessages getGameMessages();
-}
