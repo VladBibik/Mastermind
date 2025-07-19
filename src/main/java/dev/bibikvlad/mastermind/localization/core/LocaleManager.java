528ba290b3940d46729ad27dce144518d8267c79
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-19 16:37:06 +0300

    Refactor LocaleManager Move registry initialization logic to the custom class, and made message registry local variable inside the constructor

--- src/main/java/dev/bibikvlad/mastermind/localization/core/LocaleManager.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/LocaleManager.java
@@ -1,25 +1,19 @@
 package dev.bibikvlad.mastermind.localization.core;
 
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
-import dev.bibikvlad.mastermind.localization.factories.ConsoleGameMessageFactory;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 
 public class LocaleManager {
-    private final MessageFactoryRegistry messageFactoryRegistry;
     private final MessageProvider messageProvider;
 
     public LocaleManager(LocaleType localeType) {
-        this.messageFactoryRegistry = new MessageFactoryRegistry();
+        MessageFactoryRegistry messageFactoryRegistry = new MessageFactoryRegistry();
         this.messageProvider = new MessageProvider(localeType, messageFactoryRegistry);
 
-        writeRegistryEntryForEveryMessageType();
+        MessageRegistryPopulationManager.populate(messageFactoryRegistry);
     }
 
     public GameMessages getGameMessages() {
         return messageProvider.getMessages(GameMessages.class, "i18n.game_messages");
-    }
-
-    private void writeRegistryEntryForEveryMessageType() {
-        messageFactoryRegistry.register(GameMessages.class, new ConsoleGameMessageFactory());
     }
 }
