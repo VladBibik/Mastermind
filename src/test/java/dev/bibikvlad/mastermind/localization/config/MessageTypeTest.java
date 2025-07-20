cd28db6cd9b008bff628a3988454b295a2f71663
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-20 18:36:54 +0300

    Add negativeIndexThrowsException and invalidIndexThrowsException test methods

--- src/test/java/dev/bibikvlad/mastermind/localization/config/MessageTypeTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/config/MessageTypeTest.java
@@ -1,22 +1,38 @@
 package dev.bibikvlad.mastermind.localization.config;
 
-import dev.bibikvlad.mastermind.localization.messages.game.ConsoleGameMessages;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MessageTypeTest {
     @Test
     @DisplayName("Returns correct message type from the index")
     void returnsCorrectMessageTypeFromIndex() {
-        assertEquals(MessageType.GAME, MessageType.getByIndex(0));
+        assertEquals(MessageType.GAME, MessageType.fromIndex(0));
     }
 
     @Test
     @DisplayName("Returns correct message type from message class")
     void returnsCorrectMessageTypeFromMessageClass() {
-        assertEquals(MessageType.GAME, MessageType.getByMessageType(GameMessages.class));
+        assertEquals(MessageType.GAME, MessageType.fromMessageType(GameMessages.class));
+    }
+
+    @Test
+    @DisplayName("Throws exception on negative message type index")
+    void negativeIndexThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> MessageType.fromIndex(-1));
+    }
+
+    @Test
+    @DisplayName("Throws exception on invalid locale index")
+    void invalidIndexThrowsException() {
+        for (int i = 1; i < 100; i++) {
+            int index = i;
+
+            assertThrows(IllegalArgumentException.class, () -> MessageType.fromIndex(index));
+        }
     }
 }
