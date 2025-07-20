63b88489a0896ea8b2a8e61c57c99d9494f975e2
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-20 18:31:45 +0300

    Add returnsCorrectMessageTypeFromMessageClass method

--- src/test/java/dev/bibikvlad/mastermind/localization/config/MessageTypeTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/config/MessageTypeTest.java
@@ -1,5 +1,7 @@
 package dev.bibikvlad.mastermind.localization.config;
 
+import dev.bibikvlad.mastermind.localization.messages.game.ConsoleGameMessages;
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,11 @@ public class MessageTypeTest {
     @DisplayName("Returns correct message type from the index")
     void returnsCorrectMessageTypeFromIndex() {
         assertEquals(MessageType.GAME, MessageType.getByIndex(0));
+    }
+
+    @Test
+    @DisplayName("Returns correct message type from message class")
+    void returnsCorrectMessageTypeFromMessageClass() {
+        assertEquals(MessageType.GAME, MessageType.getByMessageType(GameMessages.class));
     }
 }
