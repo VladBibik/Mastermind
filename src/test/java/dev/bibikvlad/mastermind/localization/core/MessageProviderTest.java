5e15dfe94225daea57d7fc77d84b9d8152635587
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-26 16:21:39 +0300

    Rename getMessages_createsMessages_returnCorrectMessageType -> shouldReturnCorrectMessageTypeWhenRegistered

--- src/test/java/dev/bibikvlad/mastermind/localization/core/MessageProviderTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/core/MessageProviderTest.java
@@ -7,18 +7,19 @@ import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageProviderTest {
     @Test
     @DisplayName("Returns correct Message Type object from the registry")
-    public void getMessages_createsMessages_returnCorrectMessageType() {
+    public void shouldReturnCorrectMessageTypeWhenRegistered() {
         MessageProvider messageProvider =
                 new MessageProvider(LocaleType.ENGLISH, MessageRegistryInitializer.createAndPopulateRegistry());
         GameMessages providedMessages =
                 messageProvider.getMessages(GameMessages.class, MessageType.GAME.getResourceBundleName());
 
+        assertDoesNotThrow(
+                () -> messageProvider.getMessages(GameMessages.class, MessageType.GAME.getResourceBundleName()));
         assertNotNull(providedMessages);
     }
 
