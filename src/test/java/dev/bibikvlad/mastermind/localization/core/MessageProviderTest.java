a272975d2000143a2089ea7f72af5a50a26d15f6
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-26 03:11:40 +0300

    Add getMessages_failToCreateMessages_throwsIllegalStateException test method

--- src/test/java/dev/bibikvlad/mastermind/localization/core/MessageProviderTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/core/MessageProviderTest.java
@@ -2,11 +2,13 @@ package dev.bibikvlad.mastermind.localization.core;
 
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.config.MessageType;
+import dev.bibikvlad.mastermind.localization.messages.game.ConsoleGameMessages;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MessageProviderTest {
     @Test
@@ -17,6 +19,16 @@ public class MessageProviderTest {
         GameMessages providedMessages =
                 messageProvider.getMessages(GameMessages.class, MessageType.GAME.getResourceBundleName());
 
-        assertEquals(GameMessages.class, providedMessages.getClass());
+        assertNotNull(providedMessages);
+    }
+
+    @Test
+    @DisplayName("Throws exception on unregistered message type retrieval")
+    public void getMessages_failToCreateMessages_throwsIllegalStateException() {
+        MessageProvider messageProvider =
+                new MessageProvider(LocaleType.ENGLISH, MessageRegistryInitializer.createAndPopulateRegistry());
+
+        assertThrows(IllegalStateException.class,
+                () -> messageProvider.getMessages(ConsoleGameMessages.class, MessageType.GAME.getResourceBundleName()));
     }
 }
