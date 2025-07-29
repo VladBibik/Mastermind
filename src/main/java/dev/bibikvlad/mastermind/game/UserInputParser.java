e98d0dd276e934c9f3228ebd37b3cd7168d4de83
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-29 12:47:45 +0300

    Add unfinished UserInputParser class

---
+++ src/main/java/dev/bibikvlad/mastermind/game/UserInputParser.java
@@ -0,0 +1,16 @@
+package dev.bibikvlad.mastermind.game;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class UserInputParser {
+//    public static String parse(BufferedReader reader) {
+//        try {
+//
+//        } catch (IOException exception) {
+//            outputWriter.println(exception.getMessage());
+//        } finally {
+//            outputWriter.flush();
+//        }
+//    }
+}
