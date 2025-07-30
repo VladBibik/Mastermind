a84f6846b53da167f787b9db8d2a2e53a79ad1fd
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-30 19:25:21 +0300

    Refactor MastermindApplication main method to fit new Printer class creation logic

--- src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
+++ src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
@@ -2,10 +2,19 @@ package dev.bibikvlad.mastermind.app;
 
 import dev.bibikvlad.mastermind.game.MastermindConsoleGame;
 import dev.bibikvlad.mastermind.game.RandomAnswerGenerator;
+import dev.bibikvlad.mastermind.game.printer.ConsoleMastermindMessagePrinter;
+import dev.bibikvlad.mastermind.game.printer.MastermindMessagePrinter;
+import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 
 public class MastermindApplication {
     public static void main(String[] args) {
-        MastermindConsoleGame game = new MastermindConsoleGame(RandomAnswerGenerator.generate());
+        LocalizationContext localizationContext = new LocalizationContext(LocaleType.ENGLISH);
+        GameMessages gameMessages = localizationContext.getGameMessages();
+        MastermindMessagePrinter printer = new ConsoleMastermindMessagePrinter(gameMessages);
+
+        MastermindConsoleGame game = new MastermindConsoleGame(printer, RandomAnswerGenerator.generate());
 
         game.play();
     }
