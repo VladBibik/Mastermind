8c33f42654c472c9399c05eac38d0aee71cc939b
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-30 19:09:51 +0300

    Code cleanup

--- src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
+++ src/main/java/dev/bibikvlad/mastermind/app/MastermindApplication.java
@@ -2,9 +2,6 @@ package dev.bibikvlad.mastermind.app;
 
 import dev.bibikvlad.mastermind.game.MastermindConsoleGame;
 import dev.bibikvlad.mastermind.game.RandomAnswerGenerator;
-import dev.bibikvlad.mastermind.localization.config.LocaleType;
-import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
-import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 
 public class MastermindApplication {
     public static void main(String[] args) {
