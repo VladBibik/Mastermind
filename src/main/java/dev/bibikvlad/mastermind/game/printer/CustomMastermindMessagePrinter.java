0d75c6586bc267f33e92d9b7bfc8ceb60fb21484
BibikVlad <bibikvladislav.work@gmail.com>
2025-07-30 19:00:49 +0300

    Add CustomMastermindMessagePrinter class

---
+++ src/main/java/dev/bibikvlad/mastermind/game/printer/CustomMastermindMessagePrinter.java
@@ -0,0 +1,47 @@
+package dev.bibikvlad.mastermind.game.printer;
+
+import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+
+import java.io.PrintStream;
+
+public class CustomMastermindMessagePrinter implements MastermindMessagePrinter {
+    private final GameMessages gameMessages;
+    private final PrintStream printStream;
+
+    public CustomMastermindMessagePrinter(LocaleType localeType, PrintStream printStream) {
+        this.gameMessages = new LocalizationContext(localeType).getGameMessages();
+        this.printStream = printStream;
+    }
+
+    @Override
+    public void printInvalidInputMessage() {
+        printStream.println(gameMessages.getInvalidInputMessage());
+    }
+
+    @Override
+    public void printIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput) {
+        printStream.println(gameMessages.getIncorrectGuessMessage(maxTurns, currentTurn, answer, userInput));
+    }
+
+    @Override
+    public void printGameOverMessage(String answer) {
+        printStream.println(gameMessages.getGameOverMessage(answer));
+    }
+
+    @Override
+    public void printWinMessage(String answer) {
+        printStream.println(gameMessages.getWinMessage(answer));
+    }
+
+    @Override
+    public void printRulesMessage() {
+        printStream.println(gameMessages.getRulesMessage());
+    }
+
+    @Override
+    public void printAsciiLogo() {
+        printStream.println(gameMessages.getAsciiLogo());
+    }
+}
