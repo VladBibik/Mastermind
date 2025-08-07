b47c1989a6b81d027e0f85b77a4d0daa6a3c5e15
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-08 01:17:38 +0300

    Add GameStateManager class

---
+++ src/main/java/dev/bibikvlad/mastermind/game/GameStateManager.java
@@ -0,0 +1,32 @@
+package dev.bibikvlad.mastermind.game;
+
+public class GameStateManager {
+    private final int maxTurns;
+
+    private int currentTurn = 0;
+    private boolean gameClosed = false;
+
+    public GameStateManager(int maxTurns) {
+        this.maxTurns = maxTurns;
+    }
+
+    public boolean isOver() {
+        return currentTurn >= maxTurns;
+    }
+
+    public void nextTurn() {
+        currentTurn++;
+    }
+
+    public void close() {
+        gameClosed = true;
+    }
+
+    public boolean isGameClosed() {
+        return gameClosed;
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+}
