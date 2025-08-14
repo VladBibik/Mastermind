582cb344f92842d39890964642e5774023528cd2
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-14 21:42:38 +0300

    Refactor DatabaseContext class Add checkIfDBIsCreatedCreateIfNot method

--- src/main/java/dev/bibikvlad/mastermind/database/DatabaseContext.java
+++ src/main/java/dev/bibikvlad/mastermind/database/DatabaseContext.java
@@ -1,29 +1,20 @@
 package dev.bibikvlad.mastermind.database;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseContext {
     private static final String DB_URL = "jdbc:sqlite:Mastermind.db";
 
-    public static Statement getStatement() {
+    public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(DB_URL);
 
-            if (connection != null) {
-                String createTable = """
-                        CREATE TABLE IF NOT EXISTS users (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            username TEXT NOT NULL,
-                            creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                        );
-                        """;
+            checkIfDBIsCreatedCreateIfNot(connection);
 
-                try (Statement statement = connection.createStatement()) {
-                    statement.executeUpdate(createTable);
-                }
-
-                return connection;
-            }
+            return connection;
         } catch (SQLException exception) {
             System.out.println(exception.getMessage());
         }
@@ -31,17 +22,25 @@ public class DatabaseContext {
         throw new IllegalStateException("Something went wrong");
     }
 
-    public static PreparedStatement getPreparedStatement() {
-
+    private static void checkIfDBIsCreatedCreateIfNot(Connection connection) {
+        if (connection != null) {
+            String createTable = """
+                    CREATE TABLE IF NOT EXISTS users (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL,
+                        creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    );
+                    """;
+            try (Statement statement = connection.createStatement()) {
+                statement.executeUpdate(createTable);
+            } catch (SQLException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 
-    private boolean isDBCreated() {
-
+    class TEST {
+        public static void main(String[] args) {
+            Connection connection = DatabaseContext.getConnection();
+        }
     }
-}
-
-class TEST {
-    public static void main(String[] args) {
-        Connection connection = DatabaseContext.getConnection();
-    }
-}
