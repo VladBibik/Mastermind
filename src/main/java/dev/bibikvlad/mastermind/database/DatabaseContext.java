7be1a72ff3eaec75c9684fadf7eab0b4259a527f
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-15 19:16:49 +0300

    Change users -> players

--- src/main/java/dev/bibikvlad/mastermind/database/DatabaseContext.java
+++ src/main/java/dev/bibikvlad/mastermind/database/DatabaseContext.java
@@ -1,8 +1,6 @@
 package dev.bibikvlad.mastermind.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseContext {
     private static final String DB_URL = "jdbc:sqlite:Mastermind.db";
@@ -24,6 +22,34 @@ public class DatabaseContext {
     private static void checkIfDBIsCreatedCreateIfNot(Connection connection) throws SQLException {
         if (connection != null) {
             SchemaCreator.create(connection);
+        }
+    }
+}
+
+
+class TEST {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DatabaseContext.getConnection();
+
+//        PreparedStatement insertIntoPlayers =  connection.prepareStatement(
+//                "INSERT INTO players (player_name) VALUES (?)");
+//        insertIntoPlayers.setString(1, "Player1");
+//        insertIntoPlayers.executeUpdate();
+
+//        PreparedStatement insertIntoConfigs =   connection.prepareStatement(
+//                "INSERT INTO player_configurations (player_id, language) VALUES (?, ?)");
+//        insertIntoConfigs.setInt(1, 1);
+//        insertIntoConfigs.setString(2, "ENGLISH");
+//        insertIntoConfigs.executeUpdate();
+
+        PreparedStatement statement = connection.prepareStatement(
+                "SELECT * FROM players p" +
+                " LEFT JOIN player_configurations conf ON p.id = conf.player_id");
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("player_name") + " "
+                    + resultSet.getString("language"));
         }
     }
 }
