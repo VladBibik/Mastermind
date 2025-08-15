b2d990621a15cf792e286edfe079d893b73a55dc
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-15 18:07:12 +0300

    Add GameMenu class

---
+++ src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
@@ -0,0 +1,13 @@
+package dev.bibikvlad.mastermind.menu;
+
+import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
+
+public class GameMenu {
+    MastermindUserInputParser parser;
+
+    public GameMenu(MastermindUserInputParser parser) {
+        this.parser = parser;
+    }
+
+
+}
