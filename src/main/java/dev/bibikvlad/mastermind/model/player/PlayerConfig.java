dd993c75bf7bb12a85b95dd70dcbfdb9e660ce30
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-21 02:46:21 +0300

    Add PlayerConfig class

---
+++ src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
@@ -0,0 +1,29 @@
+package dev.bibikvlad.mastermind.model.player;
+
+import dev.bibikvlad.mastermind.localization.config.LocaleType;
+
+public class PlayerConfig {
+    int playerId;
+    LocaleType locale;
+
+    public PlayerConfig(int playerId, LocaleType locale) {
+        this.playerId = playerId;
+        this.locale = locale;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public LocaleType getLocale() {
+        return locale;
+    }
+
+    public void setLocale(LocaleType locale) {
+        this.locale = locale;
+    }
+}
