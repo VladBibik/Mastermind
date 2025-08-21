274f0fe49c4ff67507e14f26a4801544055e7f5b
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-22 00:39:48 +0300

    Add PlayerNotFoundException

---
+++ src/main/java/dev/bibikvlad/mastermind/exceptions/PlayerNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.bibikvlad.mastermind.exceptions;
+
+public class PlayerNotFoundException extends Exception {
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+}
