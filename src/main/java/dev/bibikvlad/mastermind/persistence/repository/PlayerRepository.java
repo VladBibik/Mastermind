4f1cfa98735bd3c09a87f451e73a40b06d31e905
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-21 03:09:53 +0300

    Add repository methods

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerRepository.java
@@ -1,4 +1,24 @@
 package dev.bibikvlad.mastermind.persistence.repository;
 
+import dev.bibikvlad.mastermind.model.player.Player;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PlayerRepository {
+    List<Player>  findAll();
+
+    Optional<Player> findById(int id);
+
+    Optional<Player> findByName(String name);
+
+    void save(Player player);
+
+    void delete(Player player);
+
+    void deleteById(int id);
+
+    void deleteByName(String name);
+
+    void update(Player oldPlayer, Player newPlayer);
 }
