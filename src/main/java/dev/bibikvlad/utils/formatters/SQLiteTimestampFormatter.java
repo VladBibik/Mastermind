46cfb0b6d08869a8c1c0828c36fa3193a2c91c62
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-23 00:16:28 +0300

    Add temp changes to the class

--- src/main/java/dev/bibikvlad/utils/formatters/SQLiteTimestampFormatter.java
+++ src/main/java/dev/bibikvlad/utils/formatters/SQLiteTimestampFormatter.java
@@ -2,10 +2,18 @@ package dev.bibikvlad.utils.formatters;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class SQLiteTimestampFormatter {
-    public static LocalDateTime format(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return LocalDateTime.parse(date, formatter);
+    //TODO: Need a refactor!!! Just temp class to be able to see normal result!
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.US);
+
+    public static LocalDateTime parse(String date) {
+        return LocalDateTime.parse(date, FORMATTER);
+    }
+
+    public static String format(LocalDateTime date) {
+        return FORMATTER.format(date);
     }
 }
