729304db5326f82488ca2dc099ffae10668396be
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-24 03:30:19 +0300

    Refactor Player class Add id field Refactor constructors Remove setters

--- src/main/java/dev/bibikvlad/mastermind/model/player/Player.java
+++ src/main/java/dev/bibikvlad/mastermind/model/player/Player.java
@@ -5,50 +5,44 @@ import dev.bibikvlad.utils.formatters.SQLiteTimestampFormatter;
 import java.time.LocalDateTime;
 
 public class Player {
-    private String playerName;
-    private LocalDateTime creationDate;
-    private PlayerConfig playerConfig;
+    private Integer id;
+    private final String playerName;
+    private final LocalDateTime creationDate;
+    private final PlayerConfig playerConfig;
 
-    //TODO: Analyse if there is a better way to fix constructor issue
     public Player(String playerName, PlayerConfig playerConfig) {
         this.playerName = playerName;
+        this.creationDate = LocalDateTime.now();
         this.playerConfig = playerConfig;
     }
 
-    public Player(String playerName, LocalDateTime creationDate, PlayerConfig playerConfig) {
+    public Player(Integer id, String playerName, LocalDateTime creationDate, PlayerConfig playerConfig) {
+        this.id = id;
         this.playerName = playerName;
         this.creationDate = creationDate;
         this.playerConfig = playerConfig;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getPlayerName() {
         return playerName;
     }
 
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-    }
-
     public LocalDateTime getCreationDate() {
         return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
     }
 
     public PlayerConfig getPlayerConfig() {
         return playerConfig;
     }
 
-    public void setPlayerConfig(PlayerConfig playerConfig) {
-        this.playerConfig = playerConfig;
-    }
-
     @Override
     public String toString() {
         return "Player: \n"
-                + "Player ID: " + playerConfig.playerId
+                + "Player ID: " + id
                 + ", Player Name = " + playerName
                 + ", Creation Date = " + SQLiteTimestampFormatter.format(creationDate)
                 + "\n    configurations:"
