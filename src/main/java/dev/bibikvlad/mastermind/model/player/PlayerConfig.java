02972a24947697f3b83500b90cf40fc9af84de19
BibikVlad <bibikvladislav.work@gmail.com>
2025-08-24 03:29:36 +0300

    Refactor PlayerConfig model class Remove playerId field and setters

--- src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
+++ src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
@@ -3,32 +3,13 @@ package dev.bibikvlad.mastermind.model.player;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 
 public class PlayerConfig {
-    int playerId;
-    LocaleType locale;
+    private final LocaleType locale;
 
-    //TODO: Analyse if there is better way to fix issue with constructors
     public PlayerConfig(LocaleType locale) {
         this.locale = locale;
     }
 
-    public PlayerConfig(int playerId, LocaleType locale) {
-        this.playerId = playerId;
-        this.locale = locale;
-    }
-
-    public int getPlayerId() {
-        return playerId;
-    }
-
-    public void setPlayerId(int playerId) {
-        this.playerId = playerId;
-    }
-
     public LocaleType getLocale() {
         return locale;
-    }
-
-    public void setLocale(LocaleType locale) {
-        this.locale = locale;
     }
 }
