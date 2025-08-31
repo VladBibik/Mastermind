b09ecc8a44aa2a263627d8b5ec67e21cb5533795
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-01 01:56:44 +0300

    Refactor PlayerConfig class Add Logo color fields, constructor parameters, and getters

--- src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
+++ src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
@@ -1,15 +1,40 @@
 package dev.bibikvlad.mastermind.model.player;
 
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
 
 public class PlayerConfig {
     private final LocaleType locale;
+    private final ConsoleColor borderColor;
+    private final ConsoleColor mainColor;
+    private final ConsoleColor accentColor;
+    private final ConsoleColor backgroundColor;
 
-    public PlayerConfig(LocaleType locale) {
+    public PlayerConfig(LocaleType locale, ConsoleColor borderColor, ConsoleColor mainColor, ConsoleColor accentColor, ConsoleColor backgroundColor) {
         this.locale = locale;
+        this.borderColor = borderColor;
+        this.mainColor = mainColor;
+        this.accentColor = accentColor;
+        this.backgroundColor = backgroundColor;
     }
 
     public LocaleType getLocale() {
         return locale;
+    }
+
+    public ConsoleColor getBorderColor() {
+        return borderColor;
+    }
+
+    public ConsoleColor getMainColor() {
+        return mainColor;
+    }
+
+    public ConsoleColor getAccentColor() {
+        return accentColor;
+    }
+
+    public ConsoleColor getBackgroundColor() {
+        return backgroundColor;
     }
 }
