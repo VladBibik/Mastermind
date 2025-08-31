f1bfdf5219bd1943881cf45bf933a8d05d5b603c
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-01 02:26:50 +0300

    Refactor map method Now it correctly maps all rows including newly introduced once

--- src/main/java/dev/bibikvlad/mastermind/model/mappers/PlayerMapper.java
+++ src/main/java/dev/bibikvlad/mastermind/model/mappers/PlayerMapper.java
@@ -1,6 +1,5 @@
 package dev.bibikvlad.mastermind.model.mappers;
 
-import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.model.player.PlayerConfig;
 import dev.bibikvlad.utils.formatters.SQLiteTimestampFormatter;
@@ -10,8 +9,7 @@ import java.sql.SQLException;
 
 public class PlayerMapper {
     public static Player map(ResultSet resultSet) throws SQLException {
-        PlayerConfig playerConfig = new PlayerConfig(LocaleType.fromLanguageString(
-                resultSet.getString("language")));
+        PlayerConfig playerConfig = PlayerConfigMapper.map(resultSet);
 
         return new Player(
                 resultSet.getLong("id"),
