0ca198e69c95cffb5d613c5a76f43428f8b0cd5c
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-10 04:07:36 +0300

    Add JdbcPlayerConfigDAO class

---
+++ src/main/java/dev/bibikvlad/mastermind/persistence/dao/JdbcPlayerConfigDAO.java
@@ -0,0 +1,35 @@
+package dev.bibikvlad.mastermind.persistence.dao;
+
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
+import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.model.player.Player;
+import dev.bibikvlad.mastermind.model.player.PlayerConfig;
+
+import java.util.Optional;
+
+public class JdbcPlayerConfigDAO implements PlayerConfigDAO {
+    @Override
+    public Optional<PlayerConfig> findById(int id) throws PersistenceException {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean save(PlayerConfig playerConfig) throws PersistenceException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(PlayerConfig playerConfig) throws PersistenceException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Player player) throws PersistenceException {
+        return false;
+    }
+
+    @Override
+    public boolean updateLocale(int playerId, LocaleType locale) throws PersistenceException {
+        return false;
+    }
+}
