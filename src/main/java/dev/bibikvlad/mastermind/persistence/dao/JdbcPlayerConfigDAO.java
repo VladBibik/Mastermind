83954196f9735d7875282c99b0eaa6b81935b7bf
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-11 03:42:46 +0300

    Add logic to the findById method

--- src/main/java/dev/bibikvlad/mastermind/persistence/dao/JdbcPlayerConfigDAO.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/dao/JdbcPlayerConfigDAO.java
@@ -2,10 +2,14 @@ package dev.bibikvlad.mastermind.persistence.dao;
 
 import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.model.mappers.PlayerConfigMapper;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.model.player.PlayerConfig;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class JdbcPlayerConfigDAO implements PlayerConfigDAO {
@@ -17,7 +21,25 @@ public class JdbcPlayerConfigDAO implements PlayerConfigDAO {
 
     @Override
     public Optional<PlayerConfig> findById(int id) throws PersistenceException {
-        return Optional.empty();
+        String fetchPlayerConfigQuery = """
+                SELECT player_id, language, logo_border_color, logo_main_color, logo_accent_color, logo_background_color
+                FROM player_configurations
+                WHERE player_id = ?;
+                """;
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(fetchPlayerConfigQuery)) {
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return Optional.of(PlayerConfigMapper.map(resultSet));
+            } else {
+                return Optional.empty();
+            }
+        } catch (SQLException exception) {
+            throw new PersistenceException("Failed to fetch player configuration by id: " + id, exception);
+        }
     }
 
     @Override
