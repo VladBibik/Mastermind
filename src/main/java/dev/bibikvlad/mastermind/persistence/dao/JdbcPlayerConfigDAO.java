a15d0ce6ac2fe952d01b2d283e0ab3aa1d93ee9e
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-12 02:30:35 +0300

    Add logic to the save method

--- src/main/java/dev/bibikvlad/mastermind/persistence/dao/JdbcPlayerConfigDAO.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/dao/JdbcPlayerConfigDAO.java
@@ -42,9 +42,26 @@ public class JdbcPlayerConfigDAO implements PlayerConfigDAO {
         }
     }
 
+    //TODO: Does it even needed?
     @Override
     public boolean save(long playerId, PlayerConfig playerConfig) throws PersistenceException {
-        return false;
+        String addPlayerConfigQuery = "INSERT INTO player_configurations (player_id, language, logo_border_color,  " +
+                "logo_main_color, logo_accent_color, logo_background_color) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(addPlayerConfigQuery)) {
+            preparedStatement.setLong(1, playerId);
+            preparedStatement.setString(2, playerConfig.getLocale().getLanguageName());
+            preparedStatement.setString(3, playerConfig.getLogoBorderColor().getDisplayName());
+            preparedStatement.setString(4, playerConfig.getLogoMainColor().getDisplayName());
+            preparedStatement.setString(5, playerConfig.getLogoAccentColor().getDisplayName());
+            preparedStatement.setString(6, playerConfig.getLogoBackgroundColor().getDisplayName());
+            preparedStatement.executeUpdate();
+        } catch (SQLException exception) {
+            throw new PersistenceException("Failed to save a Configurations for the player with ID: "
+                    + playerId, exception);
+        }
+
+        return true;
     }
 
     @Override
