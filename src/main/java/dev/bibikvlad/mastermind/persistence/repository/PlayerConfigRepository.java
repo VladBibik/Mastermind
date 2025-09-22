048302306248cbe85ed001682126c385ef709171
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-23 00:40:35 +0300

    Add TransactionManager field and constructor initialization

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerConfigRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerConfigRepository.java
@@ -1,4 +1,14 @@
 package dev.bibikvlad.mastermind.persistence.repository;
 
+import dev.bibikvlad.mastermind.persistence.dao.PlayerDAO;
+import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
+
 public class PlayerConfigRepository {
+    private final PlayerDAO playerDAO;
+    private final TransactionManager transactionManager;
+
+    public PlayerConfigRepository(PlayerDAO playerDAO, TransactionManager transactionManager) {
+        this.playerDAO = playerDAO;
+        this.transactionManager = transactionManager;
+    }
 }
