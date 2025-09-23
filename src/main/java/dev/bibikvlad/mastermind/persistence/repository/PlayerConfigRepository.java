9403739fcf14b945e4b704d9ca57d777072b3a99
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-23 22:53:40 +0300

    Add save method

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerConfigRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerConfigRepository.java
@@ -1,5 +1,7 @@
 package dev.bibikvlad.mastermind.persistence.repository;
 
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
+import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.persistence.dao.PlayerDAO;
 import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
 
@@ -10,5 +12,28 @@ public class PlayerConfigRepository {
     public PlayerConfigRepository(PlayerDAO playerDAO, TransactionManager transactionManager) {
         this.playerDAO = playerDAO;
         this.transactionManager = transactionManager;
+    }
+
+    public boolean save(Player player) throws PersistenceException {
+        boolean result = false;
+
+        try {
+            transactionManager.begin();
+
+            result = playerDAO.save(player);
+
+            transactionManager.commit();
+        } catch (PersistenceException exception) {
+            try {
+                transactionManager.rollback();
+            } catch (PersistenceException rollbackException) {
+                //TODO: Add exception handling!
+                exception.addSuppressed(rollbackException);
+            }
+
+            throw exception;
+        }
+
+        return result;
     }
 }
