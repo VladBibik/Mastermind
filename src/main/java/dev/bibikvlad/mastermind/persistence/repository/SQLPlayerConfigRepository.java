36fa89612a031c0995f074049bb6b1e94576e133
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-26 00:08:32 +0300

    Rename PlayerConfigRepository -> SQLPlayerConfigRepository

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQLPlayerConfigRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQLPlayerConfigRepository.java
@@ -8,11 +8,11 @@ import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
 import java.util.List;
 import java.util.Optional;
 
-public class PlayerConfigRepository {
+public class SQLPlayerConfigRepository {
     private final PlayerDAO playerDAO;
     private final TransactionManager transactionManager;
 
-    public PlayerConfigRepository(PlayerDAO playerDAO, TransactionManager transactionManager) {
+    public SQLPlayerConfigRepository(PlayerDAO playerDAO, TransactionManager transactionManager) {
         this.playerDAO = playerDAO;
         this.transactionManager = transactionManager;
     }
