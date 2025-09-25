a900e395c37f1b82c4b7908472cb75055e483723
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-26 00:11:18 +0300

    Add PlayerRepository interface

---
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/PlayerRepository.java
@@ -0,0 +1,29 @@
+package dev.bibikvlad.mastermind.persistence.repository;
+
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
+import dev.bibikvlad.mastermind.model.player.Player;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlayerRepository {
+    List<Player> findAll() throws PersistenceException;
+
+    Optional<Player> findById(long id) throws PersistenceException;
+
+    Optional<Player> findByName(String name) throws PersistenceException;
+
+    void delete(Player player) throws PersistenceException;
+
+    void deleteById(long id) throws PersistenceException;
+
+    void deleteByName(String name) throws PersistenceException;
+
+    boolean save(Player player) throws PersistenceException;
+
+    boolean update(Player player) throws PersistenceException;
+
+    boolean existById(long id) throws PersistenceException;
+
+    boolean existsByName(String name) throws PersistenceException;
+}
