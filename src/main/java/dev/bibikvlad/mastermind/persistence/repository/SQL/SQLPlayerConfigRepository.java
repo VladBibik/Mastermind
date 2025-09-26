e0256c104c28b7430d41aa02afb4e9f1dda6e7d5
BibikVlad <bibikvladislav.work@gmail.com>
2025-09-26 20:51:48 +0300

    Add findById method

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/SQLPlayerConfigRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/SQLPlayerConfigRepository.java
@@ -1,12 +1,19 @@
 package dev.bibikvlad.mastermind.persistence.repository.SQL;
 
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.model.player.PlayerConfig;
 import dev.bibikvlad.mastermind.persistence.dao.PlayerConfigDAO;
+
+import java.util.Optional;
 
 public class SQLPlayerConfigRepository {
     private final PlayerConfigDAO playerConfigDAO;
 
     public SQLPlayerConfigRepository(PlayerConfigDAO playerConfigDAO) {
         this.playerConfigDAO = playerConfigDAO;
+    }
+
+    public Optional<PlayerConfig> findById(long playerId) throws PersistenceException {
+        return playerConfigDAO.findById(playerId);
     }
 }
