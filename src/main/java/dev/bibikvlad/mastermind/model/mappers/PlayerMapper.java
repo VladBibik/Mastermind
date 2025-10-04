ade4ce9a5147d9051682c45ea42c99461c5fac90
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-05 00:51:16 +0300

    Refactor map method Now it wraps SQLException in PersistenceException so that SQL logic wouldn't leak into the Repository layer

--- src/main/java/dev/bibikvlad/mastermind/model/mappers/PlayerMapper.java
+++ src/main/java/dev/bibikvlad/mastermind/model/mappers/PlayerMapper.java
@@ -1,5 +1,6 @@
 package dev.bibikvlad.mastermind.model.mappers;
 
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.model.player.PlayerConfig;
 import dev.bibikvlad.utils.formatters.SQLiteTimestampFormatter;
@@ -8,14 +9,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PlayerMapper {
-    public static Player map(ResultSet resultSet) throws SQLException {
-        PlayerConfig playerConfig = PlayerConfigMapper.map(resultSet);
+    public static Player map(ResultSet resultSet) throws PersistenceException {
+        Player mappedPlayer;
 
-        return new Player(
-                resultSet.getLong("id"),
-                resultSet.getString("player_name"),
-                SQLiteTimestampFormatter.parse(resultSet.getString("creation_date")),
-                playerConfig
-        );
+        try {
+            PlayerConfig playerConfig = PlayerConfigMapper.map(resultSet);
+
+            mappedPlayer = new Player(
+                    resultSet.getLong("id"),
+                    resultSet.getString("player_name"),
+                    SQLiteTimestampFormatter.parse(resultSet.getString("creation_date")),
+                    playerConfig
+            );
+        } catch (SQLException exception) {
+            throw new PersistenceException("Failed to parse a player. Please try again later", exception);
+        }
+
+        return mappedPlayer;
     }
 }
