05b62fcc935d5ba569f71aca4c21268460033a76
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-11 01:30:22 +0300

    Add access modifier to the parser field

--- src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
@@ -3,7 +3,7 @@ package dev.bibikvlad.mastermind.menu;
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 
 public class GameMenu {
-    MastermindUserInputParser parser;
+    private final MastermindUserInputParser parser;
 
     public GameMenu(MastermindUserInputParser parser) {
         this.parser = parser;
