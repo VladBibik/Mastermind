515c8e07fac691c47976fb3ee0050257639be9b6
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-14 03:00:24 +0300

    Add new field PlayerService and its initialization in the constructor

--- src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
@@ -1,13 +1,14 @@
 package dev.bibikvlad.mastermind.menu;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class GameMenu {
     private final MastermindUserInputParser parser;
+    private final PlayerService playerService;
 
-    public GameMenu(MastermindUserInputParser parser) {
+    public GameMenu(MastermindUserInputParser parser, PlayerService playerService) {
         this.parser = parser;
+        this.playerService = playerService;
     }
-
-    
 }
