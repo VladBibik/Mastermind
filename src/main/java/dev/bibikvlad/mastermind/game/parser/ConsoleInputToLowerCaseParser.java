71abcb3208fdca4643207464b8c88850699f22ef
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-14 04:14:17 +0300

    Add TODO

--- src/main/java/dev/bibikvlad/mastermind/game/parser/ConsoleInputToLowerCaseParser.java
+++ src/main/java/dev/bibikvlad/mastermind/game/parser/ConsoleInputToLowerCaseParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//TODO: Think of a better name, or different approach to this one and ConsoleInputParser!
 public class ConsoleInputToLowerCaseParser implements MastermindUserInputParser {
     private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
