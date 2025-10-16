2a086b05c62dba1bd9bfeeb9919c294624f9f565
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-17 02:11:10 +0300

    Add fields and constructor initialization

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/PlayerLastSelectedSQLRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/PlayerLastSelectedSQLRepository.java
@@ -2,9 +2,19 @@ package dev.bibikvlad.mastermind.persistence.repository.SQL;
 
 import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.model.player.Player;
+import dev.bibikvlad.mastermind.persistence.dao.PlayerLastSelectedDAO;
+import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
 import dev.bibikvlad.mastermind.persistence.repository.PlayerLastSelectedRepository;
 
 public class PlayerLastSelectedSQLRepository implements PlayerLastSelectedRepository {
+    private final PlayerLastSelectedDAO playerLastSelectedDAO;
+    private final TransactionManager transactionManager;
+
+    public PlayerLastSelectedSQLRepository(PlayerLastSelectedDAO playerLastSelectedDAO, TransactionManager transactionManager) {
+        this.playerLastSelectedDAO = playerLastSelectedDAO;
+        this.transactionManager = transactionManager;
+    }
+
     @Override
     public boolean saveOrUpdate(long id) throws PersistenceException {
         return false;
