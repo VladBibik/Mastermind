07f31a59c8fb878c3c284eb449e57d8f863c556b
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-17 04:32:20 +0300

    Add sout that represents another menu option

--- src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/GameMenu.java
@@ -54,6 +54,7 @@ public class GameMenu {
         System.out.println("To play a new Game press: 2");
         System.out.println("To view all Players press: 3");
         System.out.println("To view again menu options press: 4");
+        System.out.println("To view current player's data press: 5");
         System.out.println("To close the game print: 'close', or 'exit'");
     }
 
