6a201f6125f84c1a2d09a19e71da372904830d68
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-18 02:09:59 +0300

    Remove all logo color fields and replace them with bundle class Remove all old getters Add 1 new one

--- src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
+++ src/main/java/dev/bibikvlad/mastermind/model/player/PlayerConfig.java
@@ -1,41 +1,22 @@
 package dev.bibikvlad.mastermind.model.player;
 
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
-import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
+import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 
 public class PlayerConfig {
     private final LocaleType locale;
-    private final ConsoleColor logoBorderColor;
-    private final ConsoleColor logoMainColor;
-    private final ConsoleColor logoAccentColor;
-    private final ConsoleColor logoBackgroundColor;
+    private final LogoColorsBundle logoColorsBundle;
 
-    public PlayerConfig(LocaleType locale, ConsoleColor logoBorderColor, ConsoleColor logoMainColor,
-                        ConsoleColor logoAccentColor, ConsoleColor logoBackgroundColor) {
+    public PlayerConfig(LocaleType locale, LogoColorsBundle logoColorsBundle) {
         this.locale = locale;
-        this.logoBorderColor = logoBorderColor;
-        this.logoMainColor = logoMainColor;
-        this.logoAccentColor = logoAccentColor;
-        this.logoBackgroundColor = logoBackgroundColor;
+        this.logoColorsBundle = logoColorsBundle;
     }
 
     public LocaleType getLocale() {
         return locale;
     }
 
-    public ConsoleColor getLogoBorderColor() {
-        return logoBorderColor;
-    }
-
-    public ConsoleColor getLogoMainColor() {
-        return logoMainColor;
-    }
-
-    public ConsoleColor getLogoAccentColor() {
-        return logoAccentColor;
-    }
-
-    public ConsoleColor getLogoBackgroundColor() {
-        return logoBackgroundColor;
+    public LogoColorsBundle getLogoColorsBundle() {
+        return logoColorsBundle;
     }
 }
