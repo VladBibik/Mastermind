f17b1186d7ab62fa08e8efb31c3caebba634da19
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-18 02:10:27 +0300

    Fix mapper to use LogoColorBundle

--- src/main/java/dev/bibikvlad/mastermind/model/mappers/PlayerConfigMapper.java
+++ src/main/java/dev/bibikvlad/mastermind/model/mappers/PlayerConfigMapper.java
@@ -2,6 +2,7 @@ package dev.bibikvlad.mastermind.model.mappers;
 
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
+import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 import dev.bibikvlad.mastermind.model.player.PlayerConfig;
 
 import java.sql.ResultSet;
@@ -11,10 +12,11 @@ public class PlayerConfigMapper {
     public static PlayerConfig map(ResultSet resultSet) throws SQLException {
         return new PlayerConfig(LocaleType.fromLanguageString(
                 resultSet.getString("language")),
-                ConsoleColor.getByDisplayName(resultSet.getString("logo_border_color")),
-                ConsoleColor.getByDisplayName(resultSet.getString("logo_main_color")),
-                ConsoleColor.getByDisplayName(resultSet.getString("logo_accent_color")),
-                ConsoleColor.getByDisplayName(resultSet.getString("logo_background_color"))
+                new LogoColorsBundle(
+                        ConsoleColor.getByDisplayName(resultSet.getString("logo_border_color")),
+                        ConsoleColor.getByDisplayName(resultSet.getString("logo_main_color")),
+                        ConsoleColor.getByDisplayName(resultSet.getString("logo_accent_color")),
+                        ConsoleColor.getByDisplayName(resultSet.getString("logo_background_color")))
         );
     }
 }
