b937d348f0bee256734029519257c658bdcc118b
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-18 03:43:54 +0300

    Refactor MastermindGameLauncher class Now instead of the main method, and point of entry into the game it's a facade class that takes two parameters LocaleType and LogoColorsBundle, creates all needed params for the console version of the game to be launched, generates answer for the game, and creates MastermindConsoleGame

--- src/main/java/dev/bibikvlad/mastermind/app/MastermindGameLauncher.java
+++ src/main/java/dev/bibikvlad/mastermind/app/MastermindGameLauncher.java
@@ -9,15 +9,25 @@ import dev.bibikvlad.mastermind.game.printer.MastermindMessagePrinter;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 
 public class MastermindGameLauncher {
-    public static void main(String[] args) {
-        LocalizationContext localizationContext = new LocalizationContext(LocaleType.ENGLISH);
+    private final LocaleType localeType;
+    private final LogoColorsBundle logoColorsBundle;
+
+    public MastermindGameLauncher(LocaleType localeType, LogoColorsBundle logoColorsBundle) {
+        this.localeType = localeType;
+        this.logoColorsBundle = logoColorsBundle;
+    }
+
+    public void launch() {
+        LocalizationContext localizationContext = new LocalizationContext(localeType);
         GameMessages gameMessages = localizationContext.getGameMessages();
         MastermindMessagePrinter printer = new ConsoleMessagePrinter(gameMessages);
         MastermindUserInputParser parser = new ConsoleInputToLowerCaseParser();
 
-        MastermindConsoleGame game = new MastermindConsoleGame(printer, parser, RandomAnswerGenerator.generate());
+        MastermindConsoleGame game = new MastermindConsoleGame(printer, parser,
+                RandomAnswerGenerator.generate(), logoColorsBundle);
 
         game.play();
     }
