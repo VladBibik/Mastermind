5cbbda45b1eaa93d46f75dcc33f3fd3559ecbdeb
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-19 03:55:29 +0300

    Refactor launch method Now it parses language first

--- src/main/java/dev/bibikvlad/mastermind/menu/FirstTimeLaunch.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/FirstTimeLaunch.java
@@ -1,12 +1,17 @@
 package dev.bibikvlad.mastermind.menu;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class FirstTimeLaunch {
     public static void launch(MastermindUserInputParser parser, PlayerService playerService) {
         System.out.println("Welcome to the Mastermind Game!");
 
-        NewPlayerCreation.create(parser, playerService);
+        LanguageSelectionMenu languageSelectionMenu = new LanguageSelectionMenu(parser);
+
+        LocaleType localeType = languageSelectionMenu.selectLanguage();
+
+        NewPlayerCreation.createWithLocale(parser, playerService, localeType);
     }
 }
