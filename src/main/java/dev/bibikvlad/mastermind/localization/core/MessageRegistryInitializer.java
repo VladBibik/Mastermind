5732b6bd7bfb4125266a3aab703f557953bcb67f
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-20 02:36:04 +0300

    Add registry entry for LogoMessages in the MessageFactoryRegistry

--- src/main/java/dev/bibikvlad/mastermind/localization/core/MessageRegistryInitializer.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/MessageRegistryInitializer.java
@@ -1,13 +1,16 @@
 package dev.bibikvlad.mastermind.localization.core;
 
 import dev.bibikvlad.mastermind.localization.factories.ConsoleGameMessageFactory;
+import dev.bibikvlad.mastermind.localization.factories.ConsoleLogoMessagesFactory;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
 
 public class MessageRegistryInitializer {
     public static MessageFactoryRegistry createAndPopulateRegistry() {
         MessageFactoryRegistry messageFactoryRegistry = new MessageFactoryRegistry();
 
         messageFactoryRegistry.register(GameMessages.class, new ConsoleGameMessageFactory());
+        messageFactoryRegistry.register(LogoMessages.class, new ConsoleLogoMessagesFactory());
 
         return messageFactoryRegistry;
     }
