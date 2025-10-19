5fcc412105da6f9d0079f8d29096fc24ae5d0e10
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-19 03:18:32 +0300

    Add .trim method call Now it works as intended returning true if provided string has only whitespaces

--- src/main/java/dev/bibikvlad/mastermind/validators/StringEmptyValidator.java
+++ src/main/java/dev/bibikvlad/mastermind/validators/StringEmptyValidator.java
@@ -2,6 +2,6 @@ package dev.bibikvlad.mastermind.validators;
 
 public class StringEmptyValidator {
     public static boolean isNullOrEmpty(String userInput) {
-        return userInput == null || userInput.isEmpty();
+        return userInput == null || userInput.trim().isEmpty();
     }
 }
