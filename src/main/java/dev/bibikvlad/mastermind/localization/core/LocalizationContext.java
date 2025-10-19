474a52960d5fbcf7241dd391d7063825eadec1ca
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-20 02:36:26 +0300

    Add getLogoMessages method to the LocalizationContext

--- src/main/java/dev/bibikvlad/mastermind/localization/core/LocalizationContext.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/LocalizationContext.java
@@ -3,6 +3,7 @@ package dev.bibikvlad.mastermind.localization.core;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.config.MessageType;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
 
 public class LocalizationContext {
     private final MessageProvider messageProvider;
@@ -18,6 +19,10 @@ public class LocalizationContext {
 
     public GameMessages getGameMessages() {
         return messageProvider.getMessages(GameMessages.class, MessageType.GAME.getResourceBundleName());
+    }
+
+    public LogoMessages getLogoMessages() {
+        return messageProvider.getMessages(LogoMessages.class, MessageType.LOGO.getResourceBundleName());
     }
 
     @SuppressWarnings("unchecked")
