69ce280f0f16b24aa863ac51712118917b7f2240
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-21 02:01:52 +0300

    Add LogoMessages field Add selectLogoColors method Add printForegroundColorChoices method

--- src/main/java/dev/bibikvlad/mastermind/menu/settings/LogoColorSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/LogoColorSelectionMenu.java
@@ -2,6 +2,9 @@ package dev.bibikvlad.mastermind.menu.settings;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
+import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
+import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
@@ -10,6 +13,7 @@ public class LogoColorSelectionMenu {
     private final PlayerService playerService;
     private final LocalizationContext localizationContext;
     private final MastermindUserInputParser parser;
+    private final LogoMessages logoMessages;
 
     public LogoColorSelectionMenu(Player currentPlayer, PlayerService playerService,
                                   LocalizationContext localizationContext, MastermindUserInputParser parser) {
@@ -17,5 +21,32 @@ public class LogoColorSelectionMenu {
         this.playerService = playerService;
         this.localizationContext = localizationContext;
         this.parser = parser;
+        this.logoMessages = localizationContext.getLogoMessages();
+    }
+
+    public LogoColorsBundle selectLogoColors() {
+        while (true) {
+
+
+            String userInput = parser.parseUserInput();
+
+
+            int userInputNumber;
+
+            try {
+                userInputNumber = Integer.parseInt(userInput);
+
+            } catch (NumberFormatException exception) {
+                System.out.println("Invalid input. Please enter a number corresponding to the menu option.");
+
+                continue;
+            }
+        }
+    }
+
+    private void printForegroundColorChoices() {
+        for (ConsoleColor color : ConsoleColor.getForegroundColors()) {
+            System.out.println(logoMessages.getColor(color.getLocalizationKey()));
+        }
     }
 }
