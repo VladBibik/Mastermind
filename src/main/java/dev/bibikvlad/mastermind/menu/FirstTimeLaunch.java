090d2a98c15196f0a5eaa0ebcb549ecfbf0f26a7
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-20 04:04:40 +0300

    Refactor FirstTimeLaunch class Now it returns created LocalizationContext

--- src/main/java/dev/bibikvlad/mastermind/menu/FirstTimeLaunch.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/FirstTimeLaunch.java
@@ -2,16 +2,23 @@ package dev.bibikvlad.mastermind.menu;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
+//TODO: Def needs to be refactored. Naming and logic should be questioned
 public class FirstTimeLaunch {
-    public static void launch(MastermindUserInputParser parser, PlayerService playerService) {
+    public static LocalizationContext getLocalizationContextAfterUserSelection(MastermindUserInputParser parser,
+                                                                               PlayerService playerService) {
         System.out.println("Welcome to the Mastermind Game!");
 
         LanguageSelectionMenu languageSelectionMenu = new LanguageSelectionMenu(parser);
 
         LocaleType localeType = languageSelectionMenu.selectLanguage();
 
+        LocalizationContext localizationContext = new LocalizationContext(localeType);
+
         NewPlayerCreation.create(parser, playerService, localeType);
+
+        return localizationContext;
     }
 }
