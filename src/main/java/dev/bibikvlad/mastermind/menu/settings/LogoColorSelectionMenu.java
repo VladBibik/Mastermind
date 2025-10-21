815727117b9c1e35ffd1856cc29f451fa357833a
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-21 03:11:33 +0300

    Add field LogoColorBundle and its initialization in the constructor

--- src/main/java/dev/bibikvlad/mastermind/menu/settings/LogoColorSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/LogoColorSelectionMenu.java
@@ -3,6 +3,7 @@ package dev.bibikvlad.mastermind.menu.settings;
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
+import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
@@ -13,6 +14,8 @@ public class LogoColorSelectionMenu {
     private final MastermindUserInputParser parser;
     private final LogoMessages logoMessages;
 
+    private LogoColorsBundle logoColorsBundle;
+
     public LogoColorSelectionMenu(Player currentPlayer, PlayerService playerService,
                                   LocalizationContext localizationContext, MastermindUserInputParser parser) {
         this.currentPlayer = currentPlayer;
@@ -20,6 +23,8 @@ public class LogoColorSelectionMenu {
         this.localizationContext = localizationContext;
         this.parser = parser;
         this.logoMessages = localizationContext.getLogoMessages();
+
+        this.logoColorsBundle = currentPlayer.getPlayerConfig().getLogoColorsBundle();
     }
 
     public void selectLogoColors() {
