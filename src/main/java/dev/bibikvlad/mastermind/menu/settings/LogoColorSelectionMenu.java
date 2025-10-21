7c9bf9af91e5105d787cbbc48a947fd2b6ab694e
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-21 03:09:47 +0300

    Refactor LogoColorSelectionMenu class Remove almost everything because I don't like the way it was going Now I have much more clear implementation in mind

--- src/main/java/dev/bibikvlad/mastermind/menu/settings/LogoColorSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/LogoColorSelectionMenu.java
@@ -3,11 +3,8 @@ package dev.bibikvlad.mastermind.menu.settings;
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
-import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
-import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
-import dev.bibikvlad.mastermind.validators.StringEmptyValidator;
 
 public class LogoColorSelectionMenu {
     private final Player currentPlayer;
@@ -25,67 +22,7 @@ public class LogoColorSelectionMenu {
         this.logoMessages = localizationContext.getLogoMessages();
     }
 
-    public LogoColorsBundle selectLogoColors() {
-        return new LogoColorsBundle(
-                selectLogoBorderColor(),
-                selectLogoBorderColor(),
-                selectLogoBorderColor(),
-                selectLogoBorderColor()
-        );
-    }
+    public void selectLogoColors() {
 
-    private ConsoleColor selectLogoBorderColor() {
-        System.out.println(getDefaultTipMessage());
-        System.out.println(getDefaultGoBackMessage());
-        System.out.println("Please select border color");
-
-        printForegroundColorChoices();
-
-        return colorSelectionLoop();
-    }
-
-    private ConsoleColor colorSelectionLoop() {
-        while (true) {
-            String userInput = parser.parseUserInput();
-
-            if (StringEmptyValidator.isNullOrEmpty(userInput)) {
-                System.out.println("Invalid input. Please enter a number corresponding to the menu option.");
-
-                continue;
-            }
-
-            try {
-                int userInputNumber = Integer.parseInt(userInput);
-
-                try {
-                    return ConsoleColor.getForegroundColorByIndex(userInputNumber);
-                } catch (IllegalArgumentException exception) {
-                    System.out.println("Invalid input. Please enter a number corresponding " +
-                            "to the desired color option.");
-                }
-            } catch (NumberFormatException exception) {
-                System.out.println("Invalid input. Please enter a number corresponding to the menu option.");
-            }
-        }
-    }
-
-    private void printForegroundColorChoices() {
-        for (ConsoleColor color : ConsoleColor.getForegroundColors()) {
-            System.out.println(logoMessages.getColor(color.getLocalizationKey()));
-        }
-    }
-
-    private void printBackgroundColorChoices() {
-        for (ConsoleColor color : ConsoleColor.getBackgroundColors()) {
-            System.out.println(logoMessages.getColor(color.getLocalizationKey()));
-        }
-    }
-
-    private String getDefaultTipMessage() {
-        return "To select a color please enter a corresponding number";
-    }
-
-    private String getDefaultGoBackMessage() {
-        return "To return to previous menu print 'close', or 'exit'";
     }
 }
