31de1b202d2d561b9dcc662e63a24d54c60d5133
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-22 03:36:55 +0300

    Add PlayerService method

--- src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerMenu.java
@@ -3,17 +3,21 @@ package dev.bibikvlad.mastermind.menu.player;
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.model.player.Player;
+import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class PlayerMenu {
     private final LocalizationContext localizationContext;
     private final MastermindUserInputParser parser;
+    private final PlayerService  playerService;
 
     private Player currentPlayer;
     private boolean isDone = false;
 
-    public PlayerMenu(LocalizationContext localizationContext, MastermindUserInputParser parser, Player currentPlayer) {
+    public PlayerMenu(LocalizationContext localizationContext, MastermindUserInputParser parser,
+                      PlayerService playerService, Player currentPlayer) {
         this.localizationContext = localizationContext;
         this.parser = parser;
+        this.playerService = playerService;
         this.currentPlayer = currentPlayer;
     }
 
