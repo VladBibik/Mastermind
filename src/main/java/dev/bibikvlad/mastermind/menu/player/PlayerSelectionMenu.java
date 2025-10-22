003561eff3a1f8a5f7dd1fa6586c3003be257aab
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-22 03:56:44 +0300

    Add selectPlayer method Add displayPlayers method Add selectPlayer method

--- src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerSelectionMenu.java
@@ -2,17 +2,66 @@ package dev.bibikvlad.mastermind.menu.player;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
+import dev.bibikvlad.mastermind.validators.StringEmptyValidator;
+
+import java.util.List;
 
 public class PlayerSelectionMenu {
     private final LocalizationContext localizationContext;
     private final MastermindUserInputParser parser;
     private final PlayerService playerService;
 
+    private List<Player> playerList;
+
     public PlayerSelectionMenu(LocalizationContext localizationContext, MastermindUserInputParser parser,
                                PlayerService playerService) {
         this.localizationContext = localizationContext;
         this.parser = parser;
         this.playerService = playerService;
+    }
+
+    public Player selectPlayer() {
+        while (true) {
+            displayPlayers();
+            System.out.println("\nTo get back to previous menu print 'exit', or 'quit'");
+
+            String userInput = parser.parseUserInput();
+
+            if (StringEmptyValidator.isNullOrEmpty(userInput)) {
+                System.out.println("Input cannot be empty. Please try again.");
+
+                continue;
+            }
+
+            if (userInput.equals("exit") || userInput.equals("quit")) {
+                return null;
+            }
+
+            int userInputNumber;
+
+            try {
+                userInputNumber = Integer.parseInt(userInput);
+            } catch (NumberFormatException exception) {
+                System.out.println("Invalid input. Please enter a number corresponding to the player index.");
+
+                continue;
+            }
+
+            return selectPlayer(userInputNumber);
+        }
+    }
+
+    private void displayPlayers() {
+        playerList = playerService.getAllPlayers();
+
+        for (int i = 0; i < playerList.size(); i++) {
+            System.out.println(i + ": " + playerList.get(i).getPlayerName());
+        }
+    }
+
+    private Player selectPlayer(int playerIndex) {
+        return playerList.get(playerIndex);
     }
 }
