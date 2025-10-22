7c30c23b9d2534cb6245a9334beaa848468426b5
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-22 03:34:01 +0300

    Add currentPlayer and isDone field Add menu method Add displayMenu method Add empty changePlayer method Add menuOptionSwitcher method

--- src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerMenu.java
@@ -2,13 +2,57 @@ package dev.bibikvlad.mastermind.menu.player;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.model.player.Player;
 
 public class PlayerMenu {
     private final LocalizationContext localizationContext;
     private final MastermindUserInputParser parser;
 
-    public PlayerMenu(LocalizationContext localizationContext, MastermindUserInputParser parser) {
+    private Player currentPlayer;
+    private boolean isDone = false;
+
+    public PlayerMenu(LocalizationContext localizationContext, MastermindUserInputParser parser, Player currentPlayer) {
         this.localizationContext = localizationContext;
         this.parser = parser;
+        this.currentPlayer = currentPlayer;
+    }
+
+    public void menu() {
+        while (isDone) {
+            displayMenu();
+
+            String userInput = parser.parseUserInput();
+
+            if (userInput.equalsIgnoreCase("exit") || userInput.equalsIgnoreCase("close")) {
+                break;
+            }
+
+            int userInputNumber;
+
+            try {
+                userInputNumber = Integer.parseInt(userInput);
+            } catch (NumberFormatException exception) {
+                System.out.println("Invalid input. Please enter a number corresponding to the menu option.");
+
+                continue;
+            }
+
+            menuOptionSwitcher(userInputNumber);
+        }
+    }
+
+    private void displayMenu() {
+        System.out.println();
+        System.out.println("1. Change player");
+        System.out.println("2. Back to main menu");
+    }
+
+    private void menuOptionSwitcher(int userInputNumber) {
+        switch (userInputNumber) {
+            case 1 -> changePlayer();
+        }
+    }
+
+    private void changePlayer() {
     }
 }
