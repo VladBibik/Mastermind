cc14bb7c7d5ff4b7f23172d1a79045bccca77992
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-22 03:43:14 +0300

    Add PlayerService field

--- src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerSelectionMenu.java
@@ -2,13 +2,17 @@ package dev.bibikvlad.mastermind.menu.player;
 
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class PlayerSelectionMenu {
     private final LocalizationContext localizationContext;
     private final MastermindUserInputParser parser;
+    private final PlayerService playerService;
 
-    public PlayerSelectionMenu(LocalizationContext localizationContext, MastermindUserInputParser parser) {
+    public PlayerSelectionMenu(LocalizationContext localizationContext, MastermindUserInputParser parser,
+                               PlayerService playerService) {
         this.localizationContext = localizationContext;
         this.parser = parser;
+        this.playerService = playerService;
     }
 }
