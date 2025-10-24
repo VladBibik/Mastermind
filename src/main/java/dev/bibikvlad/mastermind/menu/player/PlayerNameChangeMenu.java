f698d827863d591adb59d2a64154b73fbee4b3ca
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-24 03:19:53 +0300

    Add menu method

--- src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerNameChangeMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerNameChangeMenu.java
@@ -1,9 +1,11 @@
 package dev.bibikvlad.mastermind.menu.player;
 
+import dev.bibikvlad.mastermind.exceptions.PlayerAlreadyExistException;
 import dev.bibikvlad.mastermind.game.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.model.player.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
+import dev.bibikvlad.mastermind.validators.StringEmptyValidator;
 
 public class PlayerNameChangeMenu {
     private final LocalizationContext localizationContext;
@@ -17,5 +19,35 @@ public class PlayerNameChangeMenu {
         this.parser = parser;
         this.playerService = playerService;
         this.currentPlayer = currentPlayer;
+    }
+
+    public void menu() {
+        boolean isDone = false;
+
+        while (!isDone) {
+            System.out.println("Please enter a new Player's name for: " + currentPlayer.getPlayerName());
+            System.out.println();
+            System.out.println("To go back to the previous menu enter 'exit' o 'close'");
+
+            String userInput = parser.parseUserInput();
+
+            if (StringEmptyValidator.isNullOrEmpty(userInput)) {
+                System.out.println("Player's name cannot be empty");
+
+                continue;
+            }
+
+            if (userInput.equalsIgnoreCase("exit") || userInput.equalsIgnoreCase("close")) {
+                break;
+            }
+
+            try {
+                playerService.updatePlayerName(currentPlayer.getId(), userInput);
+
+                break;
+            } catch (PlayerAlreadyExistException exception) {
+                System.out.println("Player with name " + userInput + " already exists");
+            }
+        }
     }
 }
