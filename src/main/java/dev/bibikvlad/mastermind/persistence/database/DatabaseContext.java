eca9ba748417414fea308148dcc4ed9076133d9d
BibikVlad <bibikvladislav.work@gmail.com>
2025-10-25 04:05:19 +0300

    Refactor getConnection method Now it setups ON CASCADE by enabling foreign_keys

--- src/main/java/dev/bibikvlad/mastermind/persistence/database/DatabaseContext.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/database/DatabaseContext.java
@@ -3,12 +3,18 @@ package dev.bibikvlad.mastermind.persistence.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseContext {
-    private static final String DB_URL = "jdbc:sqlite:Mastermind.db";
+    private static final String DB_URL = "jdbc:sqlite:Mastermind.db" ;
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL);
+        Connection connection = DriverManager.getConnection(DB_URL);
+        Statement statement = connection.createStatement();
+
+        statement.execute("PRAGMA foreign_keys=ON");
+
+        return connection;
     }
 
     //TODO: Move this method to the custom class when app start point is decided!
