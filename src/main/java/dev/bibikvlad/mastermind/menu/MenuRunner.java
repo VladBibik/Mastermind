cb62031d1eb6e3748e40c45ede401a2cff6d6288
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-07 00:11:52 +0300

    Refactor menu runner Now it only takes 1 parameter

--- src/main/java/dev/bibikvlad/mastermind/menu/MenuRunner.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/MenuRunner.java
@@ -1,13 +1,7 @@
 package dev.bibikvlad.mastermind.menu;
 
-import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
-import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
-import dev.bibikvlad.mastermind.services.PlayerService;
-
 public class MenuRunner {
-    public static void runMenu(LocalizationContext localizationContext, MastermindUserInputParser parser,
-                               PlayerService playerService) {
-        Menu menu = new MainMenu(localizationContext, parser, playerService);
+    public static void runMenu(Menu menu) {
 
         while (!(menu instanceof ExitMenu)) {
             menu = menu.run();
