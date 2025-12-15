0276cdb86e5af6e6ace512897639eff84743909b
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-15 21:15:34 +0300

    Add MastermindGameDurationWrapper class

---
+++ src/main/java/dev/bibikvlad/mastermind/app/MastermindGameDurationWrapper.java
@@ -0,0 +1,19 @@
+package dev.bibikvlad.mastermind.app;
+
+import dev.bibikvlad.mastermind.game.MastermindConsoleGame;
+import dev.bibikvlad.mastermind.game.data.GameData;
+import dev.bibikvlad.mastermind.game.data.GameOutcome;
+
+public class MastermindGameDurationWrapper {
+    public static GameData launch(MastermindConsoleGame game) {
+        long gameStart = System.currentTimeMillis();
+
+        GameOutcome gameOutcome = game.play();
+
+        long gameFinish = System.currentTimeMillis();
+
+        long gameDuration = gameFinish - gameStart;
+
+        return new GameData(gameOutcome, gameDuration);
+    }
+}
