7d210c91d1f2bcc60c61ce38d6fdbb29ffa55853
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-17 18:28:55 +0300

    Refactor return types and parameters of methods

--- src/main/java/dev/bibikvlad/mastermind/persistence/dao/GamesDAO.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/dao/GamesDAO.java
@@ -2,15 +2,16 @@ package dev.bibikvlad.mastermind.persistence.dao;
 
 import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.game.data.GameData;
+import dev.bibikvlad.mastermind.model.game.Game;
 
 import java.util.List;
 
 public interface GamesDAO {
-    List<GameData> findAll() throws PersistenceException;
+    List<Game> findAll() throws PersistenceException;
 
-    List<GameData> findAllById() throws PersistenceException;
+    List<Game> findAllById() throws PersistenceException;
 
-    boolean save(GameData gameData) throws PersistenceException;
+    boolean save(long playerId, GameData gameData) throws PersistenceException;
 
     int count() throws PersistenceException;
 }
