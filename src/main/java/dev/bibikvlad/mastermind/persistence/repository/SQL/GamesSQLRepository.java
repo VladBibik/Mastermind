24f32c372dda2f2e37abcb7e19c595eeb4876bf9
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-19 21:51:39 +0300

    Add fields and constructor

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/GamesSQLRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/GamesSQLRepository.java
@@ -3,11 +3,21 @@ package dev.bibikvlad.mastermind.persistence.repository.SQL;
 import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.game.data.GameData;
 import dev.bibikvlad.mastermind.model.game.Game;
+import dev.bibikvlad.mastermind.persistence.dao.GamesDAO;
+import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
 import dev.bibikvlad.mastermind.persistence.repository.GamesRepository;
 
 import java.util.List;
 
 public class GamesSQLRepository implements GamesRepository {
+    private final GamesDAO gamesDAO;
+    private final TransactionManager transactionManager;
+
+    public GamesSQLRepository(GamesDAO gamesDAO, TransactionManager transactionManager) {
+        this.gamesDAO = gamesDAO;
+        this.transactionManager = transactionManager;
+    }
+
     @Override
     public List<Game> findAll() throws PersistenceException {
         return List.of();
