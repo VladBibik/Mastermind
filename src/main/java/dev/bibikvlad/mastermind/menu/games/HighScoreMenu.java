4cc737bc4500a3283dd382dbbd41a55991f37f2d
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-22 00:11:17 +0300

    Add HighScoreMenu class

---
+++ src/main/java/dev/bibikvlad/mastermind/menu/games/HighScoreMenu.java
@@ -0,0 +1,10 @@
+package dev.bibikvlad.mastermind.menu.games;
+
+import dev.bibikvlad.mastermind.menu.Menu;
+
+public class HighScoreMenu implements Menu {
+    @Override
+    public Menu run() {
+        return null;
+    }
+}
