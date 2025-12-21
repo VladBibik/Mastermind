37968af6e566852c2674e345f9a9b268ce848b9a
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-21 22:06:33 +0300

    Add GamesServiceGeneratorTEMP class Temporary measure

---
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/GamesServiceGeneratorTEMP.java
@@ -0,0 +1,26 @@
+package dev.bibikvlad.mastermind.app.bootstrap;
+
+import dev.bibikvlad.mastermind.persistence.dao.GamesDAO;
+import dev.bibikvlad.mastermind.persistence.dao.JDBC.GamesJdbcDAO;
+import dev.bibikvlad.mastermind.persistence.database.DatabaseContext;
+import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
+import dev.bibikvlad.mastermind.persistence.repository.GamesRepository;
+import dev.bibikvlad.mastermind.persistence.repository.SQL.GamesSQLRepository;
+import dev.bibikvlad.mastermind.services.GamesService;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class GamesServiceGeneratorTEMP {
+    public static GamesService get() {
+        try {
+            Connection connection = DatabaseContext.getConnection();
+            GamesDAO gamesDAO = new GamesJdbcDAO(connection);
+            GamesRepository gamesRepository = new GamesSQLRepository(gamesDAO, new TransactionManager(connection));
+
+            return new GamesService(gamesRepository);
+        } catch (SQLException exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+}
