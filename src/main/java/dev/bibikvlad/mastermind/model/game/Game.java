770d20b6bd3ee8ac91b09b793d053cd0bc437097
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-22 17:47:55 +0300

    Refactor Game class converted it to record

--- src/main/java/dev/bibikvlad/mastermind/model/game/Game.java
+++ src/main/java/dev/bibikvlad/mastermind/model/game/Game.java
@@ -4,32 +4,5 @@ import dev.bibikvlad.mastermind.game.data.GameData;
 
 import java.time.LocalDateTime;
 
-public class Game {
-    private final long gameId;
-    private final long playerId;
-    private final GameData gameData;
-    private final LocalDateTime startedAt;
-
-    public Game(long gameId, long playerId, GameData gameData, LocalDateTime startedAt) {
-        this.gameId = gameId;
-        this.playerId = playerId;
-        this.gameData = gameData;
-        this.startedAt = startedAt;
-    }
-
-    public long getGameId() {
-        return gameId;
-    }
-
-    public long getPlayerId() {
-        return playerId;
-    }
-
-    public GameData getGameData() {
-        return gameData;
-    }
-
-    public LocalDateTime getStartedAt() {
-        return startedAt;
-    }
+public record Game(long gameId, long playerId, GameData gameData, LocalDateTime startedAt) {
 }
