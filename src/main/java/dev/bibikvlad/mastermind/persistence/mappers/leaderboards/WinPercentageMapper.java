3561b129664a44f54ff813aeb2c9f918c841e098
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-23 22:51:54 +0300

    Add map method

--- src/main/java/dev/bibikvlad/mastermind/persistence/mappers/leaderboards/WinPercentageMapper.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/mappers/leaderboards/WinPercentageMapper.java
@@ -1,4 +1,15 @@
 package dev.bibikvlad.mastermind.persistence.mappers.leaderboards;
 
+import dev.bibikvlad.mastermind.model.leaderboard.WinPercentageLeaderboardEntry;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class WinPercentageMapper {
+    public static WinPercentageLeaderboardEntry map(ResultSet resultSet) throws SQLException {
+        return new WinPercentageLeaderboardEntry(
+                resultSet.getString("player_name"),
+                resultSet.getDouble("win_percentage")
+        );
+    }
 }
