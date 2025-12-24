d3eb48c9e9d9395f4dd35c81be702452a9982134
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-24 22:16:51 +0300

    Add WinLeaderboardEntryMapper class

---
+++ src/main/java/dev/bibikvlad/mastermind/persistence/mappers/leaderboards/WinLeaderboardEntryMapper.java
@@ -0,0 +1,15 @@
+package dev.bibikvlad.mastermind.persistence.mappers.leaderboards;
+
+import dev.bibikvlad.mastermind.model.leaderboard.WinsLeaderboardEntry;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WinLeaderboardEntryMapper {
+    public static WinsLeaderboardEntry map(ResultSet resultSet) throws SQLException {
+        return new WinsLeaderboardEntry(
+                resultSet.getString("player_name"),
+                resultSet.getLong("number_of_wins")
+        );
+    }
+}
