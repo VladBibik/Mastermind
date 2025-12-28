1210d8d6c5e4ab85711d5ccb5ee1d3167cae7060
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-28 13:55:46 +0300

    Add methods to the LeaderboardRepository interface

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/LeaderboardRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/LeaderboardRepository.java
@@ -1,4 +1,17 @@
 package dev.bibikvlad.mastermind.persistence.repository;
 
+import dev.bibikvlad.mastermind.model.leaderboard.*;
+
+import java.util.List;
+
 public interface LeaderboardRepository {
+    List<MainLeaderboardEntry> getMainLeaderboard();
+
+    List<TimeLeaderboardEntry> getTimeLeaderboard();
+
+    List<TurnsLeaderboardEntry> getTurnsLeaderboard();
+
+    List<WinPercentageLeaderboardEntry> getWinRateLeaderboard();
+
+    List<WinsLeaderboardEntry> getWinsLeaderboard();
 }
