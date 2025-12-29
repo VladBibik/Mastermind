4a3624bacd26ff731975409e13c9f396701b700f
BibikVlad <bibikvladislav.work@gmail.com>
2025-12-30 00:53:06 +0300

    Add field and constructor initialization

--- src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/LeaderboardSQLRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/repository/SQL/LeaderboardSQLRepository.java
@@ -2,15 +2,18 @@ package dev.bibikvlad.mastermind.persistence.repository.SQL;
 
 import dev.bibikvlad.mastermind.model.leaderboard.*;
 import dev.bibikvlad.mastermind.persistence.dao.JDBC.LeaderboardJdbcDAO;
+import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
 import dev.bibikvlad.mastermind.persistence.repository.LeaderboardRepository;
 
 import java.util.List;
 
 public class LeaderboardSQLRepository implements LeaderboardRepository {
     private final LeaderboardJdbcDAO leaderboardJdbcDAO;
+    private final TransactionManager transactionManager;
 
-    public LeaderboardSQLRepository(LeaderboardJdbcDAO leaderboardJdbcDAO) {
+    public LeaderboardSQLRepository(LeaderboardJdbcDAO leaderboardJdbcDAO, TransactionManager transactionManager) {
         this.leaderboardJdbcDAO = leaderboardJdbcDAO;
+        this.transactionManager = transactionManager;
     }
 
     @Override
