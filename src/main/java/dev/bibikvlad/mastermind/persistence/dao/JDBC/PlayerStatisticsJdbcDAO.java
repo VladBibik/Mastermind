98e6ca224035d98131bd9c4306d77be59c9e6732
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-01 22:23:00 +0300

    Add logic to the getTotalPlayTimeByPlayerId method

--- src/main/java/dev/bibikvlad/mastermind/persistence/dao/JDBC/PlayerStatisticsJdbcDAO.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/dao/JDBC/PlayerStatisticsJdbcDAO.java
@@ -1,9 +1,14 @@
 package dev.bibikvlad.mastermind.persistence.dao.JDBC;
 
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
 import dev.bibikvlad.mastermind.persistence.dao.PlayerStatisticsDAO;
 import dev.bibikvlad.mastermind.values.Time;
+import dev.bibikvlad.utils.formatters.MillisecondsToTimeFormatter;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PlayerStatisticsJdbcDAO implements PlayerStatisticsDAO {
     private final Connection connection;
@@ -14,6 +19,22 @@ public class PlayerStatisticsJdbcDAO implements PlayerStatisticsDAO {
 
     @Override
     public Time getTotalPlayTimeByPlayerId(long playerId) {
-        return null;
+        String getTotalPlayTimeByPlayerIdQuery = """
+                SELECT SUM(duration_milliseconds) AS total_playtime
+                FROM GAME
+                WHERE PLAYER_ID = ?;
+                """;
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(getTotalPlayTimeByPlayerIdQuery)) {
+            preparedStatement.setLong(1, playerId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            long duration_milliseconds = resultSet.getLong("total_playtime");
+
+            return MillisecondsToTimeFormatter.format(duration_milliseconds);
+        } catch (SQLException exception) {
+            throw new PersistenceException("Failed to fetch total play time for a player with ID: " + playerId,
+                    exception);
+        }
     }
 }
