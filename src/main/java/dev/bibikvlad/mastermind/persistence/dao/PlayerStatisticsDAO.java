456ccf1c46f83237b6ad0a9c261bfb356332d0f7
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-02 22:30:18 +0300

    Add getPlayerStatistic method

--- src/main/java/dev/bibikvlad/mastermind/persistence/dao/PlayerStatisticsDAO.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/dao/PlayerStatisticsDAO.java
@@ -1,8 +1,11 @@
 package dev.bibikvlad.mastermind.persistence.dao;
 
+import dev.bibikvlad.mastermind.model.player.PlayerStatistic;
 import dev.bibikvlad.mastermind.values.Time;
 
 public interface PlayerStatisticsDAO {
+    PlayerStatistic getPlayerStatistic(int playerId);
+
     Time getTotalPlayTime(long playerId);
 
     double getWinPercentage(long playerId);
