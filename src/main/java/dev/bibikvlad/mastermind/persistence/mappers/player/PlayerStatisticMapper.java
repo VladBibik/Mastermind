6e8a53a4e439efcf7289d25d23fe412378236521
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-02 22:23:13 +0300

    Add PlayerStatisticMapper util mapper class

---
+++ src/main/java/dev/bibikvlad/mastermind/persistence/mappers/player/PlayerStatisticMapper.java
@@ -0,0 +1,20 @@
+package dev.bibikvlad.mastermind.persistence.mappers.player;
+
+import dev.bibikvlad.mastermind.model.player.PlayerStatistic;
+import dev.bibikvlad.utils.formatters.MillisecondsToTimeFormatter;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PlayerStatisticMapper {
+    public static PlayerStatistic map(ResultSet resultSet) throws SQLException {
+        return new PlayerStatistic(
+                resultSet.getLong("game_count"),
+                resultSet.getLong("win_count"),
+                resultSet.getDouble("win_percentage"),
+                MillisecondsToTimeFormatter.format(resultSet.getLong("average_game_duration")),
+                MillisecondsToTimeFormatter.format(resultSet.getLong("fastest_win_time")),
+                resultSet.getInt("min_turns_win")
+        );
+    }
+}
