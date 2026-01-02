48f1d14f2a7c20a4db184a24270fb0952bf9c594
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-02 23:06:21 +0300

    Add totalPlayTime field

--- src/main/java/dev/bibikvlad/mastermind/model/player/PlayerStatistics.java
+++ src/main/java/dev/bibikvlad/mastermind/model/player/PlayerStatistics.java
@@ -2,6 +2,6 @@ package dev.bibikvlad.mastermind.model.player;
 
 import dev.bibikvlad.mastermind.values.Time;
 
-public record PlayerStatistics(long gameCount, long winCount, double winPercentage, Time averageGameDuration,
-                               Time fastestWinTime, int minTurnsWin) {
+public record PlayerStatistics(long gameCount, long winCount, double winPercentage, Time totalPlayTime,
+                               Time averageGameDuration, Time fastestWinTime, int minTurnsWin) {
 }
