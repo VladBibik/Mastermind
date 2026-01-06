0a230f262d61320a24a877cb95a9dbed3142a5a2
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-06 04:14:39 +0300

    Add methods to the PlayerStatisticsRepository interface

--- src/main/java/dev/bibikvlad/mastermind/persistence/player/repository/PlayerStatisticsRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/player/repository/PlayerStatisticsRepository.java
@@ -1,4 +1,20 @@
 package dev.bibikvlad.mastermind.persistence.player.repository;
 
+import dev.bibikvlad.mastermind.persistence.player.model.PlayerStatistics;
+import dev.bibikvlad.mastermind.values.Time;
+
 public interface PlayerStatisticsRepository {
+    PlayerStatistics getPlayerStatistics(long playerId);
+
+    Time getTotalPlayTime(long playerId);
+
+    double getWinPercentage(long playerId);
+
+    Time getAverageGameDuration(long playerId);
+
+    Time getFastestWinTime(long playerId);
+
+    long getWinCount(long playerId);
+
+    int getMinTurnsWin(long playerId);
 }
