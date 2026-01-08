e7eaf6d498edbdd33cfae72d6eede22b419493d4
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-09 02:54:14 +0300

    Add logic to all methods

--- src/main/java/dev/bibikvlad/mastermind/persistence/player/repository/sql/PlayerStatisticsSQLRepository.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/player/repository/sql/PlayerStatisticsSQLRepository.java
@@ -14,36 +14,36 @@ public class PlayerStatisticsSQLRepository implements PlayerStatisticsRepository
 
     @Override
     public PlayerStatistics getPlayerStatistics(long playerId) {
-        return null;
+        return playerStatisticsDAO.getPlayerStatistics(playerId);
     }
 
     @Override
     public Time getTotalPlayTime(long playerId) {
-        return null;
+        return playerStatisticsDAO.getTotalPlayTime(playerId);
     }
 
     @Override
     public double getWinPercentage(long playerId) {
-        return 0;
+        return playerStatisticsDAO.getWinPercentage(playerId);
     }
 
     @Override
     public Time getAverageGameDuration(long playerId) {
-        return null;
+        return playerStatisticsDAO.getAverageGameDuration(playerId);
     }
 
     @Override
     public Time getFastestWinTime(long playerId) {
-        return null;
+        return playerStatisticsDAO.getFastestWinTime(playerId);
     }
 
     @Override
     public long getWinCount(long playerId) {
-        return 0;
+        return playerStatisticsDAO.getWinCount(playerId);
     }
 
     @Override
     public int getMinTurnsWin(long playerId) {
-        return 0;
+        return playerStatisticsDAO.getMinTurnsWin(playerId);
     }
 }
