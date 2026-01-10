f26533e63a77248262f6515c285db11dc8237c18
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-10 20:14:12 +0300

    Rename HighScoreMenu -> LeaderboardMenu

--- src/main/java/dev/bibikvlad/mastermind/menu/games/LeaderboardMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/games/LeaderboardMenu.java
@@ -2,7 +2,7 @@ package dev.bibikvlad.mastermind.menu.games;
 
 import dev.bibikvlad.mastermind.menu.Menu;
 
-public class HighScoreMenu implements Menu {
+public class LeaderboardMenu implements Menu {
     @Override
     public Menu run() {
         return null;
