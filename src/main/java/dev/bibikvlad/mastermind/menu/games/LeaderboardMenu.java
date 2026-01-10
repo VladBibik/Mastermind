846e0bd9253bf793ac588e3df463d664ec0127d7
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-10 21:30:38 +0300

    Add getLeaderboard method

--- src/main/java/dev/bibikvlad/mastermind/menu/games/LeaderboardMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/games/LeaderboardMenu.java
@@ -5,9 +5,12 @@ import dev.bibikvlad.mastermind.input.interpreter.IntegerInputInterpreter;
 import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.menu.Menu;
+import dev.bibikvlad.mastermind.persistence.leaderboard.model.MainLeaderboardEntry;
 import dev.bibikvlad.mastermind.persistence.player.model.Player;
 import dev.bibikvlad.mastermind.services.LeaderboardService;
+import dev.bibikvlad.utils.formatters.TimeToStringFormatter;
 
+import java.util.List;
 import java.util.Optional;
 
 public class LeaderboardMenu implements Menu {
@@ -41,5 +44,22 @@ public class LeaderboardMenu implements Menu {
         System.out.println("3. Leaderboard based on number of turns needed for win");
         System.out.println("4. Win percentage leaderboard");
         System.out.println("5. Leaderboard based on the number of wins");
+    }
+
+    private Menu getLeaderboard(long playerId) {
+        Optional<List<MainLeaderboardEntry>> optionalLeaderboard = leaderboardService.getMainLeaderboard(playerId);
+
+        if (optionalLeaderboard.isEmpty()) {
+            System.out.println("No leaderboard found. Please play some games to build the leaderboard");
+
+            return this;
+        }
+
+        optionalLeaderboard.get().forEach(leaderboardEntry -> {
+            System.out.println(leaderboardEntry.playerName() + ": " + leaderboardEntry.numberOfTurns() + " turns, "
+                    + TimeToStringFormatter.format(leaderboardEntry.gameDuration()));
+        });
+
+        return this;
     }
 }
