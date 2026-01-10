1a8eb6cf702217a087a40e4084f3263e176df103
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-10 19:31:35 +0300

    Add getPlayerStatistics method

--- src/main/java/dev/bibikvlad/mastermind/services/PlayerStatisticsService.java
+++ src/main/java/dev/bibikvlad/mastermind/services/PlayerStatisticsService.java
@@ -1,11 +1,18 @@
 package dev.bibikvlad.mastermind.services;
 
+import dev.bibikvlad.mastermind.persistence.player.model.PlayerStatistics;
 import dev.bibikvlad.mastermind.persistence.player.repository.PlayerStatisticsRepository;
+
+import java.util.Optional;
 
 public class PlayerStatisticsService {
     private final PlayerStatisticsRepository playerStatisticsRepository;
 
     public PlayerStatisticsService(PlayerStatisticsRepository playerStatisticsRepository) {
         this.playerStatisticsRepository = playerStatisticsRepository;
+    }
+
+    public Optional<PlayerStatistics> getPlayerStatistics(long playerId) {
+        return Optional.ofNullable(playerStatisticsRepository.getPlayerStatistics(playerId));
     }
 }
