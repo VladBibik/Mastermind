719531116280e0baeddf61839ece71bd3bed46b5
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-10 20:21:55 +0300

    Add fields and constructor initialization

--- src/main/java/dev/bibikvlad/mastermind/menu/games/LeaderboardMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/games/LeaderboardMenu.java
@@ -1,8 +1,22 @@
 package dev.bibikvlad.mastermind.menu.games;
 
+import dev.bibikvlad.mastermind.app.bootstrap.LeaderboardServiceGeneratorTEMP;
+import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.menu.Menu;
+import dev.bibikvlad.mastermind.services.LeaderboardService;
 
 public class LeaderboardMenu implements Menu {
+    private final LocalizationContext localizationContext;
+    private final MastermindUserInputParser parser;
+    private final LeaderboardService leaderboardService;
+
+    public LeaderboardMenu(LocalizationContext localizationContext, MastermindUserInputParser parser) {
+        this.localizationContext = localizationContext;
+        this.parser = parser;
+        this.leaderboardService = LeaderboardServiceGeneratorTEMP.get();
+    }
+
     @Override
     public Menu run() {
         return null;
