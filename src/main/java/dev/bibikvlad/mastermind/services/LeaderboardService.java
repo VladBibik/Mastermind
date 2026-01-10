a4bf9c2d21be43319db6afa98ef9f9c35659ea29
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-10 19:41:58 +0300

    Add getMainLeaderboard method

--- src/main/java/dev/bibikvlad/mastermind/services/LeaderboardService.java
+++ src/main/java/dev/bibikvlad/mastermind/services/LeaderboardService.java
@@ -1,11 +1,19 @@
 package dev.bibikvlad.mastermind.services;
 
+import dev.bibikvlad.mastermind.persistence.leaderboard.model.MainLeaderboardEntry;
 import dev.bibikvlad.mastermind.persistence.leaderboard.repository.LeaderboardRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public class LeaderboardService {
     private final LeaderboardRepository leaderboardRepository;
 
     public LeaderboardService(LeaderboardRepository leaderboardRepository) {
         this.leaderboardRepository = leaderboardRepository;
+    }
+
+    public Optional<List<MainLeaderboardEntry>> getMainLeaderboard(long playerId) {
+        return Optional.ofNullable(leaderboardRepository.getOverallLeaderboard());
     }
 }
