92f636ed28ee304bb6414c575336daaa056449ac
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-12 02:53:59 +0300

    Fix PlayerSelectionMenu
    App is in broken state. If back commit to this one, app will not launch!!!

--- src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerSelectionMenu.java
@@ -1,5 +1,6 @@
 package dev.bibikvlad.mastermind.menu.player;
 
+import dev.bibikvlad.mastermind.app.bootstrap.ServiceContainer;
 import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.input.validation.StringEmptyValidator;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
@@ -9,23 +10,22 @@ import dev.bibikvlad.mastermind.services.PlayerService;
 
 import java.util.List;
 
-public class PlayerSelectionMenu implements Menu {
-    private final LocalizationContext localizationContext;
-    private final MastermindUserInputParser parser;
+public class PlayerSelectionMenu extends Menu {
     private final PlayerService playerService;
 
-    private List<Player> playerList;
+    public PlayerSelectionMenu(LocalizationContext localizationContext, ServiceContainer serviceContainer,
+                               MastermindUserInputParser parser) {
+        super(localizationContext, serviceContainer, parser);
 
-    public PlayerSelectionMenu(LocalizationContext localizationContext, MastermindUserInputParser parser,
-                               PlayerService playerService) {
-        this.localizationContext = localizationContext;
-        this.parser = parser;
-        this.playerService = playerService;
+        this.playerService = serviceContainer.getPlayerService();
     }
 
     @Override
     public Menu run() {
-        displayPlayers();
+        List<Player> playerList = getAllPlayers();
+
+        displayPlayers(playerList);
+
         System.out.println("\nTo get back to previous menu print 'exit', or 'quit'");
 
         String userInput = parser.parseUserInput();
@@ -50,7 +50,7 @@ public class PlayerSelectionMenu implements Menu {
             return this;
         }
 
-        Player player = selectPlayer(userInputNumber);
+        Player player = selectPlayer(playerList, userInputNumber);
 
         if (player == null) {
             System.out.println("Invalid input. Please enter a number corresponding to the player index.");
@@ -65,15 +65,17 @@ public class PlayerSelectionMenu implements Menu {
         return new PlayerMenu(localizationContext, parser, playerService);
     }
 
-    private void displayPlayers() {
-        playerList = playerService.getAllPlayers();
+    private List<Player> getAllPlayers() {
+        return playerService.getAllPlayers();
+    }
 
+    private void displayPlayers(List<Player> playerList) {
         for (int i = 0; i < playerList.size(); i++) {
             System.out.println((i + 1) + ": " + playerList.get(i).getPlayerName());
         }
     }
 
-    private Player selectPlayer(int playerIndex) {
+    private Player selectPlayer(List<Player> playerList, int playerIndex) {
         Player player;
 
         try {
