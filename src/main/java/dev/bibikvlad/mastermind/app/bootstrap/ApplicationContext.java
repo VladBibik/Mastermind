ddc2f708ab3935569bb14bd1d20747edaf9532fe
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-11 22:30:56 +0300

    Add getPlayerService method

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/ApplicationContext.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/ApplicationContext.java
@@ -1,6 +1,18 @@
 package dev.bibikvlad.mastermind.app.bootstrap;
 
 import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
+import dev.bibikvlad.mastermind.persistence.player.dao.PlayerConfigDAO;
+import dev.bibikvlad.mastermind.persistence.player.dao.PlayerDAO;
+import dev.bibikvlad.mastermind.persistence.player.dao.PlayerLastSelectedDAO;
+import dev.bibikvlad.mastermind.persistence.player.dao.jdbc.PlayerConfigJdbcDAO;
+import dev.bibikvlad.mastermind.persistence.player.dao.jdbc.PlayerJdbcDAO;
+import dev.bibikvlad.mastermind.persistence.player.dao.jdbc.PlayerLastSelectedJdbcDAO;
+import dev.bibikvlad.mastermind.persistence.player.repository.PlayerConfigRepository;
+import dev.bibikvlad.mastermind.persistence.player.repository.PlayerLastSelectedRepository;
+import dev.bibikvlad.mastermind.persistence.player.repository.PlayerRepository;
+import dev.bibikvlad.mastermind.persistence.player.repository.sql.PlayerConfigSQLRepository;
+import dev.bibikvlad.mastermind.persistence.player.repository.sql.PlayerLastSelectedSQLRepository;
+import dev.bibikvlad.mastermind.persistence.player.repository.sql.PlayerSQLRepository;
 import dev.bibikvlad.mastermind.services.GamesService;
 import dev.bibikvlad.mastermind.services.LeaderboardService;
 import dev.bibikvlad.mastermind.services.PlayerService;
@@ -20,5 +32,24 @@ public class ApplicationContext {
     public ApplicationContext(Connection connection, TransactionManager transactionManager) {
         this.connection = connection;
         this.transactionManager = transactionManager;
+    }
+
+    public PlayerService getPlayerService() {
+        if (playerService == null) {
+            PlayerDAO playerDAO = new PlayerJdbcDAO(connection);
+            PlayerConfigDAO playerConfigDAO = new PlayerConfigJdbcDAO(connection);
+            PlayerLastSelectedDAO playerLastSelectedDAO = new PlayerLastSelectedJdbcDAO(connection);
+
+            PlayerRepository playerRepository = new PlayerSQLRepository(playerDAO, transactionManager);
+            PlayerConfigRepository playerConfigRepository =
+                    new PlayerConfigSQLRepository(playerConfigDAO, transactionManager);
+            PlayerLastSelectedRepository playerLastSelectedRepository =
+                    new PlayerLastSelectedSQLRepository(playerLastSelectedDAO, transactionManager);
+
+            playerService = new PlayerService(playerRepository, playerConfigRepository,
+                    playerLastSelectedRepository);
+        }
+
+        return playerService;
     }
 }
