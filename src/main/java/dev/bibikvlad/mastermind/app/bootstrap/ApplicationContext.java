0cfa91adcfe267741ce932ccded1804894e2de2f
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-11 22:29:12 +0300

    Add TransactionManager field and constructor initialization

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/ApplicationContext.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/ApplicationContext.java
@@ -1,5 +1,6 @@
 package dev.bibikvlad.mastermind.app.bootstrap;
 
+import dev.bibikvlad.mastermind.persistence.database.TransactionManager;
 import dev.bibikvlad.mastermind.services.GamesService;
 import dev.bibikvlad.mastermind.services.LeaderboardService;
 import dev.bibikvlad.mastermind.services.PlayerService;
@@ -9,13 +10,15 @@ import java.sql.Connection;
 
 public class ApplicationContext {
     private final Connection connection;
+    private final TransactionManager transactionManager;
 
     private PlayerService playerService;
     private GamesService gamesService;
     private LeaderboardService leaderboardService;
     private PlayerStatisticsService playerStatisticsService;
 
-    public ApplicationContext(Connection connection) {
+    public ApplicationContext(Connection connection, TransactionManager transactionManager) {
         this.connection = connection;
+        this.transactionManager = transactionManager;
     }
 }
