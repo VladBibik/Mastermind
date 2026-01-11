fed912949fc16a2118ee5d9d6a203a39fd927d91
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-12 02:23:07 +0300

    Fix ExitMenu
    App is in broken state. If back commit to this one, app will not launch!!!

--- src/main/java/dev/bibikvlad/mastermind/menu/ExitMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/ExitMenu.java
@@ -1,6 +1,15 @@
 package dev.bibikvlad.mastermind.menu;
 
-public class ExitMenu implements Menu {
+import dev.bibikvlad.mastermind.app.bootstrap.ServiceContainer;
+import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+
+public class ExitMenu extends Menu {
+    public ExitMenu(ServiceContainer serviceContainer, LocalizationContext localizationContext,
+                    MastermindUserInputParser parser) {
+        super(serviceContainer, localizationContext, parser);
+    }
+
     @Override
     public Menu run() {
         return null;
