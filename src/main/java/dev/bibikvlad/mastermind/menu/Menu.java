4bcdb18e436581ddfe7aa388d7ef33e41992c1b5
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-12 02:10:10 +0300

    Refactor Menu interface Now it's a abstract class with fields Since menu logic is stabilized we can now take everything that applies to all menus and move it to the abstract class

--- src/main/java/dev/bibikvlad/mastermind/menu/Menu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/Menu.java
@@ -1,5 +1,21 @@
 package dev.bibikvlad.mastermind.menu;
 
-public interface Menu {
-    Menu run();
+import dev.bibikvlad.mastermind.app.bootstrap.ApplicationContext;
+import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+
+public abstract class Menu {
+    protected final ApplicationContext applicationContext;
+    protected final LocalizationContext localizationContext;
+    protected final MastermindUserInputParser parser;
+    //TODO: Add printer!
+
+    public Menu(ApplicationContext applicationContext, LocalizationContext localizationContext,
+                MastermindUserInputParser parser) {
+        this.applicationContext = applicationContext;
+        this.localizationContext = localizationContext;
+        this.parser = parser;
+    }
+
+    public abstract Menu run();
 }
