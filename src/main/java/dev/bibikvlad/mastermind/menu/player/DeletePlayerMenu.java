aca2a23502a5f5d88a0d280b72ce41c32dd69ac1
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-12 17:08:40 +0300

    Fix DeletePlayerMenu
    App is in broken state. If back commit to this one, app will not launch!!!

--- src/main/java/dev/bibikvlad/mastermind/menu/player/DeletePlayerMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/DeletePlayerMenu.java
@@ -1,23 +1,23 @@
 package dev.bibikvlad.mastermind.menu.player;
 
+import dev.bibikvlad.mastermind.app.bootstrap.ServiceContainer;
 import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.menu.Menu;
 import dev.bibikvlad.mastermind.persistence.player.model.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
-public class DeletePlayerMenu implements Menu {
-    private final LocalizationContext localizationContext;
-    private final MastermindUserInputParser parser;
+public class DeletePlayerMenu extends Menu {
     private final PlayerService playerService;
     private final Player currentPlayer;
 
-    public DeletePlayerMenu(LocalizationContext localizationContext, MastermindUserInputParser parser,
-                            PlayerService playerService) {
-        this.localizationContext = localizationContext;
-        this.parser = parser;
-        this.playerService = playerService;
-        this.currentPlayer = playerService.loadLastSelectedPlayer().get();
+    public DeletePlayerMenu(LocalizationContext localizationContext, ServiceContainer serviceContainer,
+                            MastermindUserInputParser parser) {
+        super(localizationContext, serviceContainer, parser);
+
+        this.playerService = serviceContainer.getPlayerService();
+        this.currentPlayer = playerService.loadLastSelectedPlayer().orElseThrow(
+                () -> new IllegalStateException("No last selected player found!"));
     }
 
     @Override
@@ -26,7 +26,7 @@ public class DeletePlayerMenu implements Menu {
             System.out.println("Cannot delete a player.");
             System.out.println("Please register at least one more player first.");
 
-            return new PlayerMenu(localizationContext, parser, playerService);
+            return new PlayerMenu(localizationContext, serviceContainer, parser);
         }
 
         playerService.deletePlayer(currentPlayer.getId());
@@ -34,13 +34,13 @@ public class DeletePlayerMenu implements Menu {
         System.out.println("Player with the name: " + currentPlayer.getPlayerName() + " has been deleted.");
 
         if (!playerService.isMultiplePlayersRegistered()) {
-            return new PlayerMenu(localizationContext, parser, playerService);
+            return new PlayerMenu(localizationContext, serviceContainer, parser);
         }
 
         //TODO: This def should be changed.
         System.out.println("If you'll close player selection menu, " +
                 "previous last selected player will be picked automatically.");
 
-        return new PlayerSelectionMenu(localizationContext, parser, playerService);
+        return new PlayerSelectionMenu(localizationContext, serviceContainer, parser);
     }
 }
