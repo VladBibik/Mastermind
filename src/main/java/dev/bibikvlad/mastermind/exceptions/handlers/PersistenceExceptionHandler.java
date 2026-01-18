5e91548069027745627f8b43aba1c358fc060ea0
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-19 02:26:19 +0300

    Add PersistenceExceptionHandler class

---
+++ src/main/java/dev/bibikvlad/mastermind/exceptions/handlers/PersistenceExceptionHandler.java
@@ -0,0 +1,13 @@
+package dev.bibikvlad.mastermind.exceptions.handlers;
+
+//TODO: Add printer instead of SOUT!
+public class PersistenceExceptionHandler {
+    private final static String EXCEPTION_MESSAGE = "Problem with the database occurred. " +
+            "Please check your environment and try again later";
+
+    public static void handle(Exception exception) {
+        System.out.println(EXCEPTION_MESSAGE + "\n" + exception.getMessage());
+
+        System.exit(1);
+    }
+}
