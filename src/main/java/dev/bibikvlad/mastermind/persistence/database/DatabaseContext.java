8433004cf42dfba25ba387df2d2aa075659cdc55
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-19 02:41:26 +0300

    Refactor DatabaseContext class Now it hides SQLException and operates only with PersistenceException

--- src/main/java/dev/bibikvlad/mastermind/persistence/database/DatabaseContext.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/database/DatabaseContext.java
@@ -1,20 +1,26 @@
 package dev.bibikvlad.mastermind.persistence.database;
 
+import dev.bibikvlad.mastermind.exceptions.PersistenceException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseContext {
-    private static final String DB_URL = "jdbc:sqlite:Mastermind.db" ;
+    private static final String DB_URL = "jdbc:sqlite:Mastermind.db";
 
-    public static Connection getConnection() throws SQLException {
-        Connection connection = DriverManager.getConnection(DB_URL);
-        Statement statement = connection.createStatement();
+    public static Connection getConnection() throws PersistenceException {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL);
+            try (Statement statement = connection.createStatement()) {
+                statement.execute("PRAGMA foreign_keys=ON");
+            }
 
-        statement.execute("PRAGMA foreign_keys=ON");
-
-        return connection;
+            return connection;
+        } catch (SQLException exception) {
+            throw new PersistenceException("Problem obtaining database connection", exception);
+        }
     }
 
     //TODO: Move this method to the custom class when app start point is decided!
@@ -22,7 +28,7 @@ public class DatabaseContext {
         try (Connection connection = getConnection()) {
             SchemaCreator.create(connection);
         } catch (SQLException exception) {
-            throw new IllegalStateException("Failed to initialize database schema", exception);
+            throw new PersistenceException("Failed to initialize database schema", exception);
         }
     }
 }
