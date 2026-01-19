92bc26d4a860b3c2a93ded9c34fa147b13731d68
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-19 18:37:20 +0300

    Refactor MastermindMessagePrinter interface Now it's a concrete class that uses Printer concrete implementation for different types of message printing

--- src/main/java/dev/bibikvlad/mastermind/game/printer/MastermindMessagePrinter.java
+++ src/main/java/dev/bibikvlad/mastermind/game/printer/MastermindMessagePrinter.java
@@ -1,17 +1,39 @@
 package dev.bibikvlad.mastermind.game.printer;
 
+import dev.bibikvlad.mastermind.app.printer.Printer;
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 
-public interface MastermindMessagePrinter {
-    void printInvalidInputMessage();
+public class MastermindMessagePrinter {
+    private final Printer printer;
+    private final GameMessages gameMessages;
 
-    void printIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput);
+    public MastermindMessagePrinter(Printer printer, GameMessages gameMessages) {
+        this.printer = printer;
+        this.gameMessages = gameMessages;
+    }
 
-    void printGameOverMessage(String answer);
+    public void printInvalidInputMessage() {
+        printer.printMessage(gameMessages.getInvalidInputMessage());
+    }
 
-    void printWinMessage(String answer);
+    public void printIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput) {
+        printer.printMessage(gameMessages.getIncorrectGuessMessage(maxTurns, currentTurn, answer, userInput));
+    }
 
-    void printRulesMessage();
+    public void printGameOverMessage(String answer) {
+        printer.printMessage(gameMessages.getGameOverMessage(answer));
+    }
 
-    void printAsciiLogo(LogoColorsBundle logoColorsBundle);
+    public void printWinMessage(String answer) {
+        printer.printMessage(gameMessages.getWinMessage(answer));
+    }
+
+    public void printRulesMessage() {
+        printer.printMessage(gameMessages.getRulesMessage());
+    }
+
+    public void printAsciiLogo(LogoColorsBundle logoColorsBundle) {
+        printer.printMessage(gameMessages.getAsciiLogo(logoColorsBundle));
+    }
 }
