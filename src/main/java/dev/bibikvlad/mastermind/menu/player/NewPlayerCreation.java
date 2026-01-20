b366827e4764e174a0a93e97d058221d9cf471ad
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-20 21:23:31 +0300

    Fix NewPlayerCreation class Add newly added AppContext
    Warning! App is in broken state!

--- src/main/java/dev/bibikvlad/mastermind/menu/player/NewPlayerCreation.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/NewPlayerCreation.java
@@ -1,25 +1,21 @@
 package dev.bibikvlad.mastermind.menu.player;
 
-import dev.bibikvlad.mastermind.app.bootstrap.ServiceContainer;
+import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
 import dev.bibikvlad.mastermind.exceptions.PlayerAlreadyExistException;
-import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.input.validation.StringEmptyValidator;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
-import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.menu.Menu;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
-//TODO: Either pass LocalizationContext, or already created Messages, or make this class a normal instance based class
 public class NewPlayerCreation extends Menu {
     private final PlayerService playerService;
     private final LocaleType localeType;
     private final Menu returnMenu;
 
-    public NewPlayerCreation(LocalizationContext localizationContext, ServiceContainer serviceContainer,
-                             MastermindUserInputParser parser, LocaleType localeType, Menu returnMenu) {
-        super(localizationContext, serviceContainer, parser);
+    public NewPlayerCreation(AppContext appContext, LocaleType localeType, Menu returnMenu) {
+        super(appContext);
 
-        this.playerService = serviceContainer.getPlayerService();
+        this.playerService = appContext.services().getPlayerService();
         this.localeType = localeType;
         this.returnMenu = returnMenu;
     }
@@ -29,7 +25,7 @@ public class NewPlayerCreation extends Menu {
         System.out.println("Please enter nickname of a new player");
         System.out.println("To cancel type: 'close' or 'exit'");
 
-        String newPlayerName = parser.parseUserInput();
+        String newPlayerName = appContext.parser().parseUserInput();
 
         if (StringEmptyValidator.isNullOrEmpty(newPlayerName)) {
             System.out.println("Player name cannot be empty");
@@ -49,8 +45,12 @@ public class NewPlayerCreation extends Menu {
         }
 
         try {
+            //TODO: Since here we are creating a current player with default setting we need to rethink it's instantiation
             if (playerService.savePlayerWithProvidedLocale(newPlayerName, localeType)) {
                 System.out.println("Player with name " + newPlayerName + " has been created.\n");
+
+                //TODO:This is need to be changed. Only here to see if everything works with AppContext
+                appContext.setCurrentPlayer(playerService.loadLastSelectedPlayer().get());
 
                 return returnMenu;
             }
