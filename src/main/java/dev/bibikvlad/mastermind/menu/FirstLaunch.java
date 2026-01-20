bcc38acfc8a0e48c3e63af0594428c340d06a357
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-20 21:24:17 +0300

    Fix FirstLaunch class Add newly added AppContext
    Warning! App is in broken state!

--- src/main/java/dev/bibikvlad/mastermind/menu/FirstLaunch.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/FirstLaunch.java
@@ -1,26 +1,25 @@
 package dev.bibikvlad.mastermind.menu;
 
-import dev.bibikvlad.mastermind.app.bootstrap.ServiceContainer;
-import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.menu.player.NewPlayerCreation;
 import dev.bibikvlad.mastermind.menu.settings.LanguageSelectionMenu;
 
 public class FirstLaunch {
-    public static void start(MastermindUserInputParser parser, ServiceContainer serviceContainer) {
+    public static void start(AppContext appContext) {
         System.out.println("Welcome to the Mastermind Game!");
 
-        LanguageSelectionMenu languageSelectionMenu = new LanguageSelectionMenu(parser);
+        LanguageSelectionMenu languageSelectionMenu = new LanguageSelectionMenu(appContext.parser());
 
         LocaleType localeType = languageSelectionMenu.selectLanguage();
 
         LocalizationContext localizationContext = new LocalizationContext(localeType);
+        appContext.serLocalizationContext(localizationContext);
 
-        Menu mainMenu = new MainMenu(localizationContext, serviceContainer, parser);
+        Menu mainMenu = new MainMenu(appContext);
 
-        Menu playerCreationMenu = new NewPlayerCreation(localizationContext, serviceContainer, parser, localeType,
-                mainMenu);
+        Menu playerCreationMenu = new NewPlayerCreation(appContext, localeType, mainMenu);
 
         MenuRunner.runMenu(playerCreationMenu);
     }
