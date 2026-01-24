88cc134f70a965ca397b16e0866b9936bf114037
BibikVlad <bibikvladislav.work@gmail.com>
2026-01-24 22:05:08 +0300

    Some random testing

---
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/FunctionTEST.java
@@ -0,0 +1,106 @@
+package dev.bibikvlad.mastermind.app.bootstrap;
+
+import dev.bibikvlad.mastermind.game.RandomAnswerGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.function.*;
+
+public class FunctionTEST {
+    public static void main(String[] args) {
+        Test test = new Test();
+
+        System.out.println(test.test().apply("Kino"));
+        System.out.println(test.supTest().get());
+        System.out.println(test.testUnFunctions(RandomAnswerGenerator.generate().hashCode()));
+        System.out.println(test.biTesting(RandomAnswerGenerator.generate(), RandomAnswerGenerator.generate().hashCode()));
+        System.out.println(test.predicateTesting(RandomAnswerGenerator.generate()));
+        test.runnableTest();
+        test.funcParamTest(string -> {
+            char[] charArray = string.toCharArray();
+            Random random = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < charArray.length; i++) {
+                sum += String.valueOf(charArray[i]).hashCode() * random.nextInt(777);
+            }
+
+            return sum;
+        });
+    }
+}
+
+class Test {
+    public Function<String, Integer> test() {
+        return string -> string.length() + 2;
+    }
+
+    public Supplier<String> supTest() {
+        return RandomAnswerGenerator::generate;
+    }
+
+    public String testUnFunctions(Integer hash) {
+        IntFunction<String> stringLongFunction = Integer::toString;
+
+        return stringLongFunction.apply(hash);
+    }
+
+    public Double biTesting(String answer, Integer hash) {
+        BiFunction<String, Integer, Double> biFunction = (randomAnswer, answerHash) -> {
+            if (randomAnswer.length() == 0) {
+                return 0D;
+            }
+
+            return (double) answerHash / randomAnswer.length();
+        };
+
+        return biFunction.apply(answer, hash);
+    }
+
+    public Boolean predicateTesting(String string) {
+        Predicate<String> stringPredicate = value -> value.hashCode() > 10_000;
+
+        return stringPredicate.test(string);
+    }
+
+    public void runnableTest() {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            Random random = new Random();
+            List<Future<String>> futures = new ArrayList<>();
+
+            Callable<String> callable = () -> {
+                Thread.sleep(random.nextLong(1000));
+
+                return "Thread: " + Thread.currentThread().getName() + " is done working.";
+            };
+
+            for (int i = 0; i < 1_000_000; i++) {
+                futures.add(executorService.submit(callable));
+            }
+
+            executorService.shutdown();
+
+            futures.forEach(future -> {
+                try {
+                    System.out.println(future.get());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            if (!executorService.awaitTermination(50000, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public void funcParamTest(Function<String, Integer> test) {
+        System.out.println(test.apply(RandomAnswerGenerator.generate()));
+    }
+}
