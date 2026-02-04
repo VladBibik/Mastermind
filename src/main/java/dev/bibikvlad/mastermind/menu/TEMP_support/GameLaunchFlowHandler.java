7beecc1fbca33abf1d993cd29a33c8046532673a
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-04 05:05:00 +0300

    Add Player field

--- src/main/java/dev/bibikvlad/mastermind/menu/TEMP_support/GameLaunchFlowHandler.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/TEMP_support/GameLaunchFlowHandler.java
@@ -4,6 +4,7 @@ import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
 import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.persistence.player.model.Player;
 import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class GameLaunchFlowHandler {
@@ -11,11 +12,13 @@ public class GameLaunchFlowHandler {
     private final MastermindUserInputParser parser;
     private final Printer printer;
     private final PlayerService playerService;
+    private final Player currentPlayer;
 
     public GameLaunchFlowHandler(AppContext appContext) {
         this.localizationContext = appContext.localizationContext();
         this.parser = appContext.parser();
         this.printer = appContext.printer();
         this.playerService = appContext.services().getPlayerService();
+        this.currentPlayer = appContext.currentPlayer();
     }
 }
