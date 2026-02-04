bb139c61ae8f4f1447557051dc649e5a0b69775b
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-04 05:10:34 +0300

    Refactor PlayerService to GamesService

--- src/main/java/dev/bibikvlad/mastermind/menu/TEMP_support/GameLaunchFlowHandler.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/TEMP_support/GameLaunchFlowHandler.java
@@ -5,20 +5,20 @@ import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
 import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.persistence.player.model.Player;
-import dev.bibikvlad.mastermind.services.PlayerService;
+import dev.bibikvlad.mastermind.services.GamesService;
 
 public class GameLaunchFlowHandler {
     private final LocalizationContext localizationContext;
     private final MastermindUserInputParser parser;
     private final Printer printer;
-    private final PlayerService playerService;
+    private final GamesService gamesService;
     private final Player currentPlayer;
 
     public GameLaunchFlowHandler(AppContext appContext) {
         this.localizationContext = appContext.localizationContext();
         this.parser = appContext.parser();
         this.printer = appContext.printer();
-        this.playerService = appContext.services().getPlayerService();
+        this.gamesService = appContext.services().getGamesService();
         this.currentPlayer = appContext.currentPlayer();
     }
 }
