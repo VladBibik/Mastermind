c318aacc54b4852a9305d9a7e50c7ed8c4c3c558
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-06 04:51:59 +0300

    Refactor exit strategy Now it relies on ExitMenu instance instead of null

--- src/main/java/dev/bibikvlad/mastermind/menu/MenuRunner.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/MenuRunner.java
@@ -3,7 +3,7 @@ package dev.bibikvlad.mastermind.menu;
 public class MenuRunner {
     public static void runMenu(Menu menu) {
 
-        while (menu != null) {
+        while (!(menu instanceof ExitMenu)) {
             menu = menu.run();
         }
     }
