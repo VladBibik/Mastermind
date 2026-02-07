01cbca87b2ac95a46142d0b164935b29964cb23c
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-07 18:46:47 +0300

    Refactor LocalizationContext Remove 2nd constructor Remove specific, parameterless methods Add getMessages method with Class<T> as parameter Now class is immutable

--- src/main/java/dev/bibikvlad/mastermind/localization/core/LocalizationContext.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/LocalizationContext.java
@@ -3,36 +3,21 @@ package dev.bibikvlad.mastermind.localization.core;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.config.MessageType;
 import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
-import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
-import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
 
 public class LocalizationContext {
-    private MessageProvider messageProvider;
-    private MessageFactoryRegistry messageFactoryRegistry;
+    private final MessageProvider messageProvider;
 
     public LocalizationContext(LocaleType localeType) {
-        this.messageFactoryRegistry = MessageRegistryInitializer.createAndPopulateRegistry();
+        MessageFactoryRegistry messageFactoryRegistry = MessageRegistryInitializer.createAndPopulateRegistry();
         this.messageProvider = new MessageProvider(localeType, messageFactoryRegistry);
     }
 
-    public LocalizationContext(MessageProvider messageProvider) {
-        this.messageProvider = messageProvider;
-    }
-
-    public void changeLocale(LocaleType localeType) {
-        messageProvider = new MessageProvider(localeType, messageFactoryRegistry);
-    }
-
-    public GameMessages getGameMessages() {
-        return messageProvider.getMessages(GameMessages.class);
-    }
-
-    public LogoMessages getLogoMessages() {
-        return messageProvider.getMessages(LogoMessages.class);
+    public <T extends LocalizedMessages> T getMessages(Class<T> messageType) {
+        return messageProvider.getMessages(messageType);
     }
 
     @SuppressWarnings("unchecked")
     public <T extends LocalizedMessages> T getMessages(MessageType messageType) {
-        return (T) messageProvider.getMessages(messageType.getMessageType());
+        return (T) getMessages(messageType.getMessageClass());
     }
 }
