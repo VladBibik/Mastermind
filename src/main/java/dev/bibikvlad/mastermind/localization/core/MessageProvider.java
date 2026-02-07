36985705b560aa2102a3badfcc7ffc9f23cd3924
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-08 00:19:32 +0300

    Refactor MessageProvider Add message caching

--- src/main/java/dev/bibikvlad/mastermind/localization/core/MessageProvider.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/MessageProvider.java
@@ -3,24 +3,35 @@ package dev.bibikvlad.mastermind.localization.core;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class MessageProvider {
     private final LocaleType localeType;
     private final MessageFactoryRegistry messageFactoryRegistry;
+    private final Map<Class<? extends LocalizedMessages>, LocalizedMessages> cache = new HashMap<>();
 
     public MessageProvider(LocaleType localeType, MessageFactoryRegistry messageFactoryRegistry) {
         this.localeType = localeType;
         this.messageFactoryRegistry = messageFactoryRegistry;
     }
 
+    @SuppressWarnings("unchecked")
     public <T extends LocalizedMessages> T getMessages(Class<T> messageType) {
-        LocalizedMessageConfig<T> localizedMessageConfig =
-                messageFactoryRegistry.getLocalizedMessageConfig(messageType);
+        if (cache.containsKey(messageType)) {
+            return (T) cache.get(messageType);
+        } else {
+            LocalizedMessageConfig<T> localizedMessageConfig =
+                    messageFactoryRegistry.getLocalizedMessageConfig(messageType);
 
-        ResourceBundle resourceBundle =
-                ResourceBundle.getBundle(localizedMessageConfig.getBundleName(), localeType.getLocale());
+            ResourceBundle resourceBundle =
+                    ResourceBundle.getBundle(localizedMessageConfig.getBundleName(), localeType.getLocale());
 
-        return localizedMessageConfig.getMessageFactory().create(resourceBundle);
+            T messages = localizedMessageConfig.getMessageFactory().create(resourceBundle);
+            cache.put(messageType, messages);
+
+            return messages;
+        }
     }
 }
