a5e4192ff403d5d6289e016a115f122c09ce8606
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-07 05:21:15 +0300

    Rename MessagesDefinition -> LocalizedMessageConfig

--- src/main/java/dev/bibikvlad/mastermind/localization/core/LocalizedMessageConfig.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/LocalizedMessageConfig.java
@@ -2,12 +2,12 @@ package dev.bibikvlad.mastermind.localization.core;
 
 import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
 
-public class MessagesDefinition<T extends LocalizedMessages> {
+public class LocalizedMessageConfig<T extends LocalizedMessages> {
     private final Class<T> messageType;
     private final String bundleName;
     private final MessageFactory<T> messageFactory;
 
-    public MessagesDefinition(Class<T> messageType, String bundleName, MessageFactory<T> messageFactory) {
+    public LocalizedMessageConfig(Class<T> messageType, String bundleName, MessageFactory<T> messageFactory) {
         this.messageType = messageType;
         this.bundleName = bundleName;
         this.messageFactory = messageFactory;
