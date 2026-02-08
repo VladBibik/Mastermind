032dadc350661f32c51f682adfa40bc6e7341b3f
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-08 21:44:56 +0300

    Fix LocalizationContextTest

--- src/test/java/dev/bibikvlad/mastermind/localization/core/LocalizationContextTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/core/LocalizationContextTest.java
@@ -3,69 +3,31 @@ package dev.bibikvlad.mastermind.localization.core;
 import dev.bibikvlad.mastermind.clues.InputVisualRepresentation;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.config.MessageType;
-import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
-import dev.bibikvlad.mastermind.localization.messages.game.StubGameMessages;
 import dev.bibikvlad.utils.strings.Emojis;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
+//TODO: Add more tests!
 public class LocalizationContextTest {
-    private final String ANSWER = "RGBW";
+    private static GameMessages gameMessages;
 
-    @Test
-    @DisplayName("getMessages(MessageType.GAME) returns same GameMessages as getGameMessages()")
-    void getMessagesReturnsSameInstanceAsGetGameMessages() {
+    @BeforeAll
+    static void setUp() {
         LocalizationContext localizationContext = new LocalizationContext(LocaleType.ENGLISH);
-        GameMessages messagesFromParametrizedMethod = localizationContext.getMessages(MessageType.GAME);
-
-        assertEquals(localizationContext.getGameMessages().getWinMessage(ANSWER),
-                messagesFromParametrizedMethod.getWinMessage(ANSWER));
+        gameMessages = localizationContext.getMessages(MessageType.GAME);
     }
 
     @Test
     @DisplayName("getGameMessages() returns expected English win message")
     void getGameMessagesReturnsExpectedEnglishWinMessage() {
-        LocalizationContext localizationContext = new LocalizationContext(LocaleType.ENGLISH);
-        GameMessages messagesFromGameMethod = localizationContext.getGameMessages();
-
+        String ANSWER = "RGBW";
         assertEquals("You Won! " + Emojis.CELEBRATION_TADA + "\n" +
                         "You are the Mastermind!\n" +
                         "Solution was: " + InputVisualRepresentation.getVisualRepresentation(ANSWER),
-                messagesFromGameMethod.getWinMessage(ANSWER));
-    }
-
-    @Test
-    @DisplayName("getGameMessages() delegates with correct type and bundle name")
-    void getGameMessagesDelegatesToProviderWithCorrectParameters() {
-        GameMessages dummy = new StubGameMessages();
-        FakeMessageProvider fakeMessageProvider = new FakeMessageProvider(dummy);
-
-        LocalizationContext localizationContext = new LocalizationContext(fakeMessageProvider);
-        GameMessages result = localizationContext.getGameMessages();
-
-        assertSame(dummy, result);
-        assertEquals(GameMessages.class, fakeMessageProvider.lastRequestedType);
-    }
-
-    static class FakeMessageProvider extends MessageProvider {
-        private final Object toReturn;
-        private Class<?> lastRequestedType;
-        private String lastRequestedMessage;
-
-        public FakeMessageProvider(Object toReturn) {
-            super(null, null);
-            this.toReturn = toReturn;
-        }
-
-        @SuppressWarnings("unchecked")
-        public <T extends LocalizedMessages> T getMessages(Class<T> messageType) {
-            this.lastRequestedType = messageType;
-
-            return (T) toReturn;
-        }
+                gameMessages.getWinMessage(ANSWER));
     }
 }
