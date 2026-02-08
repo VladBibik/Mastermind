35910295dae91911f4d2df5706a9b30a2be81cf5
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-08 22:19:48 +0300

    Add TODO

--- src/main/java/dev/bibikvlad/mastermind/localization/messages/game/GameMessages.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/game/GameMessages.java
@@ -4,6 +4,7 @@ import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
 import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 
 public interface GameMessages extends LocalizedMessages {
+    //TODO: Now we have dedicated error messages!
     String getInvalidInputMessage();
 
     String getIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput);
