d1a649d1b53984ccb490d5705547f779e18793d5
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-08 22:44:09 +0300

    Add initialization for error and main menu messages

--- src/main/java/dev/bibikvlad/mastermind/localization/core/MessageRegistryInitializer.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/MessageRegistryInitializer.java
@@ -1,8 +1,12 @@
 package dev.bibikvlad.mastermind.localization.core;
 
+import dev.bibikvlad.mastermind.localization.factories.ConsoleErrorMessageFactory;
 import dev.bibikvlad.mastermind.localization.factories.ConsoleGameMessageFactory;
 import dev.bibikvlad.mastermind.localization.factories.ConsoleLogoMessagesFactory;
+import dev.bibikvlad.mastermind.localization.factories.ConsoleMainMenuMessageFactory;
+import dev.bibikvlad.mastermind.localization.messages.error.ErrorMessages;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.main.MainMenuMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
 
 public class MessageRegistryInitializer {
@@ -13,6 +17,16 @@ public class MessageRegistryInitializer {
                 GameMessages.class,
                 "i18n.game_messages",
                 new ConsoleGameMessageFactory()
+        ));
+        messageFactoryRegistry.register(new LocalizedMessageConfig<>(
+                ErrorMessages.class,
+                "i18n.error_messages",
+                new ConsoleErrorMessageFactory()
+        ));
+        messageFactoryRegistry.register(new LocalizedMessageConfig<>(
+                MainMenuMessages.class,
+                "i18n.main_menu",
+                new ConsoleMainMenuMessageFactory()
         ));
         messageFactoryRegistry.register(new LocalizedMessageConfig<>(
                 LogoMessages.class,
