61d0ff761955ac3b7371b973073674b45252fbe7
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-15 02:33:51 +0300

    Refactor all methods Add parameters they need

--- src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/stats/StatsMessages.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/stats/StatsMessages.java
@@ -1,4 +1,24 @@
 package dev.bibikvlad.mastermind.localization.messages.menu.stats;
 
+import dev.bibikvlad.mastermind.persistence.player.model.PlayerStatistics;
+import dev.bibikvlad.mastermind.values.Time;
+
 public interface StatsMessages {
+    String getDefaultStatArrangement(String playerName, PlayerStatistics playerStatistics);
+
+    String getHeader(String playerName);
+
+    String getGamesPlayed(long gamesPlayed);
+
+    String getWins(long winCount);
+
+    String getWinPercentage(double winPercentage);
+
+    String getTotalPlayTime(Time totalPlayTime);
+
+    String getAverageGameDuration(Time averageGameDuration);
+
+    String getFastestWinTime(Time fastestWinTime);
+
+    String getBestTurnCount(int minTurnWin);
 }
