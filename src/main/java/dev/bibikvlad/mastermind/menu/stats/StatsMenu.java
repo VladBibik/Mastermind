bdc2c1419cf16e0e2b136a51f6aa9fcca0a748b0
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-16 06:52:48 +0300

    Add StatsMenu class

---
+++ src/main/java/dev/bibikvlad/mastermind/menu/stats/StatsMenu.java
@@ -0,0 +1,15 @@
+package dev.bibikvlad.mastermind.menu.stats;
+
+import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
+import dev.bibikvlad.mastermind.menu.Menu;
+
+public class StatsMenu extends Menu {
+    public StatsMenu(AppContext appContext) {
+        super(appContext);
+    }
+
+    @Override
+    public Menu run() {
+        return null;
+    }
+}
