76b1cc342fcc90c383a94d3b32c20ce175e80475
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-16 08:13:19 +0300

    Add StatsMenu logic

--- src/main/java/dev/bibikvlad/mastermind/menu/stats/StatsMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/stats/StatsMenu.java
@@ -1,15 +1,60 @@
 package dev.bibikvlad.mastermind.menu.stats;
 
 import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
+import dev.bibikvlad.mastermind.app.printer.Printer;
+import dev.bibikvlad.mastermind.input.parser.MastermindUserInputParser;
+import dev.bibikvlad.mastermind.localization.config.MessageType;
+import dev.bibikvlad.mastermind.localization.messages.error.InteractionMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.stats.StatsMessages;
+import dev.bibikvlad.mastermind.menu.MainMenu;
 import dev.bibikvlad.mastermind.menu.Menu;
+import dev.bibikvlad.mastermind.persistence.player.model.Player;
+import dev.bibikvlad.mastermind.persistence.player.model.PlayerStatistics;
+import dev.bibikvlad.mastermind.services.PlayerStatisticsService;
 
 public class StatsMenu extends Menu {
+    private final PlayerStatisticsService playerStatisticsService;
+    private final Player currentPlayer;
+    private final StatsMessages statsMessages;
+    private final InteractionMessages interactionMessages;
+    private final Printer printer;
+    private final MastermindUserInputParser parser;
+
     public StatsMenu(AppContext appContext) {
         super(appContext);
+
+        this.playerStatisticsService = appContext.services().getPlayerStatisticsService();
+        this.currentPlayer = appContext.currentPlayer();
+        this.statsMessages = appContext.localizationContext().getMessages(MessageType.STATS);
+        this.interactionMessages = appContext.localizationContext().getMessages(MessageType.INTERACTION);
+        this.printer = appContext.printer();
+        this.parser = appContext.parser();
     }
 
     @Override
     public Menu run() {
-        return null;
+        printStats();
+
+        confirmToContinue();
+
+        return new MainMenu(appContext);
+    }
+
+    private void printStats() {
+        PlayerStatistics stats = fetchPlayerStatistics(currentPlayer.getId());
+
+        printer.printMessage(statsMessages.getDefaultStatArrangement(currentPlayer.getPlayerName(), stats));
+    }
+
+    private PlayerStatistics fetchPlayerStatistics(long playerId) {
+        return playerStatisticsService.getPlayerStatistics(playerId)
+                .orElseThrow(() -> new IllegalStateException("Statistics for the player with ID: "
+                        + playerId + " not found"));
+    }
+
+    private void confirmToContinue() {
+        printer.printMessage(interactionMessages.getPressEnterMessage());
+
+        parser.parseUserInput();
     }
 }
