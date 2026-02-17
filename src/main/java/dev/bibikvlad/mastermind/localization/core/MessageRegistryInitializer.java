bb8efef9fc082f7731b352b53b9d995f72b86c7b
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-17 05:50:43 +0300

    Add GameMenuMessages initialization in registry

--- src/main/java/dev/bibikvlad/mastermind/localization/core/MessageRegistryInitializer.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/core/MessageRegistryInitializer.java
@@ -3,6 +3,7 @@ package dev.bibikvlad.mastermind.localization.core;
 import dev.bibikvlad.mastermind.localization.factories.*;
 import dev.bibikvlad.mastermind.localization.messages.error.InteractionMessages;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.game.GameMenuMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.MainMenuMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.stats.StatsMessages;
@@ -25,6 +26,11 @@ public class MessageRegistryInitializer {
                 MainMenuMessages.class,
                 "i18n.main_menu",
                 new ConsoleMainMenuMessageFactory()
+        ));
+        messageFactoryRegistry.register(new LocalizedMessageConfig<>(
+                GameMenuMessages.class,
+                "i18n.game_menu",
+                new ConsoleGameMenuMessageFactory()
         ));
         messageFactoryRegistry.register(new LocalizedMessageConfig<>(
                 StatsMessages.class,
