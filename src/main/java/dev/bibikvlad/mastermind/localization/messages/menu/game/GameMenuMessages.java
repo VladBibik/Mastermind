0115610fadd960c58f8a182a4869406073a7e30b
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-17 05:49:48 +0300

    Add GameMenuMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/game/GameMenuMessages.java
@@ -0,0 +1,7 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.game;
+
+import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+
+public interface GameMenuMessages extends LocalizedMessages {
+    String playAgain();
+}
