9311ae2ae19a444f0a601e24fae78f0499afedf5
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-18 12:25:35 +0300

    Change type of some Time field to long Reason even though Time helps to organize duration in some cases for other it doesn't make sense single game duration will be represented by clock style formatting instead of formating that requires hour, min, sec, etc

--- src/main/java/dev/bibikvlad/mastermind/persistence/player/model/PlayerStatistics.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/player/model/PlayerStatistics.java
@@ -3,5 +3,5 @@ package dev.bibikvlad.mastermind.persistence.player.model;
 import dev.bibikvlad.mastermind.values.Time;
 
 public record PlayerStatistics(long gameCount, long winCount, double winPercentage, Time totalPlayTime,
-                               Time averageGameDuration, Time fastestWinTime, int minTurnsWin) {
+                               long averageGameDuration, long fastestWinTime, int minTurnsWin) {
 }
