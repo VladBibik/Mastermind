cf4335e25c5c36c8d1963235013123660b1a6378
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-20 09:44:11 +0300

    Add PlayerCreationMenu

---
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/PlayerCreationMenu.java
@@ -0,0 +1,16 @@
+package dev.bibikvlad.mastermind.menu.player;
+
+import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
+import dev.bibikvlad.mastermind.menu.Menu;
+
+public class PlayerCreationMenu extends Menu {
+
+    public PlayerCreationMenu(AppContext appContext) {
+        super(appContext);
+    }
+
+    @Override
+    public Menu run() {
+        return null;
+    }
+}
