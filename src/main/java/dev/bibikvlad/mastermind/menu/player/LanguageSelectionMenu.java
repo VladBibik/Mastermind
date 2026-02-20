15f56265a327872445094bfe26bbbcd917607293
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-20 12:49:02 +0300

    Add fields

--- src/main/java/dev/bibikvlad/mastermind/menu/player/LanguageSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/player/LanguageSelectionMenu.java
@@ -1,12 +1,22 @@
 package dev.bibikvlad.mastermind.menu.player;
 
 import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
+import dev.bibikvlad.mastermind.app.printer.Printer;
+import dev.bibikvlad.mastermind.input.parser.Parser;
 import dev.bibikvlad.mastermind.menu.Menu;
+import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class LanguageSelectionMenu extends Menu {
+    private final PlayerService playerService;
+    private final Printer printer;
+    private final Parser parser;
 
     public LanguageSelectionMenu(AppContext appContext) {
         super(appContext);
+
+        this.playerService = appContext.services().getPlayerService();
+        this.printer = appContext.printer();
+        this.parser = appContext.parser();
     }
 
     @Override
