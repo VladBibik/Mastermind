4c406ceaa16fb9b4f37f4aac34a665ef5bf7d6f7
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-21 08:39:43 +0300

    Refactor SettingsMenu Implement LanguageSelectionMenu

--- src/main/java/dev/bibikvlad/mastermind/menu/settings/SettingsMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/SettingsMenu.java
@@ -2,27 +2,16 @@ package dev.bibikvlad.mastermind.menu.settings;
 
 import dev.bibikvlad.mastermind.app.bootstrap.AppContext;
 import dev.bibikvlad.mastermind.input.interpreter.IntegerInputInterpreter;
-import dev.bibikvlad.mastermind.localization.config.LocaleType;
-import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
 import dev.bibikvlad.mastermind.menu.MainMenu;
 import dev.bibikvlad.mastermind.menu.Menu;
+import dev.bibikvlad.mastermind.menu.player.LanguageSelectionMenu;
 import dev.bibikvlad.mastermind.menu.settings.logo.LogoColorSelectionMenu;
-import dev.bibikvlad.mastermind.persistence.player.model.Player;
-import dev.bibikvlad.mastermind.services.PlayerService;
 
 import java.util.Optional;
 
 public class SettingsMenu extends Menu {
-    private final PlayerService playerService;
-
-    private Player currentPlayer;
-
     public SettingsMenu(AppContext appContext) {
         super(appContext);
-
-        this.playerService = appContext.services().getPlayerService();
-        this.currentPlayer = playerService.loadLastSelectedPlayer().orElseThrow(
-                () -> new IllegalStateException("No last selected player found!"));
     }
 
     //TODO: Should we go back on 'close', and 'exit'?
@@ -64,34 +53,7 @@ public class SettingsMenu extends Menu {
     }
 
     private Menu changeLanguage() {
-        FirstLaunchLanguageSelection firstLaunchLanguageSelection =
-                new FirstLaunchLanguageSelection(appContext.printer(), appContext.parser());
-
-        LocaleType localeType = firstLaunchLanguageSelection.selectLanguage();
-
-        return checkSelectedLanguage(localeType);
-    }
-
-    private Menu checkSelectedLanguage(LocaleType localeType) {
-        if (localeType.equals(currentPlayer.getPlayerConfig().locale())) {
-            System.out.println("Language is already selected!");
-
-            return this;
-        } else {
-            playerService.updatePlayerLocale(currentPlayer.getId(), localeType);
-
-            currentPlayer = currentPlayer.withLocale(localeType);
-
-            //TODO:Language name should be localized!
-            System.out.println("Language changed to " + localeType.getLanguageName());
-
-            //TODO: Think about this one! Maybe it's better to move it to the other method, or even other class. Maybe FirstLaunchLanguageSelection should handle it
-            LocalizationContext localizationContext = new LocalizationContext(localeType);
-            AppContext appContext = new AppContext(localizationContext, this.appContext.services(),
-                    this.appContext.printer(), this.appContext.parser(), this.currentPlayer);
-
-            return new SettingsMenu(appContext);
-        }
+        return new LanguageSelectionMenu(appContext);
     }
 
     private Menu changeLogoColor() {
