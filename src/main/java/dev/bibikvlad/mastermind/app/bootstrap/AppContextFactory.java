f936f3958bc4f257e032b627b6d2a65379f9902e
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-22 07:07:56 +0300

    Add AppContextFactory

---
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/AppContextFactory.java
@@ -0,0 +1,9 @@
+package dev.bibikvlad.mastermind.app.bootstrap;
+
+public class AppContextFactory {
+    private final AppContext appContext;
+
+    public AppContextFactory(AppContext appContext) {
+        this.appContext = appContext;
+    }
+}
