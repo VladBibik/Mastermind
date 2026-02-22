2095a28db562c497dd652bb26f159bf2d84fe571
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-22 07:24:39 +0300

    Add recreateWithLocale and recreateForNewPlayer methods

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/AppContextFactory.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/AppContextFactory.java
@@ -1,9 +1,26 @@
 package dev.bibikvlad.mastermind.app.bootstrap;
 
+import dev.bibikvlad.mastermind.localization.config.LocaleType;
+import dev.bibikvlad.mastermind.localization.core.LocalizationContext;
+import dev.bibikvlad.mastermind.persistence.player.model.Player;
+
 public class AppContextFactory {
     private final AppContext appContext;
 
     public AppContextFactory(AppContext appContext) {
         this.appContext = appContext;
+    }
+
+    public AppContext recreateWithLocale(LocaleType localeType) {
+        Player updatedPlayer = appContext.currentPlayer().withLocale(localeType);
+        LocalizationContext newLocalizationContext = new LocalizationContext(localeType);
+
+        return new AppContext(newLocalizationContext, this.appContext.services(),
+                this.appContext.printer(), this.appContext.parser(), updatedPlayer);
+    }
+
+    public AppContext recreateForNewPlayer(Player newPlayer) {
+        return new AppContext(this.appContext.localizationContext(), this.appContext.services(),
+                this.appContext.printer(), this.appContext.parser(), newPlayer);
     }
 }
