a6be374b8520a7b00483df6ba914981294ae1ff5
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-23 09:46:52 +0300

    Remove index field

--- src/main/java/dev/bibikvlad/mastermind/localization/config/LocaleType.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/config/LocaleType.java
@@ -6,38 +6,25 @@ import java.util.Map;
 import java.util.Optional;
 
 public enum LocaleType {
-    ENGLISH(1, Locale.ENGLISH),
-    RUSSIAN(2, Locale.of("ru"));
+    ENGLISH(Locale.ENGLISH),
+    RUSSIAN(Locale.of("ru"));
 
-    private final int localeIndex;
     private final Locale locale;
 
-    private static final Map<Integer, LocaleType> BY_INDEX = new HashMap<>();
     private static final Map<String, LocaleType> BY_LOCALE_TAG = new HashMap<>();
 
     static {
         for (LocaleType localeType : LocaleType.values()) {
-            BY_INDEX.put(localeType.getLocaleIndex(), localeType);
             BY_LOCALE_TAG.put(localeType.getLocale().getLanguage().toLowerCase(), localeType);
         }
     }
 
-    LocaleType(int localeIndex, Locale locale) {
-        this.localeIndex = localeIndex;
+    LocaleType(Locale locale) {
         this.locale = locale;
-    }
-
-    public int getLocaleIndex() {
-        return localeIndex;
     }
 
     public Locale getLocale() {
         return locale;
-    }
-
-    public static LocaleType fromLocaleIndex(int localeIndex) {
-        return Optional.ofNullable(BY_INDEX.get(localeIndex)).orElseThrow(
-                () -> new IllegalArgumentException("Invalid LocaleIndex: " + localeIndex));
     }
 
     public static LocaleType fromLocale(Locale locale) {
