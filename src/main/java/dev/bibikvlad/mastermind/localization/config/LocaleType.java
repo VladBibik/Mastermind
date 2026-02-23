668eebc43728cbfdeb0fe4aaaa1bcce0e7e3d3ac
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-23 09:50:10 +0300

    Remove map and retrieval logic

--- src/main/java/dev/bibikvlad/mastermind/localization/config/LocaleType.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/config/LocaleType.java
@@ -1,9 +1,6 @@
 package dev.bibikvlad.mastermind.localization.config;
 
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
 
 public enum LocaleType {
     ENGLISH(Locale.ENGLISH),
@@ -11,24 +8,11 @@ public enum LocaleType {
 
     private final Locale locale;
 
-    private static final Map<String, LocaleType> BY_LOCALE_TAG = new HashMap<>();
-
-    static {
-        for (LocaleType localeType : LocaleType.values()) {
-            BY_LOCALE_TAG.put(localeType.getLocale().getLanguage().toLowerCase(), localeType);
-        }
-    }
-
     LocaleType(Locale locale) {
         this.locale = locale;
     }
 
     public Locale getLocale() {
         return locale;
-    }
-
-    public static LocaleType fromLocale(Locale locale) {
-        return Optional.ofNullable(BY_LOCALE_TAG.get(locale.getLanguage().toLowerCase())).orElseThrow(
-                () -> new IllegalArgumentException("Invalid Locale: " + locale));
     }
 }
