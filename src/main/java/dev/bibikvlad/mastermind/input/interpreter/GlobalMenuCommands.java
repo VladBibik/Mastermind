1e587f2f44ffc4cd488091c6914b1be219e077c0
BibikVlad <bibikvladislav.work@gmail.com>
2026-02-27 14:13:49 +0300

    Add GlobalMenuCommands class

---
+++ src/main/java/dev/bibikvlad/mastermind/input/interpreter/GlobalMenuCommands.java
@@ -0,0 +1,29 @@
+package dev.bibikvlad.mastermind.input.interpreter;
+
+import java.util.Set;
+
+public class GlobalMenuCommands {
+    public static final Set<String> PLAY = Set.of(
+            "play",
+            "",
+            "start",
+            "go",
+            "mastermind",
+            "1",
+            "run",
+            "new",
+            "new game",
+            "launch",
+            "execute"
+    );
+    public static final Set<String> EXIT = Set.of(
+            "exit",
+            "close",
+            "quit",
+            "back",
+            "return",
+            "leave",
+            "q",
+            "0"
+    );
+}
