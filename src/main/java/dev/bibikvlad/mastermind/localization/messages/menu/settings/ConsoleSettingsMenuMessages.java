16441c9ed634c1b1412562105280c9b5194ab32f
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-01 20:57:28 +0300

    Add ConsoleSettingsMenuMessages class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/settings/ConsoleSettingsMenuMessages.java
@@ -0,0 +1,16 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.settings;
+
+import java.util.ResourceBundle;
+
+public class ConsoleSettingsMenuMessages implements SettingsMenuMessages {
+    private final ResourceBundle resourceBundle;
+
+    public ConsoleSettingsMenuMessages(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
+    @Override
+    public String getMenuOptions() {
+        return resourceBundle.getString("menu_options");
+    }
+}
