ae5776a251adea44998be9a8819784f05cf54f78
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-01 21:06:27 +0300

    Add ConsoleSettingsMessageFactory class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/factories/ConsoleSettingsMessageFactory.java
@@ -0,0 +1,14 @@
+package dev.bibikvlad.mastermind.localization.factories;
+
+import dev.bibikvlad.mastermind.localization.core.MessageFactory;
+import dev.bibikvlad.mastermind.localization.messages.menu.settings.ConsoleSettingsMenuMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.settings.SettingsMenuMessages;
+
+import java.util.ResourceBundle;
+
+public class ConsoleSettingsMessageFactory implements MessageFactory<SettingsMenuMessages> {
+    @Override
+    public SettingsMenuMessages create(ResourceBundle resourceBundle) {
+        return new ConsoleSettingsMenuMessages(resourceBundle);
+    }
+}
