94da0eeb54d2d715fd807392f30b74302a411659
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-02 20:14:11 +0300

    Add empty BackgroundColorSelectionMenu class

---
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/logo/color/BackgroundColorSelectionMenu.java
@@ -0,0 +1,15 @@
+package dev.bibikvlad.mastermind.menu.settings.logo.color;
+
+import dev.bibikvlad.mastermind.app.context.AppContext;
+import dev.bibikvlad.mastermind.menu.Menu;
+
+public class BackgroundColorSelectionMenu extends Menu {
+    public BackgroundColorSelectionMenu(AppContext appContext) {
+        super(appContext);
+    }
+
+    @Override
+    public Menu run() {
+        return null;
+    }
+}
