e2274185079da83b220e7e3d27b93db426b96c37
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-02 13:47:52 +0300

    Beautify code

--- src/main/java/dev/bibikvlad/mastermind/menu/settings/SettingsMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/SettingsMenu.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class SettingsMenu extends Menu {
     private final Printer printer;
     private final SettingsMenuMessages settingsMenuMessages;
-    private final InteractionMessages  interactionMessages;
+    private final InteractionMessages interactionMessages;
 
     private boolean showMenuOnNextLoop = true;
 
