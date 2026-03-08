d0d0691b8a06e600ee01fc26cff46235d54295af
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-08 23:04:04 +0300

    Add ConsoleLogoColorMessages class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/settings/logo/ConsoleLogoColorMessages.java
@@ -0,0 +1,16 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.settings.logo;
+
+import java.util.ResourceBundle;
+
+public class ConsoleLogoColorMessages implements LogoColorSelectionMessages {
+    private final ResourceBundle resourceBundle;
+
+    public ConsoleLogoColorMessages(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
+    @Override
+    public String getColorReturn() {
+        return resourceBundle.getString("color.return");
+    }
+}
