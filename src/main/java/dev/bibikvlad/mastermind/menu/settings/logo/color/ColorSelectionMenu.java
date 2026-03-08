d4b59595b8a0dad65d022e928f28f6aa7314f7e8
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-08 22:39:54 +0300

    Refactor index selection logic

--- src/main/java/dev/bibikvlad/mastermind/menu/settings/logo/color/ColorSelectionMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/settings/logo/color/ColorSelectionMenu.java
@@ -2,13 +2,14 @@ package dev.bibikvlad.mastermind.menu.settings.logo.color;
 
 import dev.bibikvlad.mastermind.app.context.AppContext;
 import dev.bibikvlad.mastermind.app.printer.Printer;
+import dev.bibikvlad.mastermind.input.interpreter.IntegerInputInterpreter;
 import dev.bibikvlad.mastermind.input.parser.Parser;
-import dev.bibikvlad.mastermind.input.validation.StringEmptyValidator;
 import dev.bibikvlad.mastermind.localization.config.MessageType;
 import dev.bibikvlad.mastermind.localization.messages.menu.settings.logo.LogoMessages;
 import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
 import dev.bibikvlad.utils.strings.GameCluesConstants;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 public class ColorSelectionMenu {
@@ -38,30 +39,12 @@ public class ColorSelectionMenu {
         while (true) {
             printer.printMessage("Enter '0' to return to the Logo Color Selection Menu");
 
-            String userInput = parser.parseUserInput();
-
-            if (StringEmptyValidator.isNullOrEmpty(userInput)) {
-                printer.printMessage("Input cannot be empty. Please try again.");
-
-                continue;
-            }
-
-            if (userInput.equals("exit") || userInput.equals("quit")) {
-                return null;
-            }
-
-            int userInputNumber;
+            Optional<Integer> selection = IntegerInputInterpreter.readSelection(parser);
 
             try {
-                userInputNumber = Integer.parseInt(userInput);
-            } catch (NumberFormatException exception) {
-                printer.printMessage("Invalid input. Please enter a number corresponding to the color option.");
-
-                continue;
-            }
-
-            try {
-                return colorSelector.apply(userInputNumber);
+                return selection
+                        .map(colorSelector)
+                        .orElse(null);
             } catch (IllegalArgumentException exception) {
                 printer.printMessage("Invalid input. Please enter a number corresponding to the color option.");
             }
