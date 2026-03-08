ddc1b520b4f15b9e2c5db7f3bffadaa0aa759a0c
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-08 23:02:32 +0300

    Add LogoColorSelectionMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/settings/logo/LogoColorSelectionMessages.java
@@ -0,0 +1,7 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.settings.logo;
+
+import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+
+public interface LogoColorSelectionMessages extends LocalizedMessages {
+    String getColorReturn();
+}
