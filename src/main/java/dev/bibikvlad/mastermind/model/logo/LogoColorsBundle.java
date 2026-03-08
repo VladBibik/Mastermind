4a517a85bc0dcfb15f01ecdb70df86e429df6e18
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-08 14:25:41 +0300

    Add withX methods

--- src/main/java/dev/bibikvlad/mastermind/model/logo/LogoColorsBundle.java
+++ src/main/java/dev/bibikvlad/mastermind/model/logo/LogoColorsBundle.java
@@ -4,4 +4,20 @@ import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
 
 public record LogoColorsBundle(ConsoleColor logoBorderColor, ConsoleColor logoMainColor, ConsoleColor logoAccentColor,
                                ConsoleColor logoBackgroundColor) {
+
+    public LogoColorsBundle withLogoBorderColor(ConsoleColor borderColor) {
+        return new LogoColorsBundle(borderColor, logoMainColor, logoAccentColor, logoBackgroundColor);
+    }
+
+    public LogoColorsBundle withLogoMainColor(ConsoleColor mainColor) {
+        return new LogoColorsBundle(logoBorderColor, mainColor, logoAccentColor, logoBackgroundColor);
+    }
+
+    public LogoColorsBundle withLogoAccentColor(ConsoleColor accentColor) {
+        return new LogoColorsBundle(logoBorderColor, logoMainColor, accentColor, logoBackgroundColor);
+    }
+
+    public LogoColorsBundle withLogoBackgroundColor(ConsoleColor backgroundColor) {
+        return new LogoColorsBundle(logoBorderColor, logoMainColor, logoAccentColor, backgroundColor);
+    }
 }
