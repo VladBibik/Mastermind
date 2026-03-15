b1827d9b09ba5f8ff07d64c4c796762824e7539b
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-15 19:30:02 +0300

    Refactor message formatting logic

--- src/main/java/dev/bibikvlad/mastermind/localization/messages/game/ConsoleGameMessages.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/game/ConsoleGameMessages.java
@@ -8,6 +8,7 @@ import dev.bibikvlad.utils.strings.Emojis;
 import dev.bibikvlad.utils.strings.GameCluesConstants;
 import dev.bibikvlad.utils.strings.logos.ColoredAsciiLogo;
 
+import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 public class ConsoleGameMessages implements GameMessages {
@@ -19,41 +20,37 @@ public class ConsoleGameMessages implements GameMessages {
 
     @Override
     public String getInvalidInputMessage() {
-        return resourceBundle.getString("invalid_input")
-                .replace("{VALID_SYMBOLS}", ConsoleColoredValidSymbols.getSymbols());
+        return MessageFormat.format(resourceBundle.getString("invalid_input"),
+                ConsoleColoredValidSymbols.getSymbols());
     }
 
     @Override
     public String getIncorrectGuessMessage(int maxTurns, int currentTurn, String answer, String userInput) {
-        return resourceBundle.getString("incorrect_guess")
-                .replace("{MAX_TURNS}", String.valueOf(maxTurns))
-                .replace("{CURRENT_TURN}", String.valueOf(currentTurn + 1))
-                .replace("{USER_INPUT}", userInput)
-                .replace("{VISUAL_REPRESENTATION}", ClueGenerator.generate(answer, userInput));
+        return MessageFormat.format(resourceBundle.getString("incorrect_guess"),
+                String.valueOf(currentTurn + 1), String.valueOf(maxTurns), userInput,
+                ClueGenerator.generate(answer, userInput));
     }
 
     @Override
     public String getGameOverMessage(String answer) {
-        return resourceBundle.getString("game_over")
-                .replace("{VISUAL_REPRESENTATION}", InputVisualRepresentation.getVisualRepresentation(answer));
+        return MessageFormat.format(resourceBundle.getString("game_over"),
+                InputVisualRepresentation.getVisualRepresentation(answer));
     }
 
     @Override
     public String getWinMessage(String answer) {
-        return resourceBundle.getString("win")
-                .replace("{TADA_EMOJI}", Emojis.CELEBRATION_TADA)
-                .replace("{ANSWER}", InputVisualRepresentation.getVisualRepresentation(answer));
+        return MessageFormat.format(resourceBundle.getString("win"), Emojis.CELEBRATION_TADA,
+                InputVisualRepresentation.getVisualRepresentation(answer));
     }
 
     @Override
     public String getRulesMessage() {
-        return resourceBundle.getString("rules")
-                .replace("{VALID_SYMBOLS}", ConsoleColoredValidSymbols.getSymbols())
-                .replace("{CIRCLE_SHADED}", String.valueOf(GameCluesConstants.CIRCLE_SHADED))
-                .replace("{CIRCLE_EMPTY}", String.valueOf(GameCluesConstants.CIRCLE_EMPTY))
-                .replace("{UNDERSCORE}", String.valueOf(GameCluesConstants.UNDERSCORE));
+        return MessageFormat.format(resourceBundle.getString("rules"),
+                ConsoleColoredValidSymbols.getSymbols(), String.valueOf(GameCluesConstants.CIRCLE_SHADED),
+                String.valueOf(GameCluesConstants.CIRCLE_EMPTY), String.valueOf(GameCluesConstants.UNDERSCORE));
     }
 
+    //TODO: Move out AsciiLogo and delete methods!
     @Override
     public String getAsciiLogo(LogoColorsBundle logoColorsBundle) {
         return ColoredAsciiLogo.getLogo(logoColorsBundle);
