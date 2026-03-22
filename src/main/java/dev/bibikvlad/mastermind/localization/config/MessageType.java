42f9306ccc66bff081e9500e3a20c98a62c8f75d
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-22 12:58:07 +0300

    Add new Enum type for Time formatting

--- src/main/java/dev/bibikvlad/mastermind/localization/config/MessageType.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/config/MessageType.java
@@ -1,10 +1,11 @@
 package dev.bibikvlad.mastermind.localization.config;
 
 import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
-import dev.bibikvlad.mastermind.localization.messages.interaction.InteractionMessages;
+import dev.bibikvlad.mastermind.localization.messages.common.TimeFormattingMessages;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
-import dev.bibikvlad.mastermind.localization.messages.menu.main.game.GameMenuMessages;
+import dev.bibikvlad.mastermind.localization.messages.interaction.InteractionMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.MainMenuMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.main.game.GameMenuMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.settings.SettingsMenuMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.settings.language.LanguageSelectionMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.settings.logo.LogoColorSelectionMessages;
@@ -20,7 +21,8 @@ public enum MessageType {
     LANGUAGE_MENU(LanguageSelectionMessages.class),
     LOGO(LogoColorSelectionMessages.class),
     COLOR(ColorMessages.class),
-    INTERACTION(InteractionMessages.class);
+    INTERACTION(InteractionMessages.class),
+    TIME(TimeFormattingMessages.class);
 
     private final Class<? extends LocalizedMessages> messageClass;
 
