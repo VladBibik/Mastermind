df08c569d034b09944307d993ac17e8144ab25a7
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-22 14:42:13 +0300

    Refactor time variable Change it to long

--- src/main/java/dev/bibikvlad/mastermind/persistence/leaderboard/model/MainLeaderboardEntry.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/leaderboard/model/MainLeaderboardEntry.java
@@ -2,5 +2,5 @@ package dev.bibikvlad.mastermind.persistence.leaderboard.model;
 
 import dev.bibikvlad.mastermind.values.Time;
 
-public record MainLeaderboardEntry(String playerName, int numberOfTurns, Time gameDuration) {
+public record MainLeaderboardEntry(String playerName, int numberOfTurns, long gameDuration) {
 }
--- src/main/java/dev/bibikvlad/mastermind/persistence/leaderboard/model/TimeLeaderboardEntry.java
+++ src/main/java/dev/bibikvlad/mastermind/persistence/leaderboard/model/TimeLeaderboardEntry.java
@@ -2,5 +2,5 @@ package dev.bibikvlad.mastermind.persistence.leaderboard.model;
 
 import dev.bibikvlad.mastermind.values.Time;
 
-public record TimeLeaderboardEntry(String playerName, Time gameDuration) {
+public record TimeLeaderboardEntry(String playerName, long gameDuration) {
 }
