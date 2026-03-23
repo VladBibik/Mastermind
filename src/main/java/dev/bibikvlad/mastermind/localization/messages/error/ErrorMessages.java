be1b7cf3281aec4ce613dd01e856fa919cb74d8b
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-23 19:03:21 +0300

    Refactor getStatsNotFoundMessage method Add playerId parameter

--- src/main/java/dev/bibikvlad/mastermind/localization/messages/error/ErrorMessages.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/error/ErrorMessages.java
@@ -3,5 +3,5 @@ package dev.bibikvlad.mastermind.localization.messages.error;
 import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
 
 public interface ErrorMessages extends LocalizedMessages {
-    String getStatsNotFoundMessage();
+    String getStatsNotFoundMessage(long playerId);
 }
