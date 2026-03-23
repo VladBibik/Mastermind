553eccdb5a83c896a68ebe34479afdcf526bb2f5
BibikVlad <bibikvladislav.work@gmail.com>
2026-03-23 16:26:50 +0300

    Refactor format method Move boolean logic out of if statements and expanded it to include 0 time measurements if higher value exist

--- src/main/java/dev/bibikvlad/utils/formatters/TimeToStringFormatter.java
+++ src/main/java/dev/bibikvlad/utils/formatters/TimeToStringFormatter.java
@@ -7,12 +7,16 @@ public class TimeToStringFormatter {
     public static String format(Time time, TimeFormattingMessages messages) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        if (time.hour() > 0) stringBuilder.append(messages.getHours(time.hour())).append(" ");
-        if (time.minutes() > 0) stringBuilder.append(messages.getMinutes(time.minutes())).append(" ");
-        if (time.second() > 0) stringBuilder.append(messages.getSeconds(time.second())).append(" ");
+        boolean hasHours = time.hour() > 0;
+        boolean hasMinutes = hasHours || time.minutes() > 0;
+        boolean hasSeconds = hasMinutes || time.second() > 0;
+
+        if (hasHours) stringBuilder.append(messages.getHours(time.hour())).append(" ");
+        if (hasMinutes) stringBuilder.append(messages.getMinutes(time.minutes())).append(" ");
+        if (hasSeconds) stringBuilder.append(messages.getSeconds(time.second())).append(" ");
 
         stringBuilder.append(messages.getMilliseconds(time.millisecond()));
 
-        return stringBuilder.toString().trim();
+        return stringBuilder.toString();
     }
 }
