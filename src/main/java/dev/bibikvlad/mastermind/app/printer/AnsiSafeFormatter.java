8173fbab80f6045401c9b9a17218e1d6d6a92cc4
BibikVlad <bibikvladislav.work@gmail.com>
2026-04-05 15:00:53 +0300

    Add AnsiSafeFormatter class Add format method

---
+++ src/main/java/dev/bibikvlad/mastermind/app/printer/AnsiSafeFormatter.java
@@ -0,0 +1,11 @@
+package dev.bibikvlad.mastermind.app.printer;
+
+import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
+
+public class AnsiSafeFormatter {
+    private static final String RESET = ConsoleColor.RESET.getCode();
+
+    public static String format(String message) {
+        return message + RESET;
+    }
+}
