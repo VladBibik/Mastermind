eafc998b9d06717bbadd52e3df78272e66bfe77a
BibikVlad <bibikvladislav.work@gmail.com>
2026-04-07 12:05:03 +0300

    Delete Name normalization logic

--- src/main/java/dev/bibikvlad/mastermind/model/player/utils/PlayerNameNormalizer.java
+++
@@ -1,9 +0,0 @@
-package dev.bibikvlad.mastermind.model.player.utils;
-
-public class PlayerNameNormalizer {
-    public static String normalize(String name) {
-        return name.replace("\n", "")
-                .replace("\r", "")
-                .replace("\t", "");
-    }
-}
