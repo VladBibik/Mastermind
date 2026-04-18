1f5e4667ebc497ef1a7f7c7927f1f263207c51f2
BibikVlad <bibikvladislav.work@gmail.com>
2026-04-19 00:39:42 +0300

    Add empty WinPercentageLeaderboardMenu class

---
+++ src/main/java/dev/bibikvlad/mastermind/menu/main/leaderboards/percentage/WinPercentageLeaderboardMenu.java
@@ -0,0 +1,15 @@
+package dev.bibikvlad.mastermind.menu.main.leaderboards.percentage;
+
+import dev.bibikvlad.mastermind.app.context.AppContext;
+import dev.bibikvlad.mastermind.menu.core.Menu;
+
+public class WinPercentageLeaderboardMenu extends Menu {
+    public WinPercentageLeaderboardMenu(AppContext appContext) {
+        super(appContext);
+    }
+
+    @Override
+    public Menu run() {
+        return null;
+    }
+}
