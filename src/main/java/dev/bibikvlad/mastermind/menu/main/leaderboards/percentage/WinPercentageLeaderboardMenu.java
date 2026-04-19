0fe83ab6bec15f06352da5e74f26b87e4f724802
BibikVlad <bibikvladislav.work@gmail.com>
2026-04-19 13:54:44 +0300

    Add Printer and Parser fields

--- src/main/java/dev/bibikvlad/mastermind/menu/main/leaderboards/percentage/WinPercentageLeaderboardMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/main/leaderboards/percentage/WinPercentageLeaderboardMenu.java
@@ -1,15 +1,24 @@
 package dev.bibikvlad.mastermind.menu.main.leaderboards.percentage;
 
 import dev.bibikvlad.mastermind.app.context.AppContext;
+import dev.bibikvlad.mastermind.app.printer.Printer;
+import dev.bibikvlad.mastermind.input.parser.Parser;
 import dev.bibikvlad.mastermind.menu.core.Menu;
 
 public class WinPercentageLeaderboardMenu extends Menu {
+    private final Printer printer;
+    private final Parser parser;
+
     public WinPercentageLeaderboardMenu(AppContext appContext) {
         super(appContext);
+
+        this.printer = appContext.printer();
+        this.parser = appContext.parser();
     }
 
     @Override
     public Menu run() {
         return null;
     }
+
 }
