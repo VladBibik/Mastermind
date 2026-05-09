0212a8ab2384e9e161a4acbe7b32aae130e85fb9
BibikVlad <bibikvladislav.work@gmail.com>
2026-05-09 13:11:03 +0300

    Add ProfileMenuMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/ProfileMenuMessages.java
@@ -0,0 +1,9 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile;
+
+import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+
+public interface ProfileMenuMessages extends LocalizedMessages {
+    String getMenuOptions();
+
+    String getSwitchError();
+}
