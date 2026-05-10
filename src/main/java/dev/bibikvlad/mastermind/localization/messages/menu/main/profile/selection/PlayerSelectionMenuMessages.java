06f6720da29bba58851188072a18bc0747f30ee0
BibikVlad <bibikvladislav.work@gmail.com>
2026-05-10 18:22:29 +0300

    Add PlayerSelectionMenuMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/selection/PlayerSelectionMenuMessages.java
@@ -0,0 +1,7 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile.selection;
+
+public interface PlayerSelectionMenuMessages {
+    String getBackMenuOption();
+
+    String getPlayerSelected(String playerName);
+}
