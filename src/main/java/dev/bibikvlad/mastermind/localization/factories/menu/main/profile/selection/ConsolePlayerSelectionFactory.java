eba8ab57b6dea585072234b357021acc47027ce0
BibikVlad <bibikvladislav.work@gmail.com>
2026-05-13 00:06:26 +0300

    Add ConsolePlayerSelectionFactory class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/factories/menu/main/profile/selection/ConsolePlayerSelectionFactory.java
@@ -0,0 +1,14 @@
+package dev.bibikvlad.mastermind.localization.factories.menu.main.profile.selection;
+
+import dev.bibikvlad.mastermind.localization.core.MessageFactory;
+import dev.bibikvlad.mastermind.localization.messages.menu.main.profile.selection.ConsoleSelectionMenuMessages;
+import dev.bibikvlad.mastermind.localization.messages.menu.main.profile.selection.PlayerSelectionMenuMessages;
+
+import java.util.ResourceBundle;
+
+public class ConsolePlayerSelectionFactory implements MessageFactory<PlayerSelectionMenuMessages> {
+    @Override
+    public PlayerSelectionMenuMessages create(ResourceBundle resourceBundle) {
+        return new ConsoleSelectionMenuMessages(resourceBundle);
+    }
+}
