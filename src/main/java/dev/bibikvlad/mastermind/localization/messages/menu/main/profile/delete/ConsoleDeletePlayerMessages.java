1e028a5541de56c5fc5557302a42a1ff147441eb
BibikVlad <bibikvladislav.work@gmail.com>
2026-05-24 17:26:07 +0300

    Add ConsoleDeletePlayerMessages class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/delete/ConsoleDeletePlayerMessages.java
@@ -0,0 +1,48 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile.delete;
+
+import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public class ConsoleDeletePlayerMessages implements DeletePlayerMenuMessages {
+    private static final String RED_CONSOLE_CODE = ConsoleColor.RED.getCode();
+    private static final String CONSOLE_RESET_CODE = ConsoleColor.RESET.getCode();
+
+    private final ResourceBundle resourceBundle;
+
+    public ConsoleDeletePlayerMessages(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
+    @Override
+    public String getFirstWarning(String playerName) {
+        String redWarning = RED_CONSOLE_CODE + resourceBundle.getString("first_warning_color")
+                + CONSOLE_RESET_CODE;
+
+        return redWarning + MessageFormat.format(resourceBundle.getString("first_warning_message"), playerName);
+    }
+
+    @Override
+    public String getDeleteConfirmation(String playerName) {
+        String redWarning = RED_CONSOLE_CODE + resourceBundle.getString("delete_confirmation_color")
+                + CONSOLE_RESET_CODE;
+
+        return redWarning + MessageFormat.format(resourceBundle.getString("delete_confirmation"), playerName);
+    }
+
+    @Override
+    public String getPlayerDeleted(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_deleted"), playerName);
+    }
+
+    @Override
+    public String getPlayerAutoSelected(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_auto_selected"), playerName);
+    }
+
+    @Override
+    public String getAutoSelectionWarning(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("automatic_player_selection_warning"), playerName);
+    }
+}
