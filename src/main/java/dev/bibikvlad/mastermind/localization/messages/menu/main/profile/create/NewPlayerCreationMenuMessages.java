68728dcedf2c3b2b2bb2c666b4396069259db4c7
BibikVlad <bibikvladislav.work@gmail.com>
2026-05-31 18:55:01 +0300

    Add NewPlayerCreationMenuMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/create/NewPlayerCreationMenuMessages.java
@@ -0,0 +1,15 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile.create;
+
+import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+
+public interface NewPlayerCreationMenuMessages extends LocalizedMessages {
+    String getNewPlayerTitle();
+
+    String getPlayerCreatedSuccess(String playerName);
+
+    String getPlayerAlreadyExistsError(String playerName);
+
+    String getPlayerNameEmptyError(String playerName);
+
+    String getPlayerNameLengthError(String playerName);
+}
