254c26fe06ff6cfb1b58ac9b4b21793e0d49ee59
BibikVlad <bibikvladislav.work@gmail.com>
2026-05-31 21:16:16 +0300

    Add ConsoleNewPlayerCreationMessages class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/create/ConsoleNewPlayerCreationMessages.java
@@ -0,0 +1,37 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile.create;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public class ConsoleNewPlayerCreationMessages implements NewPlayerCreationMenuMessages {
+    private final ResourceBundle resourceBundle;
+
+    public ConsoleNewPlayerCreationMessages(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
+    @Override
+    public String getNewPlayerTitle() {
+        return resourceBundle.getString("new_player_title");
+    }
+
+    @Override
+    public String getPlayerCreatedSuccess(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_created_success"), playerName);
+    }
+
+    @Override
+    public String getPlayerAlreadyExistsError(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_already_exists_error"), playerName);
+    }
+
+    @Override
+    public String getPlayerNameEmptyError(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_name_empty_error"), playerName);
+    }
+
+    @Override
+    public String getPlayerNameLengthError(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_name_length_error"), playerName);
+    }
+}
