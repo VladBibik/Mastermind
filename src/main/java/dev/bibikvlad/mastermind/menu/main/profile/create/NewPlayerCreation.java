2e21aac39a1ca50f88a5cd39a109138e7cd1a6a6
BibikVlad <bibikvladislav.work@gmail.com>
2026-06-07 02:04:39 +0300

    Rename newPlayerCreationMenuMessages -> creationMessages field

--- src/main/java/dev/bibikvlad/mastermind/menu/main/profile/create/NewPlayerCreation.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/main/profile/create/NewPlayerCreation.java
@@ -5,7 +5,6 @@ import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.exceptions.PlayerAlreadyExistException;
 import dev.bibikvlad.mastermind.input.parser.Parser;
 import dev.bibikvlad.mastermind.input.validation.PlayerNameValidator;
-import dev.bibikvlad.mastermind.input.validation.StringEmptyValidator;
 import dev.bibikvlad.mastermind.localization.config.LocaleType;
 import dev.bibikvlad.mastermind.localization.config.MessageType;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.profile.create.NewPlayerCreationMenuMessages;
@@ -18,7 +17,7 @@ public class NewPlayerCreation extends Menu {
     private final Printer printer;
     private final Parser parser;
     private final PlayerService playerService;
-    private final NewPlayerCreationMenuMessages newPlayerCreationMenuMessages;
+    private final NewPlayerCreationMenuMessages creationMessages;
 
     public NewPlayerCreation(AppContext appContext) {
         super(appContext);
@@ -26,12 +25,12 @@ public class NewPlayerCreation extends Menu {
         this.printer = appContext.printer();
         this.parser = appContext.parser();
         this.playerService = appContext.services().getPlayerService();
-        this.newPlayerCreationMenuMessages = appContext.localizationContext().getMessages(MessageType.CREATE);
+        this.creationMessages = appContext.localizationContext().getMessages(MessageType.CREATE);
     }
 
     @Override
     public Menu run() {
-        printer.printMessage(newPlayerCreationMenuMessages.getNewPlayerTitle());
+        printer.printMessage(creationMessages.getNewPlayerTitle());
 
         String newPlayerName = parser.parseUserInput();
 
@@ -50,18 +49,18 @@ public class NewPlayerCreation extends Menu {
             AppContext appContext = new AppContext(this.appContext.localizationContext(), this.appContext.services(),
                     this.appContext.printer(), this.appContext.parser(), createdPlayer);
 
-            printer.printMessage(newPlayerCreationMenuMessages.getPlayerCreatedSuccess(newPlayerName));
+            printer.printMessage(creationMessages.getPlayerCreatedSuccess(newPlayerName));
 
             return new ProfileMenu(appContext);
         } catch (PlayerAlreadyExistException exception) {
-            printer.printMessage(newPlayerCreationMenuMessages.getPlayerAlreadyExistsError(newPlayerName));
+            printer.printMessage(creationMessages.getPlayerAlreadyExistsError(newPlayerName));
         }
 
         return this;
     }
 
     private boolean isPlayerNameValid(String newPlayerName) {
-        PlayerNameValidator validator = new PlayerNameValidator(printer, newPlayerCreationMenuMessages);
+        PlayerNameValidator validator = new PlayerNameValidator(printer, creationMessages);
 
         return validator.validate(newPlayerName);
     }
