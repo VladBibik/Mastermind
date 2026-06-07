71b2b6b2ef547487e7c003070035ab921338ed21
BibikVlad <bibikvladislav.work@gmail.com>
2026-06-07 15:27:11 +0300

    Refactor PlayerCreationInputInterpreter class Change return type and logic of readSelection method

--- src/main/java/dev/bibikvlad/mastermind/input/interpreter/PlayerCreationInputInterpreter.java
+++ src/main/java/dev/bibikvlad/mastermind/input/interpreter/PlayerCreationInputInterpreter.java
@@ -2,16 +2,14 @@ package dev.bibikvlad.mastermind.input.interpreter;
 
 import dev.bibikvlad.mastermind.input.parser.Parser;
 
-import java.util.Optional;
-
 public class PlayerCreationInputInterpreter {
-    public static Optional<String> readSelection(Parser parser) {
+    public static PlayerCreationSelection readSelection(Parser parser) {
         String userInput = parser.parseUserInput().trim().toLowerCase();
 
         if (GlobalMenuCommands.EXIT.contains(userInput)) {
-            return Optional.empty();
+            return new PlayerCreationSelection(true, userInput);
         } else {
-            return Optional.of(userInput);
+            return new PlayerCreationSelection(false, userInput);
         }
     }
 }
