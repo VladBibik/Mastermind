d18b5d1474d6e331f75ea4d525329f9bb995357b
BibikVlad <bibikvladislav.work@gmail.com>
2026-06-10 19:08:38 +0300

    Fix user input correction Refactor exit command logic

--- src/main/java/dev/bibikvlad/mastermind/input/interpreter/PlayerCreationInputInterpreter.java
+++ src/main/java/dev/bibikvlad/mastermind/input/interpreter/PlayerCreationInputInterpreter.java
@@ -4,9 +4,10 @@ import dev.bibikvlad.mastermind.input.parser.Parser;
 
 public class PlayerCreationInputInterpreter {
     public static PlayerCreationInput readSelection(Parser parser) {
-        String userInput = parser.parseUserInput().trim().toLowerCase();
+        String userInput = parser.parseUserInput();
 
-        if (GlobalMenuCommands.EXIT.contains(userInput)) {
+        //TODO: Think if we should use trim and toLowerCase here!
+        if (GlobalMenuCommands.EXIT.contains(userInput.trim().toLowerCase())) {
             return new PlayerCreationInput(true, userInput);
         } else {
             return new PlayerCreationInput(false, userInput);
