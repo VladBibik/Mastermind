4cdfe3335a5453983f1b9f4b5de1544acae87664
BibikVlad <bibikvladislav.work@gmail.com>
2026-06-23 16:18:02 +0300

    Add ConsolePlayerNameMessages class

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/name/ConsolePlayerNameMessages.java
@@ -0,0 +1,32 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile.name;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public class ConsolePlayerNameMessages implements PlayerNameMessages {
+    private final ResourceBundle resourceBundle;
+
+    public ConsolePlayerNameMessages(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
+    @Override
+    public String getAlreadyExistsError(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("player_already_exists_error"), playerName);
+    }
+
+    @Override
+    public String getNameEmptyError() {
+        return MessageFormat.format(resourceBundle.getString("player_name_empty_error"));
+    }
+
+    @Override
+    public String getPlayerNameLengthError() {
+        return MessageFormat.format(resourceBundle.getString("player_name_length_error"));
+    }
+
+    @Override
+    public String getReservedCommandConfirmation(String playerName) {
+        return MessageFormat.format(resourceBundle.getString("reserved_command_confirmation"), playerName);
+    }
+}
