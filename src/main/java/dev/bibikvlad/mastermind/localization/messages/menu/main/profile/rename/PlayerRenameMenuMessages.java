689d4db1a487b57bda893fdba19e38470600c3f9
BibikVlad <bibikvladislav.work@gmail.com>
2026-06-28 02:58:31 +0300

    Add PlayerRenameMenuMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/profile/rename/PlayerRenameMenuMessages.java
@@ -0,0 +1,9 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.profile.rename;
+
+import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+
+public interface PlayerRenameMenuMessages extends LocalizedMessages {
+    String getPlayerNameRenameTitle(String playerName);
+
+    String getPlayerNameRenameSuccess(String playerName);
+}
