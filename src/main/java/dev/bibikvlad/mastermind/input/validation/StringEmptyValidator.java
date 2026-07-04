782914d2a36ad87c6277f1420d34f3b25dce5ed2
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-04 04:48:29 +0300

    Remove TODO

--- src/main/java/dev/bibikvlad/mastermind/input/validation/StringEmptyValidator.java
+++ src/main/java/dev/bibikvlad/mastermind/input/validation/StringEmptyValidator.java
@@ -1,6 +1,5 @@
 package dev.bibikvlad.mastermind.input.validation;
 
-//TODO: After all menu updates I think this class could be deleted!
 public class StringEmptyValidator {
     public static boolean isNullOrEmpty(String userInput) {
         return userInput == null || userInput.trim().isEmpty();
