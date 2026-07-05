53fa74b7a1287eff008bba0e0bc56bff73dc29a4
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-05 16:19:38 +0300

    Refactor LocaleTypeTest class to match current state of LocaleType enum

--- src/test/java/dev/bibikvlad/mastermind/localization/config/LocaleTypeTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/config/LocaleTypeTest.java
@@ -12,34 +12,14 @@ public class LocaleTypeTest {
     @Test
     @DisplayName("Returns correct locale type from locale index")
     void fromLocaleIndexTest() {
-        assertEquals(LocaleType.ENGLISH, LocaleType.fromLocaleIndex(1));
-        assertEquals(LocaleType.RUSSIAN, LocaleType.fromLocaleIndex(2));
-    }
-
-    @Test
-    @DisplayName("Returns correct locale type from language string")
-    void fromLanguageStringTest() {
-        assertEquals(LocaleType.ENGLISH, LocaleType.fromLanguageString("English"));
-        assertEquals(LocaleType.RUSSIAN, LocaleType.fromLanguageString("Russian"));
-
-        assertEquals(LocaleType.ENGLISH, LocaleType.fromLanguageString("ENGLISH"));
-        assertEquals(LocaleType.RUSSIAN, LocaleType.fromLanguageString("RUSSIAN"));
-
-        assertEquals(LocaleType.ENGLISH, LocaleType.fromLanguageString("EnGlIsH"));
-        assertEquals(LocaleType.RUSSIAN, LocaleType.fromLanguageString("rUssIan"));
-    }
-
-    @Test
-    @DisplayName("Returns correct locale type from locale")
-    void fromLocaleTest() {
-        assertEquals(LocaleType.ENGLISH, LocaleType.fromLocale(Locale.ENGLISH));
-        assertEquals(LocaleType.RUSSIAN, LocaleType.fromLocale(Locale.of("ru")));
+        assertEquals(LocaleType.ENGLISH, LocaleType.fromIndex(1));
+        assertEquals(LocaleType.RUSSIAN, LocaleType.fromIndex(2));
     }
 
     @Test
     @DisplayName("Throws exception on negative locale index")
     void negativeLocaleIndexThrowsException() {
-        assertThrows(IllegalArgumentException.class, () -> LocaleType.fromLocaleIndex(-1));
+        assertThrows(IllegalArgumentException.class, () -> LocaleType.fromIndex(-1));
     }
 
     @Test
@@ -48,7 +28,7 @@ public class LocaleTypeTest {
         for (int i = 3; i < 100; i++) {
             int index = i;
 
-            assertThrows(IllegalArgumentException.class, () -> LocaleType.fromLocaleIndex(index));
+            assertThrows(IllegalArgumentException.class, () -> LocaleType.fromIndex(index));
         }
     }
 }
