301a9ff45b1094b0bbd42a7b4e6a3220add4166d
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-12 19:13:57 +0300

    Refactor hardcoded string messages Extract them into separate methods

--- src/test/java/dev/bibikvlad/mastermind/localization/messages/game/ConsoleGameMessagesRuTest.java
+++ src/test/java/dev/bibikvlad/mastermind/localization/messages/game/ConsoleGameMessagesRuTest.java
@@ -25,60 +25,40 @@ class ConsoleGameMessagesRuTest {
     @DisplayName("Returns correct Invalid Input Message String")
     void testInvalidInputMessage() {
         String result = gameMessages.getInvalidInput();
-        String expected = "Неверный формат ответа. Ответ должен содержать только буквы: "
-                + ConsoleColoredValidSymbols.getSymbols();
 
-        assertEquals(result, expected);
+        assertEquals(result, getExpectedInvalidInput());
     }
 
     @Test
     @DisplayName("Returns correct Incorrect Guess Message String")
     void testIncorrectGuessMessage() {
         String result = gameMessages.getIncorrectGuess(10, 5, "rgby", "rbww");
-        String expected = "Ход: 6 из 10.\n" +
-                "Ваш ответ: rbww            " + ClueGenerator.generate("rgby", "rbww");
 
-        assertEquals(result, expected);
+        assertEquals(result, getExpectedIncorrectGuess());
     }
 
     @Test
     @DisplayName("Returns correct Game Over Message String")
     void testGameOverMessage() {
         String result = gameMessages.getGameOver("rgby");
-        String expected = "Game Over! Ответом была комбинация: "
-                + InputVisualRepresentation.getVisualRepresentation("rgby");
 
-        assertEquals(result, expected);
+        assertEquals(result, getExpectedGameOver());
     }
 
     @Test
     @DisplayName("Returns correct Win Message String")
     void testWinMessage() {
         String result = gameMessages.getWin("rgby");
-        String expected = "Вы победили! " + Emojis.CELEBRATION_TADA +
-                "\nВы Mastermind!\n" +
-                "Решением была комбинация: " + InputVisualRepresentation.getVisualRepresentation("rgby");
 
-        assertEquals(result, expected);
+        assertEquals(result, getExpectedWin());
     }
 
     @Test
     @DisplayName("Returns correct Rules Message String")
     void testRulesMessage() {
         String result = gameMessages.getRules();
-        String expected = "Пазл состоит из 4 ячеек. Каждый ход Вы выбираете из 6 цветов.\n"
-                + "Варианты цветов: " + ConsoleColoredValidSymbols.getSymbols() + "\n"
-                + "Пример хода: ybgr\n"
-                + "Подсказка:\n"
-                + GameCluesConstants.CIRCLE_SHADED + "   Правильный цвет в правильном положении\n"
-                + GameCluesConstants.CIRCLE_EMPTY + "   Правильный цвет в неправильном положении\n"
-                + GameCluesConstants.UNDERSCORE + "   Неправильный цвет\n"
-                + "\n"
-                + "Порядок символов в подсказке не обязательно совпадает с позицией цвета.\n"
-                + "Введите 'help', или 'rules', чтобы снова увидеть правила.\n"
-                + "Введите 'close', или 'exit', чтобы выйти из игры и увидеть ответ.\n";
 
-        assertEquals(result, expected);
+        assertEquals(result, getExpectedRules());
     }
 
     @Test
@@ -95,5 +75,40 @@ class ConsoleGameMessagesRuTest {
         String expected = ColoredAsciiLogo.getLogo(logoColorsBundle);
 
         assertEquals(result, expected);
+    }
+
+    String getExpectedInvalidInput() {
+        return "Неверный формат ответа. Ответ должен содержать только буквы: "
+                + ConsoleColoredValidSymbols.getSymbols();
+    }
+
+    String getExpectedIncorrectGuess() {
+        return "Ход: 6 из 10.\n" +
+                "Ваш ответ: rbww            " + ClueGenerator.generate("rgby", "rbww");
+    }
+
+    String getExpectedGameOver() {
+        return "Game Over! Ответом была комбинация: "
+                + InputVisualRepresentation.getVisualRepresentation("rgby");
+    }
+
+    String getExpectedWin() {
+        return "Вы победили! " + Emojis.CELEBRATION_TADA +
+                "\nВы Mastermind!\n" +
+                "Решением была комбинация: " + InputVisualRepresentation.getVisualRepresentation("rgby");
+    }
+
+    String getExpectedRules() {
+        return "Пазл состоит из 4 ячеек. Каждый ход Вы выбираете из 6 цветов.\n"
+                + "Варианты цветов: " + ConsoleColoredValidSymbols.getSymbols() + "\n"
+                + "Пример хода: ybgr\n"
+                + "Подсказка:\n"
+                + GameCluesConstants.CIRCLE_SHADED + "   Правильный цвет в правильном положении\n"
+                + GameCluesConstants.CIRCLE_EMPTY + "   Правильный цвет в неправильном положении\n"
+                + GameCluesConstants.UNDERSCORE + "   Неправильный цвет\n"
+                + "\n"
+                + "Порядок символов в подсказке не обязательно совпадает с позицией цвета.\n"
+                + "Введите 'help', или 'rules', чтобы снова увидеть правила.\n"
+                + "Введите 'close', или 'exit', чтобы выйти из игры и увидеть ответ.\n";
     }
 }
