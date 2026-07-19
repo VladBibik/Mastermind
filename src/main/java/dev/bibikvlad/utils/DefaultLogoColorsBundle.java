f158fda01b7c3db11d039b2f1bbc8001a65ffda3
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-19 17:25:32 +0300

    Refactor DefaultLogoColorsBundle Delete static method and replace it with static constant because there is no point in creating a new instance for a default value

--- src/main/java/dev/bibikvlad/utils/DefaultLogoColorsBundle.java
+++ src/main/java/dev/bibikvlad/utils/DefaultLogoColorsBundle.java
@@ -4,16 +4,14 @@ import dev.bibikvlad.mastermind.model.enums.ConsoleColor;
 import dev.bibikvlad.mastermind.model.logo.LogoColorsBundle;
 
 public class DefaultLogoColorsBundle {
+    public static final LogoColorsBundle INSTANCE = new LogoColorsBundle(
+            ConsoleColor.ORCHID,
+            ConsoleColor.ORANGE,
+            ConsoleColor.BRIGHT_RED,
+            ConsoleColor.BACKGROUND_BLACK
+    );
+
     private DefaultLogoColorsBundle() {
         throw new AssertionError("Utility class");
-    }
-
-    public static LogoColorsBundle get() {
-        return new LogoColorsBundle(
-                ConsoleColor.ORCHID,
-                ConsoleColor.ORANGE,
-                ConsoleColor.BRIGHT_RED,
-                ConsoleColor.BACKGROUND_BLACK
-        );
     }
 }
