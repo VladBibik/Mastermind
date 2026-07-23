c5f4436713fed06a118159afe1245aec3616f7cf
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-23 18:54:33 +0300

    Add isDevelopment method

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabasePathResolver.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabasePathResolver.java
@@ -1,7 +1,21 @@
 package dev.bibikvlad.mastermind.app.bootstrap.path;
 
+import dev.bibikvlad.mastermind.app.bootstrap.MastermindAppLauncher;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DatabasePathResolver {
     private boolean isNotContainer() {
         return System.getenv("MASTERMIND_RUNTIME").isEmpty();
+    }
+
+    private boolean isDevelopment() {
+        URL location = MastermindAppLauncher.class.getProtectionDomain().getCodeSource().getLocation();
+        Path path = Paths.get(location.getPath());
+
+        return Files.isDirectory(path);
     }
 }
