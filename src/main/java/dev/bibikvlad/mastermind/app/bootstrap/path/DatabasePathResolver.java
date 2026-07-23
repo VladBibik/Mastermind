cb44bc1bcfb088364d6d560d0c5ccb8a5269c84c
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-23 22:45:31 +0300

    Refactor DatabasePathResolver class Simplify class removing detection logic. Basically bootstrap class at this point

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabasePathResolver.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabasePathResolver.java
@@ -1,44 +1,15 @@
 package dev.bibikvlad.mastermind.app.bootstrap.path;
 
-import dev.bibikvlad.mastermind.app.bootstrap.MastermindAppLauncher;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DatabasePathResolver {
     public Path getDatabasePath() {
         DatabaseDeploymentModeFactory factory = new DatabaseDeploymentModeFactory();
-        DatabaseLocationProvider provider;
+        DeploymentModeDetector detector = new DeploymentModeDetector();
 
-        if (isContainer()) {
-            provider = factory.getProvider(DeploymentMode.CONTAINER);
-
-            return provider.getDatabasePath();
-        }
-
-        if (isDevelopment()) {
-            provider = factory.getProvider(DeploymentMode.DEVELOPMENT);
-        } else {
-            provider = factory.getProvider(DeploymentMode.PORTABLE);
-        }
+        DeploymentMode deploymentMode = detector.detect();
+        DatabaseLocationProvider provider = factory.getProvider(deploymentMode);
 
         return provider.getDatabasePath();
-    }
-
-    private boolean isContainer() {
-        return System.getenv("MASTERMIND_RUNTIME") != null;
-    }
-
-    private boolean isDevelopment() {
-        try {
-            URI location = MastermindAppLauncher.class.getProtectionDomain().getCodeSource().getLocation().toURI();
-
-            return Files.isDirectory(Path.of(location));
-        } catch (URISyntaxException exception) {
-            //TODO: Think about exception handling logic!
-            throw new RuntimeException(exception);
-        }
     }
 }
