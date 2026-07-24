21784d8a9873b5f6b6084fec538bdfc96705dda3
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-24 18:14:56 +0300

    Refactor DatabasePathResolver single method Now it fully build jdbc sqlite url

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabasePathResolver.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabasePathResolver.java
@@ -1,15 +1,13 @@
 package dev.bibikvlad.mastermind.app.bootstrap.path;
 
-import java.nio.file.Path;
-
 public class DatabasePathResolver {
-    public Path getDatabasePath() {
+    public String getJdbcUrl() {
         DatabaseDeploymentModeFactory factory = new DatabaseDeploymentModeFactory();
         DeploymentModeDetector detector = new DeploymentModeDetector();
 
         DeploymentMode deploymentMode = detector.detect();
         DatabaseLocationProvider provider = factory.getProvider(deploymentMode);
 
-        return provider.getDatabasePath();
+        return "jdbc:sqlite:" + provider.getDatabasePath();
     }
 }
