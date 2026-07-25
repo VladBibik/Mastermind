1c43c642ce40cf47f1bc6e4ab6f00932bf5e5aa6
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-25 04:44:32 +0300

    Refactor simplify switch case

--- src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabaseLocationProviderFactory.java
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/path/DatabaseLocationProviderFactory.java
@@ -2,17 +2,10 @@ package dev.bibikvlad.mastermind.app.bootstrap.path;
 
 public class DatabaseLocationProviderFactory {
     public DatabaseLocationProvider getProvider(DeploymentMode deploymentMode) {
-        switch (deploymentMode) {
-            case DEVELOPMENT -> {
-                return new DevelopmentDatabaseLocationProvider();
-            }
-            case PORTABLE -> {
-                return new PortableDatabaseLocationProvider();
-            }
-            case CONTAINER -> {
-                return new ContainerDatabaseLocationProvider();
-            }
-            default -> throw new IllegalArgumentException("Unknown deployment mode " + deploymentMode);
-        }
+        return switch (deploymentMode) {
+            case DEVELOPMENT -> new DevelopmentDatabaseLocationProvider();
+            case PORTABLE -> new PortableDatabaseLocationProvider();
+            case CONTAINER -> new ContainerDatabaseLocationProvider();
+        };
     }
 }
