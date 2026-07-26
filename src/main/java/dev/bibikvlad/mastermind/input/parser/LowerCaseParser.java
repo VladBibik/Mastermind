9807b398e68696a8798971a9ea71d26c88d2a71f
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-26 18:57:20 +0300

    Refactor LowerCaseParser to be a decorator on top of ConsoleInputParser

--- src/main/java/dev/bibikvlad/mastermind/input/parser/LowerCaseParser.java
+++ src/main/java/dev/bibikvlad/mastermind/input/parser/LowerCaseParser.java
@@ -1,18 +1,14 @@
 package dev.bibikvlad.mastermind.input.parser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class LowerCaseParser implements Parser {
-    private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private final Parser parser;
+
+    public LowerCaseParser(Parser parser) {
+        this.parser = parser;
+    }
 
     @Override
     public String parse() {
-        try {
-            return bufferedReader.readLine().toLowerCase();
-        } catch (IOException exception) {
-            throw new IllegalStateException("Could not read line from input stream", exception);
-        }
+        return parser.parse().toLowerCase();
     }
 }
