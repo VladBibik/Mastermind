3124029e22f4b42639f3fe1dbff1b88a181371dd
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-30 02:31:01 +0300

    Add TestConsoleTuning class
    Testing console configurations to be able to display unicode icons, emojis, etc

---
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/TestConsoleTuning.java
@@ -0,0 +1,44 @@
+package dev.bibikvlad.mastermind.app.bootstrap;
+
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+
+public class TestConsoleTuning {
+    private static final int UTF8_CODE_PAGE = 65001;
+
+    public static void tune() {
+        if (!System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+            return;
+        }
+
+        try (Arena arena = Arena.ofConfined()) {
+            Linker linker = Linker.nativeLinker();
+            SymbolLookup kernel32 = SymbolLookup.libraryLookup("Kernel32", arena);
+
+            MethodHandle setConsoleOutputCP = linker.downcallHandle(
+                    kernel32.find("SetConsoleOutputCP").orElseThrow(),
+                    FunctionDescriptor.of(
+                            ValueLayout.JAVA_INT,
+                            ValueLayout.JAVA_INT
+                    )
+            );
+
+            MethodHandle setConsoleCP = linker.downcallHandle(
+                    kernel32.find("SetConsoleCP").orElseThrow(),
+                    FunctionDescriptor.of(
+                            ValueLayout.JAVA_INT,
+                            ValueLayout.JAVA_INT
+                    )
+            );
+
+            int outputResult = (int) setConsoleOutputCP.invokeExact(UTF8_CODE_PAGE);
+            int inputResult = (int) setConsoleCP.invokeExact(UTF8_CODE_PAGE);
+
+            if (outputResult == 0 || inputResult == 0) {
+                throw new IllegalStateException("Failed to set Windows console UTF-8 code page");
+            }
+        } catch (Throwable throwable) {
+            throw new RuntimeException("Failed to configure Windows console", throwable);
+        }
+    }
+}
