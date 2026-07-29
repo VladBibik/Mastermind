ed94d94fe0ab5e53dd29ca8143fa9ad43aaa9f48
BibikVlad <bibikvladislav.work@gmail.com>
2026-07-29 05:52:13 +0300

    Delete TODO and format method

--- src/main/java/dev/bibikvlad/utils/formatters/SQLiteTimestampFormatter.java
+++ src/main/java/dev/bibikvlad/utils/formatters/SQLiteTimestampFormatter.java
@@ -5,15 +5,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class SQLiteTimestampFormatter {
-    //TODO: Need a refactor!!! Just temp class to be able to see normal result!
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.US);
 
     public static LocalDateTime parse(String date) {
         return LocalDateTime.parse(date, FORMATTER);
-    }
-
-    public static String format(LocalDateTime date) {
-        return FORMATTER.format(date);
     }
 }
