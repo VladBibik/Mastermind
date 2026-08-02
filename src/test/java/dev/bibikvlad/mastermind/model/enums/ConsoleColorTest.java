affbb303789d3a25db2055f50435534d7bb42479
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-02 22:04:06 +0300

    Update DisplayName Rename method

--- src/test/java/dev/bibikvlad/mastermind/model/enums/ConsoleColorTest.java
+++ src/test/java/dev/bibikvlad/mastermind/model/enums/ConsoleColorTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConsoleColorTest {
     @Test
-    @DisplayName("Correctly returns enum from the index")
-    void validIndexReturnsCorrectColor() {
+    @DisplayName("Correctly returns foreground console color from the index")
+    void validIndexReturnsCorrectForegroundColor() {
         assertEquals(ConsoleColor.BLACK, ConsoleColor.fromForegroundColorByIndex(1));
         assertEquals(ConsoleColor.BRIGHT_YELLOW, ConsoleColor.fromForegroundColorByIndex(12));
         assertEquals(ConsoleColor.GOLD, ConsoleColor.fromForegroundColorByIndex(29));
