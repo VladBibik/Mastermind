96c2d89541df880d78898d9f3a3db1a2bf746595
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-05 04:58:52 +0300

    Refactor console printer Fix issue with incorrect emoji and Russian letters showing on the terminal when .exe was built

--- src/main/java/dev/bibikvlad/mastermind/app/printer/ConsolePrinter.java
+++ src/main/java/dev/bibikvlad/mastermind/app/printer/ConsolePrinter.java
@@ -1,7 +1,17 @@
 package dev.bibikvlad.mastermind.app.printer;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class ConsolePrinter implements Printer {
     public void printMessage(String message) {
-        System.out.println(message);
+        byte[] bytes = (message + System.lineSeparator()).getBytes(StandardCharsets.UTF_8);
+
+        try {
+            System.out.write(bytes);
+            System.out.flush();
+        } catch (IOException exception) {
+            throw new IllegalStateException("Could not write to console", exception);
+        }
     }
 }
