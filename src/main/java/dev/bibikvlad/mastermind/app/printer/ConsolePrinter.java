06690d4000bc34141bc009a25d46f35aaf0e6a14
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-05 23:13:33 +0300

    Refactor ConsolePrinter class Made it simpler

--- src/main/java/dev/bibikvlad/mastermind/app/printer/ConsolePrinter.java
+++ src/main/java/dev/bibikvlad/mastermind/app/printer/ConsolePrinter.java
@@ -1,17 +1,20 @@
 package dev.bibikvlad.mastermind.app.printer;
 
-import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
 public class ConsolePrinter implements Printer {
-    public void printMessage(String message) {
-        byte[] bytes = (message + System.lineSeparator()).getBytes(StandardCharsets.UTF_8);
+    private final PrintStream out;
 
-        try {
-            System.out.write(bytes);
-            System.out.flush();
-        } catch (IOException exception) {
-            throw new IllegalStateException("Could not write to console", exception);
-        }
+    public ConsolePrinter() {
+        this.out = new PrintStream(
+                System.out,
+                true,
+                StandardCharsets.UTF_8
+        );
+    }
+
+    public void printMessage(String message) {
+        out.println(message);
     }
 }
