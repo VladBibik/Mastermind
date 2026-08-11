5cf10900a3c0b2a96699eafb6bafaffdc7313563
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-11 17:13:21 +0300

    Add testTimeFormattingAccuracy test method

--- src/test/java/dev/bibikvlad/utils/formatters/TimeToStringFormatterTest.java
+++ src/test/java/dev/bibikvlad/utils/formatters/TimeToStringFormatterTest.java
@@ -4,8 +4,12 @@ import dev.bibikvlad.mastermind.localization.messages.common.ConsoleTimeFormatti
 import dev.bibikvlad.mastermind.localization.messages.common.TimeFormattingMessages;
 import dev.bibikvlad.mastermind.values.Time;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TimeToStringFormatterTest {
     private static TimeFormattingMessages messages;
@@ -16,5 +20,13 @@ class TimeToStringFormatterTest {
         messages = new ConsoleTimeFormattingMessages(
                 ResourceBundle.getBundle("i18n.common.time_formatting"));
         time = new Time(12, 34, 25, 377);
+    }
+
+    @Test
+    @DisplayName("Returns correctly formatted time")
+    void testTimeFormattingAccuracy() {
+        String expectedTime = "12h 34min 25sec 377ms";
+
+        assertEquals(expectedTime, TimeToStringFormatter.format(time, messages));
     }
 }
