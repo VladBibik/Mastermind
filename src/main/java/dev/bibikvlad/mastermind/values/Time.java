dcd77dc00fcf770fdcf647a1e2933e72dfa7b341
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-14 03:24:35 +0300

    Add validator to the constructor

--- src/main/java/dev/bibikvlad/mastermind/values/Time.java
+++ src/main/java/dev/bibikvlad/mastermind/values/Time.java
@@ -1,4 +1,11 @@
 package dev.bibikvlad.mastermind.values;
 
 public record Time(long hour, long minutes, long second, long millisecond) {
+    public Time {
+        TimeValidator validator = new TimeValidator();
+
+        if (!validator.validate(hour, minutes, second, millisecond)) {
+            throw new IllegalArgumentException("Invalid hour or minutes or seconds or milliseconds");
+        }
+    }
 }
