71381f83d48ce033382be0a132f082a229fe766a
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-16 04:35:59 +0300

    Refactor validate method Change if statements from the chain to individual cases

--- src/main/java/dev/bibikvlad/mastermind/values/TimeValidator.java
+++ src/main/java/dev/bibikvlad/mastermind/values/TimeValidator.java
@@ -8,11 +8,17 @@ public class TimeValidator {
     public static void validate(long hour, long minutes, long second, long millisecond) {
         if (hour < 0) {
             throw new IllegalArgumentException("Hour cannot be negative");
-        } else if (minutes < 0 || minutes > 59) {
+        }
+
+        if (minutes < 0 || minutes > 59) {
             throw new IllegalArgumentException("Minutes must be between 0 and 59");
-        } else if (second < 0 || second > 59) {
+        }
+
+        if (second < 0 || second > 59) {
             throw new IllegalArgumentException("Seconds must be between 0 and 59");
-        } else if (millisecond < 0 || millisecond > 999) {
+        }
+
+        if (millisecond < 0 || millisecond > 999) {
             throw new IllegalArgumentException("Milliseconds must be between 0 and 999");
         }
     }
