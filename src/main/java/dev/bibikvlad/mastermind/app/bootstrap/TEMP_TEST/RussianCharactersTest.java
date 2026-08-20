f1f02b6d7c8ae9fccf1751bd2f032d58820eb5a1
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-20 05:55:21 +0300

    Add RussianCharactersTest class with diagnostic related methods

---
+++ src/main/java/dev/bibikvlad/mastermind/app/bootstrap/TEMP_TEST/RussianCharactersTest.java
@@ -0,0 +1,27 @@
+package dev.bibikvlad.mastermind.app.bootstrap.TEMP_TEST;
+
+import dev.bibikvlad.mastermind.input.parser.ConsoleInputParser;
+import dev.bibikvlad.mastermind.input.parser.Parser;
+
+public class RussianCharactersTest {
+    public void test() {
+        System.out.println("=== INPUT TEST ===");
+        System.out.print("Enter Russian text: ");
+
+        Parser parser = new ConsoleInputParser();
+        String input = parser.parse();
+
+        System.out.println("Received: [" + input + "]");
+        System.out.println("UTF-16 length: " + input.length());
+        System.out.println(
+                "Code points: " +
+                        input.codePointCount(0, input.length())
+        );
+
+        System.out.println("Code points:");
+
+        input.codePoints().forEach(codePoint ->
+                System.out.printf("U+%04X%n", codePoint)
+        );
+    }
+}
