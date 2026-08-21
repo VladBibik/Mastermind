0e7599766b1282c7510fd5383e6c24aaaedf80b4
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-21 05:44:05 +0300

    Refactor ConsoleInputParser BufferReader field initialization

--- src/main/java/dev/bibikvlad/mastermind/input/parser/ConsoleInputParser.java
+++ src/main/java/dev/bibikvlad/mastermind/input/parser/ConsoleInputParser.java
@@ -3,9 +3,11 @@ package dev.bibikvlad.mastermind.input.parser;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ConsoleInputParser implements Parser {
-    private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private final BufferedReader bufferedReader = new BufferedReader(
+            new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
     @Override
     public String parse() {
