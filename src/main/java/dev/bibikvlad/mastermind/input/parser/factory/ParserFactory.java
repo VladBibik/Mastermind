6f5338d6ff73fd6ac27016351c0d33788dec4c65
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-23 03:52:31 +0300

    Refactor logic in ParserFactory class

--- src/main/java/dev/bibikvlad/mastermind/input/parser/factory/ParserFactory.java
+++ src/main/java/dev/bibikvlad/mastermind/input/parser/factory/ParserFactory.java
@@ -6,31 +6,14 @@ import dev.bibikvlad.mastermind.input.parser.WindowsConsoleInputParser;
 
 public class ParserFactory {
     private ParserFactory() {
-        throw new AssertionError(
-                "The class ParserFactory cannot be instantiated."
-        );
+        throw new AssertionError("The class ParserFactory cannot be instantiated.");
     }
 
-    public static Parser create() {
-        if (shouldUseWindowsConsoleInputParser()) {
+    public static Parser create(boolean isWindows) {
+        if (isWindows) {
             return new WindowsConsoleInputParser();
         }
 
         return new ConsoleInputParser();
-    }
-
-    private static boolean shouldUseWindowsConsoleInputParser() {
-        return isWindows() && isWindowsConsoleHost();
-    }
-
-    private static boolean isWindows() {
-        return System.getProperty("os.name")
-                .toLowerCase()
-                .startsWith("windows");
-    }
-
-    private static boolean isWindowsConsoleHost() {
-        // TODO: determine the actual terminal/host
-        return false;
     }
 }
