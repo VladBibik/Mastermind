dd2f17553550892d28f8a0ddc6ca0500bd0de878
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-25 19:22:17 +0300

    Add private constructor

--- src/main/java/dev/bibikvlad/mastermind/localization/config/CompatibilityLocale.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/config/CompatibilityLocale.java
@@ -3,6 +3,10 @@ package dev.bibikvlad.mastermind.localization.config;
 import java.util.Locale;
 
 public class CompatibilityLocale {
+    private CompatibilityLocale() {
+        throw new AssertionError("Can't instantiate CompatibilityLocale class");
+    }
+
     public static Locale getCompitabilityLocale() {
         return new Locale.Builder()
                 .setLanguage("en")
