1a95dcdb921b83a9014a394e5ae1a8ad963edac1
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-26 04:24:05 +0300

    Refactor GuessEvaluator class Since clue symbols now rely on the mode we need to take them into the account before creating evaluation message

--- src/main/java/dev/bibikvlad/mastermind/game/GuessEvaluator.java
+++ src/main/java/dev/bibikvlad/mastermind/game/GuessEvaluator.java
@@ -2,15 +2,18 @@ package dev.bibikvlad.mastermind.game;
 
 import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+import dev.bibikvlad.utils.strings.clue.ClueSymbols;
 
 public class GuessEvaluator {
     private final String correctAnswer;
     private final Printer printer;
     private final GameMessages gameMessages;
+    private final ClueSymbols clueSymbols;
 
-    public GuessEvaluator(Printer printer, GameMessages gameMessages, String correctAnswer) {
+    public GuessEvaluator(Printer printer, GameMessages gameMessages, ClueSymbols clueSymbols, String correctAnswer) {
         this.printer = printer;
         this.gameMessages = gameMessages;
+        this.clueSymbols = clueSymbols;
         this.correctAnswer = correctAnswer;
     }
 
@@ -20,7 +23,7 @@ public class GuessEvaluator {
 
             return true;
         } else {
-            printer.printMessage(gameMessages.getIncorrectGuess(maxTurns, turn, correctAnswer, userInput));
+            printer.printMessage(gameMessages.getIncorrectGuess(clueSymbols, maxTurns, turn, correctAnswer, userInput));
 
             return false;
         }
