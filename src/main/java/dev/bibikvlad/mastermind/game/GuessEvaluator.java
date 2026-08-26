a07ab01204b5092603565510fd1fd93ed510b68c
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-27 02:49:22 +0300

    Refactor visual representation retrieval to depend on compatibility mode

--- src/main/java/dev/bibikvlad/mastermind/game/GuessEvaluator.java
+++ src/main/java/dev/bibikvlad/mastermind/game/GuessEvaluator.java
@@ -1,5 +1,6 @@
 package dev.bibikvlad.mastermind.game;
 
+import dev.bibikvlad.mastermind.app.game.mode.GameModeDependentMessages;
 import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
 import dev.bibikvlad.utils.strings.clue.ClueSymbols;
@@ -8,21 +9,26 @@ public class GuessEvaluator {
     private final String correctAnswer;
     private final Printer printer;
     private final GameMessages gameMessages;
-    private final ClueSymbols clueSymbols;
+    private final GameModeDependentMessages gameModeDependentMessages;
 
-    public GuessEvaluator(Printer printer, GameMessages gameMessages, ClueSymbols clueSymbols, String correctAnswer) {
+    public GuessEvaluator(Printer printer, GameMessages gameMessages,
+                          GameModeDependentMessages gameModeDependentMessages, String correctAnswer) {
         this.printer = printer;
         this.gameMessages = gameMessages;
-        this.clueSymbols = clueSymbols;
+        this.gameModeDependentMessages = gameModeDependentMessages;
         this.correctAnswer = correctAnswer;
     }
 
     public boolean evaluate(String userInput, int turn, int maxTurns) {
         if (userInput.equals(correctAnswer)) {
-            printer.printMessage(gameMessages.getWin(correctAnswer));
+            String visualRepresentation = gameModeDependentMessages.getVisualRepresentation(correctAnswer);
+
+            printer.printMessage(gameMessages.getWin(visualRepresentation));
 
             return true;
         } else {
+            ClueSymbols clueSymbols = gameModeDependentMessages.getClueSymbols();
+
             printer.printMessage(gameMessages.getIncorrectGuess(clueSymbols, maxTurns, turn, correctAnswer, userInput));
 
             return false;
