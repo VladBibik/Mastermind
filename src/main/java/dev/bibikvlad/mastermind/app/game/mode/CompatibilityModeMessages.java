2b4bece0130cfa19c2f6ede22d9d0f08ac2a09e7
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-26 04:16:59 +0300

    Add CompatibilityModeMessages class

---
+++ src/main/java/dev/bibikvlad/mastermind/app/game/mode/CompatibilityModeMessages.java
@@ -0,0 +1,28 @@
+package dev.bibikvlad.mastermind.app.game.mode;
+
+import dev.bibikvlad.utils.strings.ConsoleCompatibleValidSymbols;
+import dev.bibikvlad.utils.strings.clue.ClueSymbols;
+import dev.bibikvlad.utils.strings.clue.CompatibilityClueSymbols;
+import dev.bibikvlad.utils.strings.logos.DefaultAsciiLogo;
+
+public class CompatibilityModeMessages implements GameModeDependentMessages {
+    @Override
+    public String getLogo() {
+        return DefaultAsciiLogo.getLogo();
+    }
+
+    @Override
+    public String getVisualRepresentation(String answer) {
+        return answer;
+    }
+
+    @Override
+    public ClueSymbols getClueSymbols() {
+        return new CompatibilityClueSymbols();
+    }
+
+    @Override
+    public String validSymbols() {
+        return ConsoleCompatibleValidSymbols.getSymbols();
+    }
+}
