583c28c31cbd1f1f5bda1f0a32f02f280d2f238f
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-26 04:19:00 +0300

    Refactor GameMessages method parameters

--- src/main/java/dev/bibikvlad/mastermind/localization/messages/game/GameMessages.java
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/game/GameMessages.java
@@ -1,15 +1,17 @@
 package dev.bibikvlad.mastermind.localization.messages.game;
 
 import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+import dev.bibikvlad.utils.strings.clue.ClueSymbols;
 
 public interface GameMessages extends LocalizedMessages {
-    String getInvalidInput();
+    String getInvalidInput(String validSymbols);
 
-    String getIncorrectGuess(int maxTurns, int currentTurn, String answer, String userInput);
+    String getIncorrectGuess(ClueSymbols clueSymbols, int maxTurns, int currentTurn,
+                             String answer, String userInput);
 
     String getGameOver(String answer);
 
     String getWin(String answer);
 
-    String getRules();
+    String getRules(ClueSymbols clueSymbols, String validSymbols);
 }
