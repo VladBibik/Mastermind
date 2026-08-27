b88d68f5027ae75a78828d6004c9724cbc2621a8
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-27 05:44:37 +0300

    Add GameOutput class with full logic

---
+++ src/main/java/dev/bibikvlad/mastermind/game/output/GameOutput.java
@@ -0,0 +1,62 @@
+package dev.bibikvlad.mastermind.game.output;
+
+import dev.bibikvlad.mastermind.app.game.mode.GamePresentation;
+import dev.bibikvlad.mastermind.app.printer.Printer;
+import dev.bibikvlad.mastermind.clues.ClueGenerator;
+import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
+
+public class GameOutput {
+    private final Printer printer;
+    private final GameMessages gameMessages;
+    private final GamePresentation gamePresentation;
+
+    public GameOutput(Printer printer, GameMessages gameMessages, GamePresentation gamePresentation) {
+        this.printer = printer;
+        this.gameMessages = gameMessages;
+        this.gamePresentation = gamePresentation;
+    }
+
+    public void printInvalidInput() {
+        printer.printMessage(
+                gameMessages.getInvalidInput(
+                        gamePresentation.getValidSymbols()
+                )
+        );
+    }
+
+    public void printIncorrectGuess(int maxTurns, int currentTurn, String answer, String userInput) {
+        printer.printMessage(
+                gameMessages.getIncorrectGuess(maxTurns, currentTurn, answer, userInput,
+                        ClueGenerator.generate(
+                                gamePresentation.getClueSymbols(),
+                                answer,
+                                userInput
+                        )
+                )
+        );
+    }
+
+    public void printGameOver(String answer) {
+        printer.printMessage(
+                gameMessages.getGameOver(
+                        gamePresentation.getVisualRepresentation(answer)
+                )
+        );
+    }
+
+    public void printWin(String answer) {
+        printer.printMessage(
+                gameMessages.getWin(
+                        gamePresentation.getVisualRepresentation(answer)
+                )
+        );
+    }
+
+    public void printRules() {
+        printer.printMessage(gameMessages.getRules(
+                        gamePresentation.getClueSymbols(),
+                        gamePresentation.getValidSymbols()
+                )
+        );
+    }
+}
