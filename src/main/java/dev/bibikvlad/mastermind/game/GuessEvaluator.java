c2918e047f0cda8cb43e5e037d5cc13aafbfd50e
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-27 05:55:13 +0300

    Refactor GuessEvaluator to use GameOutput

--- src/main/java/dev/bibikvlad/mastermind/game/GuessEvaluator.java
+++ src/main/java/dev/bibikvlad/mastermind/game/GuessEvaluator.java
@@ -1,35 +1,23 @@
 package dev.bibikvlad.mastermind.game;
 
-import dev.bibikvlad.mastermind.app.game.mode.GamePresentation;
-import dev.bibikvlad.mastermind.app.printer.Printer;
-import dev.bibikvlad.mastermind.localization.messages.game.GameMessages;
-import dev.bibikvlad.utils.strings.clue.ClueSymbols;
+import dev.bibikvlad.mastermind.game.output.GameOutput;
 
 public class GuessEvaluator {
+    private final GameOutput gameOutput;
     private final String correctAnswer;
-    private final Printer printer;
-    private final GameMessages gameMessages;
-    private final GamePresentation gamePresentation;
 
-    public GuessEvaluator(Printer printer, GameMessages gameMessages,
-                          GamePresentation gamePresentation, String correctAnswer) {
-        this.printer = printer;
-        this.gameMessages = gameMessages;
-        this.gamePresentation = gamePresentation;
+    public GuessEvaluator(GameOutput gameOutput, String correctAnswer) {
+        this.gameOutput = gameOutput;
         this.correctAnswer = correctAnswer;
     }
 
     public boolean evaluate(String userInput, int turn, int maxTurns) {
         if (userInput.equals(correctAnswer)) {
-            String visualRepresentation = gamePresentation.getVisualRepresentation(correctAnswer);
-
-            printer.printMessage(gameMessages.getWin(visualRepresentation));
+            gameOutput.printWin(correctAnswer);
 
             return true;
         } else {
-            ClueSymbols clueSymbols = gamePresentation.getClueSymbols();
-
-            printer.printMessage(gameMessages.getIncorrectGuess(clueSymbols, maxTurns, turn, correctAnswer, userInput));
+            gameOutput.printIncorrectGuess(maxTurns, turn, correctAnswer, userInput);
 
             return false;
         }
