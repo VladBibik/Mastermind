f186c5789e0cf12c2ff8171f3230497d51536a58
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-30 21:41:02 +0300

    Add CompatibilityMenuMessages interface

---
+++ src/main/java/dev/bibikvlad/mastermind/localization/messages/menu/main/settings/compatibility/CompatibilityMenuMessages.java
@@ -0,0 +1,7 @@
+package dev.bibikvlad.mastermind.localization.messages.menu.main.settings.compatibility;
+
+import dev.bibikvlad.mastermind.localization.messages.LocalizedMessages;
+
+public interface CompatibilityMenuMessages extends LocalizedMessages {
+    String getCompatibilityWarning();
+}
