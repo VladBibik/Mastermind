ae9f75c3510c8d3fafc8f774f180c4e340aae6ab
BibikVlad <bibikvladislav.work@gmail.com>
2026-08-30 05:38:32 +0300

    Add essential fields for CompatibilityMenu

--- src/main/java/dev/bibikvlad/mastermind/menu/main/settings/compatibility/CompatibilityMenu.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/main/settings/compatibility/CompatibilityMenu.java
@@ -1,12 +1,23 @@
 package dev.bibikvlad.mastermind.menu.main.settings.compatibility;
 
 import dev.bibikvlad.mastermind.app.context.AppContext;
+import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.menu.core.Menu;
 import dev.bibikvlad.mastermind.menu.main.settings.SettingsMenu;
+import dev.bibikvlad.mastermind.model.player.Player;
+import dev.bibikvlad.mastermind.services.PlayerService;
 
 public class CompatibilityMenu extends Menu {
+    private final Printer printer;
+    private final Player currentPlayer;
+    private final PlayerService playerService;
+
     public CompatibilityMenu(AppContext appContext) {
         super(appContext);
+
+        this.printer = appContext.printer();
+        this.currentPlayer = appContext.currentPlayer();
+        this.playerService = appContext.services().getPlayerService();
     }
 
     @Override
