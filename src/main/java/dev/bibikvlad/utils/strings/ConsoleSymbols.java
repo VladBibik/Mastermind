72d05badc95fbf00b6e8f6232d0fcac55cf35c32
BibikVlad <bibikvladislav.work@gmail.com>
2026-09-03 05:22:04 +0300

    Add private constructor

--- src/main/java/dev/bibikvlad/utils/strings/ConsoleSymbols.java
+++ src/main/java/dev/bibikvlad/utils/strings/ConsoleSymbols.java
@@ -1,5 +1,9 @@
 package dev.bibikvlad.utils.strings;
 
 public class ConsoleSymbols {
+    private ConsoleSymbols() {
+        throw new AssertionError("ConsoleSymbols cannot be instantiated.");
+    }
+
     public static final char CIRCLE_SOLID = '⬤';
 }
