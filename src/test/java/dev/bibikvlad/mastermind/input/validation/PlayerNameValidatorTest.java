577c91fbbcd0d7ee9ca13460652d7b5b9d6a4955
BibikVlad <bibikvladislav.work@gmail.com>
2026-09-06 23:08:19 +0300

    Add nameWithLengthMoreThan100ReturnsFals test method

--- src/test/java/dev/bibikvlad/mastermind/input/validation/PlayerNameValidatorTest.java
+++ src/test/java/dev/bibikvlad/mastermind/input/validation/PlayerNameValidatorTest.java
@@ -4,12 +4,26 @@ import dev.bibikvlad.mastermind.app.printer.ConsolePrinter;
 import dev.bibikvlad.mastermind.app.printer.Printer;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.profile.name.ConsolePlayerNameMessages;
 import dev.bibikvlad.mastermind.localization.messages.menu.main.profile.name.PlayerNameMessages;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class PlayerNameValidatorTest {
     private final PlayerNameMessages messages = new ConsolePlayerNameMessages(
             ResourceBundle.getBundle("i18n.menu.main.profile.name.player_name"));
     private final Printer printer = new ConsolePrinter();
     private final PlayerNameValidator playerNameValidator = new PlayerNameValidator(printer, messages);
+
+    @Test
+    @DisplayName("Name with length more than 100 returns fals")
+    void nameWithLengthMoreThan100ReturnsFals() {
+        StringBuilder playerName = new StringBuilder("12345678910");
+
+        playerName.append(String.valueOf(playerName).repeat(11));
+
+        assertFalse(playerNameValidator.validateAndPrintErrors(playerName.toString()));
+    }
 }
