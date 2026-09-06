995a6a8e3d11184a0ec984dadc0b91f4c09935ac
BibikVlad <bibikvladislav.work@gmail.com>
2026-09-06 23:14:19 +0300

    Add empty constructor

--- src/main/java/dev/bibikvlad/mastermind/input/validation/StringEmptyValidator.java
+++ src/main/java/dev/bibikvlad/mastermind/input/validation/StringEmptyValidator.java
@@ -1,6 +1,10 @@
 package dev.bibikvlad.mastermind.input.validation;
 
 public class StringEmptyValidator {
+    private StringEmptyValidator() {
+        throw new AssertionError("StringEmptyValidator cannot be instantiated");
+    }
+
     public static boolean isNullOrEmpty(String userInput) {
         return userInput == null || userInput.trim().isEmpty();
     }
