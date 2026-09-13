91ff104d1b5a6cea3690f80ce6a8ef451e09f333
BibikVlad <bibikvladislav.work@gmail.com>
2026-09-13 19:16:42 +0300

    Refactor exception message

--- src/main/java/dev/bibikvlad/utils/DefaultLogoColorsBundle.java
+++ src/main/java/dev/bibikvlad/utils/DefaultLogoColorsBundle.java
@@ -12,6 +12,6 @@ public class DefaultLogoColorsBundle {
     );
 
     private DefaultLogoColorsBundle() {
-        throw new AssertionError("Utility class");
+        throw new AssertionError("Cannot instantiate DefaultLogoColorsBundle");
     }
 }
