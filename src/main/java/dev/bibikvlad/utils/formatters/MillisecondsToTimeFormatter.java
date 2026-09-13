57fb775876419bbf0681ad7484204bedcebbc929
BibikVlad <bibikvladislav.work@gmail.com>
2026-09-14 00:36:04 +0300

    Add private constructor

--- src/main/java/dev/bibikvlad/utils/formatters/MillisecondsToTimeFormatter.java
+++ src/main/java/dev/bibikvlad/utils/formatters/MillisecondsToTimeFormatter.java
@@ -5,6 +5,11 @@ import dev.bibikvlad.mastermind.values.Time;
 import java.time.Duration;
 
 public class MillisecondsToTimeFormatter {
+
+    private MillisecondsToTimeFormatter() {
+        throw new AssertionError("Cannot instantiate DefaultLogoColorsBundle");
+    }
+
     public static Time format(long milliseconds) {
         Duration duration = Duration.ofMillis(milliseconds);
 
