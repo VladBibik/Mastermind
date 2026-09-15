09d3ef32e387ee824173c64c52c68209bcb7f88d
BibikVlad <bibikvladislav.work@gmail.com>
2026-09-15 04:49:39 +0300

    Add private constructor

--- src/main/java/dev/bibikvlad/mastermind/menu/core/MenuRunner.java
+++ src/main/java/dev/bibikvlad/mastermind/menu/core/MenuRunner.java
@@ -1,6 +1,11 @@
 package dev.bibikvlad.mastermind.menu.core;
 
 public class MenuRunner {
+
+    private  MenuRunner() {
+        throw new AssertionError("Cannot instantiate MenuRunner");
+    }
+
     public static void runMenu(Menu menu) {
 
         while (!(menu instanceof ExitMenu)) {
